2a01e83954fce82c6e60527eff4a020cdfbd82b5
Juanin92 <j.claveria19@com>
2024-12-18 18:15:40 -0300

    Create ticket-customer component

---
+++ veloProweb-frontend/src/app/components/customerView/ticket-customer/ticket-customer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-customer',
+  standalone: true,
+  imports: [],
+  templateUrl: './ticket-customer.component.html',
+  styleUrl: './ticket-customer.component.css'
+})
+export class TicketCustomerComponent {
+
+}
