55b2afe7093b45d97769d0737c7560a29d28fd6c
Juanin92 <j.claveria19@com>
2024-12-20 22:46:47 -0300

    create/add subcategory test

---
+++ veloProWeb/src/test/java/com/veloProWeb/Service/Product/SubcategoryServiceTest.java
@@ -0,0 +1,76 @@
+package com.veloProWeb.Service.Product;
+
+import com.veloProWeb.Model.Entity.Product.BrandProduct;
+import com.veloProWeb.Model.Entity.Product.CategoryProduct;
+import com.veloProWeb.Model.Entity.Product.SubcategoryProduct;
+import com.veloProWeb.Repository.Product.SubcategoryProductRepo;
+import com.veloProWeb.Validation.CategoriesValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class SubcategoryServiceTest {
+
+    @InjectMocks private SubcategoryService subcategoryService;
+    @Mock private SubcategoryProductRepo subcategoryProductRepo;
+    @Mock private CategoriesValidator validator;
+    private SubcategoryProduct subcategory;
+    private SubcategoryProduct existingSubcategory;
+    private CategoryProduct category;
+
+    @BeforeEach
+    void setUp(){
+        category = new CategoryProduct();
+        category.setName("Comida");
+        category.setId(2L);
+        subcategory = new SubcategoryProduct();
+        existingSubcategory = new SubcategoryProduct();
+        existingSubcategory.setName("Arroz");
+    }
+
+    //Prueba para crear una nueva marca
+    @Test
+    public void save_valid(){
+        subcategory.setName("leche");
+        when(subcategoryProductRepo.findByNameAndCategoryId("Accesorios",1L)).thenReturn(Optional.empty());
+        doNothing().when(validator).validateSubcategory("leche");
+        subcategoryService.save(subcategory, category);
+
+        verify(validator).validateSubcategory("asus");
+        verify(subcategoryProductRepo).findByNameAndCategoryId("Leche", 1L);
+        verify(subcategoryProductRepo).save(subcategory);
+        assertEquals("Asus", subcategory.getName());
+    }
+    @Test
+    public void save_invalidExistingBrand(){
+        subcategory.setName("Arroz");
+        when(subcategoryProductRepo.findByNameAndCategoryId("Comida", 2L)).thenReturn(Optional.of(existingSubcategory));
+        doNothing().when(validator).validateSubcategory("Arroz");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            subcategoryService.save(subcategory, category);
+        });
+
+        assertEquals("Nombre Existente: Hay registro de esta Subcategoría en la Categoría", exception.getMessage());
+        verify(validator).validateSubcategory("Arroz");
+        verify(subcategoryProductRepo).findByNameAndCategoryId("Comida", 2L);
+        verify(subcategoryProductRepo, never()).save(subcategory);
+    }
+
+    //Prueba para obtener todas las marcas
+    @Test
+    public void getAll_valid(){
+        subcategoryService.getAll();
+        verify(subcategoryProductRepo).findAll();
+    }
+}
