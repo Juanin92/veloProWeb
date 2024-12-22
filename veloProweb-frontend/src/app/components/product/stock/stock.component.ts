93b3732faec5cc06f9bf3d1536fe291ce54d92cf
Juanin92 <j.claveria19@com>
2024-12-22 18:38:15 -0300

    Create/Add product DTO class

--- veloProweb-frontend/src/app/components/product/stock/stock.component.ts
+++ veloProweb-frontend/src/app/components/product/stock/stock.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StockService } from '../../../services/Product/stock.service';
 import { Product } from '../../../models/Entity/Product/product.model';
 import { CommonModule } from '@angular/common';
+import { ProductDTO } from '../../../models/DTO/product-dto';
 
 @Component({
   selector: 'app-stock',
@@ -12,21 +13,21 @@ import { CommonModule } from '@angular/common';
 })
 export class StockComponent implements OnInit{
 
-  products: Product[] = [];
-  filteredProducts: Product[] = [];
+  products: ProductDTO[] = [];
+  filteredProducts: ProductDTO[] = [];
   textFilter: string = '';
 
   constructor(private stockService: StockService){}
 
   ngOnInit(): void {
     this.getProducts();
-    console.log('Producto: ', this.products);
   }
 
   getProducts(): void{
     this.stockService.getProducts().subscribe((list) =>{
       this.products = list;
       this.filteredProducts = list;
+      console.log('Producto: ', this.products);
     }, (error) => {
       console.log('Error no se encontró ningún producto', error);
     });
@@ -37,9 +38,9 @@ export class StockComponent implements OnInit{
       this.filteredProducts = this.products;
     } else {
       this.filteredProducts = this.products.filter(product =>
-        product.brand.name.toLowerCase().includes(this.textFilter.toLowerCase()) ||
-        product.category.name.toLowerCase().includes(this.textFilter.toLowerCase()) ||
-        product.subcategoryProduct.name.toLowerCase().includes(this.textFilter.toLowerCase()) ||
+        product.brand.toLowerCase().includes(this.textFilter.toLowerCase()) ||
+        product.category.toLowerCase().includes(this.textFilter.toLowerCase()) ||
+        product.subcategoryProduct.toLowerCase().includes(this.textFilter.toLowerCase()) ||
         product.description.toLowerCase().includes(this.textFilter.toLowerCase())
       );
     }
--- veloProweb-frontend/src/app/services/Product/stock.service.ts
+++ veloProweb-frontend/src/app/services/Product/stock.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Product } from '../../models/Entity/Product/product.model';
+import { ProductDTO } from '../../models/DTO/product-dto';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +12,7 @@ export class StockService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getProducts(): Observable<Product[]>{
-    return this.httpClient.get<Product[]>(this.apiUrl);
+  getProducts(): Observable<ProductDTO[]>{
+    return this.httpClient.get<ProductDTO[]>(this.apiUrl);
   }
 }
