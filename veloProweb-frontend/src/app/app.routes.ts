b6806d14a281f9057bb08bd067c111da865bec4a
Juanin92 <j.claveria19@com>
2024-12-22 15:58:10 -0300

    Add stock components functionality

--- veloProweb-frontend/src/app/app.routes.ts
+++ veloProweb-frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CustomerComponent } from './components/customerView/customer.component';
+import { StockComponent } from './components/product/stock/stock.component';
 
 export const routes: Routes = [
-    {path: 'clientes', component: CustomerComponent}
+    {path: 'clientes', component: CustomerComponent},
+    {path: 'stock', component: StockComponent}
 ];
