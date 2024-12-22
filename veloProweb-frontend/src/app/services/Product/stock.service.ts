a7589a3c7c38e8b8a806202011d858329ab2d41e
Juanin92 <j.claveria19@com>
2024-12-22 15:38:22 -0300

    Add product service method

--- veloProweb-frontend/src/app/services/Product/stock.service.ts
+++ veloProweb-frontend/src/app/services/Product/stock.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Product } from '../../models/Entity/Product/product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +11,8 @@ export class StockService {
   private apiUrl = 'http://localhost:8080/clientes';
 
   constructor(private httpClient: HttpClient) { }
+
+  getProducts(): Observable<Product[]>{
+    return this.httpClient.get<Product[]>(this.apiUrl);
+  }
 }
