dd752360f951ca22e7cb195da0d8b7b43702a824
Juanin92 <j.claveria19@com>
2024-12-23 13:30:07 -0300

    Modify updateproduct components

--- veloProweb-frontend/src/app/components/product/update-product/update-product.component.ts
+++ veloProweb-frontend/src/app/components/product/update-product/update-product.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProductService } from '../../../services/Product/product.service';
+import { ProductHelperService } from '../../../services/Product/product-helper.service';
+import { NotificationService } from '../../../utils/notification-service.service';
+import { Product } from '../../../models/Entity/Product/product.model';
+import { ProductValidator } from '../../../validation/product-validator';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-update-product',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './update-product.component.html',
   styleUrl: './update-product.component.css'
 })
 export class UpdateProductComponent {
+
+  @Input() selectedProduct: Product;
+  validator = ProductValidator;
+
+  constructor(
+    private productService: ProductService,
+    private helper: ProductHelperService,
+    private notification: NotificationService){
+      this.selectedProduct = helper.createEmptyProduct();
+    }
 
 }
