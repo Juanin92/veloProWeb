59e4a82d491454555600a88e5edc7c03dc11bb04
Juanin92 <j.claveria19@com>
2024-12-23 13:33:14 -0300

    Modify stock  components

--- veloProweb-frontend/src/app/components/product/stock/stock.component.ts
+++ veloProweb-frontend/src/app/components/product/stock/stock.component.ts
@@ -5,11 +5,13 @@ import { CommonModule, NgStyle } from '@angular/common';
 import { ProductDTO } from '../../../models/DTO/product-dto';
 import { FormsModule } from '@angular/forms';
 import { AddProductComponent } from "../add-product/add-product.component";
+import { ProductHelperService } from '../../../services/Product/product-helper.service';
+import { UpdateProductComponent } from "../update-product/update-product.component";
 
 @Component({
   selector: 'app-stock',
   standalone: true,
-  imports: [CommonModule, NgStyle, FormsModule, AddProductComponent],
+  imports: [CommonModule, NgStyle, FormsModule, AddProductComponent, UpdateProductComponent],
   templateUrl: './stock.component.html',
   styleUrl: './stock.component.css'
 })
@@ -17,9 +19,14 @@ export class StockComponent implements OnInit{
 
   products: ProductDTO[] = [];
   filteredProducts: ProductDTO[] = [];
+  selectedProduct: Product;
   textFilter: string = '';
 
-  constructor(private stockService: ProductService){}
+  constructor(
+    private stockService: ProductService,
+    private helper: ProductHelperService){
+      this.selectedProduct = helper.createEmptyProduct();
+    }
 
   ngOnInit(): void {
     this.getProducts();
@@ -34,6 +41,14 @@ export class StockComponent implements OnInit{
       console.log('Error no se encontró ningún producto', error);
     });
   }
+
+  openModalCustomer(product: Product): void {
+      if (product) {
+        this.selectedProduct = { ...product };
+      } else {
+        console.error('No se pudo abrir modal, el producto no esta definido');
+      }
+    }
 
   statusColor(status: string): string {
     switch (status) {
