2468e32f452ad2ecd9cb25ba74fd1a5d7759226d
Juanin92 <j.claveria19@com>
2024-12-23 12:24:38 -0300

    Create product validator

---
+++ veloProweb-frontend/src/app/validation/product-validator.ts
@@ -0,0 +1,37 @@
+import { ProductDTO } from "../models/DTO/product-dto";
+import { Product } from "../models/Entity/Product/product.model";
+
+export class ProductValidator {
+    static validateForm(selectedProduct: Product): boolean {
+            return this.isFieldValid(selectedProduct, 'description') &&
+                    this.isFieldValid(selectedProduct, 'brand') &&
+                    this.isFieldValid(selectedProduct, 'category') &&
+                    this.isFieldValid(selectedProduct, 'subcategoryProduct') &&
+                    this.isFieldValid(selectedProduct, 'unit');
+        }
+    
+        static isFieldValid(product: Product, fieldName: keyof Product): boolean {
+            const value = product[fieldName];
+            if (fieldName === 'description') {
+                return value !== null && typeof value === 'string' && value.trim().length < 0;
+            }
+            if (fieldName === 'brand' || fieldName === 'category' || fieldName === 'subcategoryProduct' || fieldName === 'unit') {
+                return value !== null;
+            }
+            return true;
+        }
+
+        static validateStock(stock: number): boolean{
+            if (stock <= 0) {
+                return false;
+            }
+            return true;
+        }
+
+        static validateSalePrice(price: number): boolean{
+            if (price <= 0) {
+                return false;
+            }
+            return true;
+        }
+}
