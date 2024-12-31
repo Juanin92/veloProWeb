377efcfc4eb63fd330a162f58f79bad852c02375
Juanin92 <j.claveria19@com>
2024-12-31 13:04:13 -0300

    Create endpoint new product

--- veloProWeb/src/main/java/com/veloProWeb/Controller/Product/StockController.java
+++ veloProWeb/src/main/java/com/veloProWeb/Controller/Product/StockController.java
@@ -3,13 +3,13 @@ package com.veloProWeb.Controller.Product;
 import com.veloProWeb.Model.Entity.Product.Product;
 import com.veloProWeb.Service.Product.Interfaces.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/stock")
@@ -21,5 +21,18 @@ public class StockController {
     @GetMapping()
     public ResponseEntity<List<Product>> getListProducts(){
         return ResponseEntity.ok(productService.getAll());
+    }
+
+    @PostMapping()
+    public ResponseEntity<Map<String, String>> createProduct(@RequestBody Product product){
+        Map<String, String> response =  new HashMap<>();
+        try{
+            productService.create(product);
+            response.put("message","Producto creado exitosamente!");
+            return ResponseEntity.ok(response);
+        }catch (IllegalArgumentException e){
+            response.put("message",e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
     }
 }
