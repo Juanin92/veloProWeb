4d762f92ec982d26341d8f2f4bc6df269d789efa
Juanin92 <j.claveria19@com>
2025-01-01 17:23:00 -0300

    Change name folder customer component

---
+++ veloProweb-frontend/src/app/components/customer/test/ticket-customer.component.spec.ts
@@ -0,0 +1,23 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { TicketCustomerComponent } from '../ticket-customer/ticket-customer.component';
+
+
+// describe('TicketCustomerComponent', () => {
+//   let component: TicketCustomerComponent;
+//   let fixture: ComponentFixture<TicketCustomerComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [TicketCustomerComponent]
+//     })
+//     .compileComponents();
+
+//     fixture = TestBed.createComponent(TicketCustomerComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
