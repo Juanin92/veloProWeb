347ef8321026bde9c439c17ff9af3ff1295fdcc7
Juanin92 <j.claveria19@com>
2025-01-03 20:50:58 -0300

    Add service anotation

--- veloProWeb/src/main/java/com/veloProWeb/Service/Purchase/SupplierService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/Purchase/SupplierService.java
@@ -5,10 +5,12 @@ import com.veloProWeb.Repository.Purchase.SupplierRepo;
 import com.veloProWeb.Service.Purchase.Interfaces.ISupplierService;
 import com.veloProWeb.Validation.SupplierValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class SupplierService implements ISupplierService {
 
     @Autowired private SupplierRepo supplierRepo;
