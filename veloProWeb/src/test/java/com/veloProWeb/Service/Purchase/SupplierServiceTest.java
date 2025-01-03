5e579f1b9c5fc317a45994b21494941258746076
Juanin92 <j.claveria19@com>
2025-01-03 18:44:50 -0300

    Create supplier service test

---
+++ veloProWeb/src/test/java/com/veloProWeb/Service/Purchase/SupplierServiceTest.java
@@ -0,0 +1,61 @@
+package com.veloProWeb.Service.Purchase;
+
+import com.veloProWeb.Model.Entity.Purchase.Supplier;
+import com.veloProWeb.Repository.Purchase.SupplierRepo;
+import com.veloProWeb.Validation.SupplierValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class SupplierServiceTest {
+
+    @InjectMocks private SupplierService service;
+    @Mock private SupplierRepo repo;
+    @Mock private SupplierValidator validator;
+    private Supplier supplier;
+
+    @BeforeEach
+    void setUp(){
+        supplier = new Supplier();
+        supplier.setId(1L);
+        supplier.setName("Samsung");
+        supplier.setEmail(null);
+        supplier.setRut("12123123-7");
+        supplier.setPhone("+569 12345678");
+    }
+
+    //Prueba para crear un nuevo proveedor
+    @Test
+    public void createSupplier_valid(){
+        when(repo.findByRut(supplier.getRut())).thenReturn(Optional.empty());
+        service.createSupplier(supplier);
+        verify(validator).validate(supplier);
+        verify(repo).save(supplier);
+    }
+    @Test
+    public void createSupplier_ExistingSupplier(){
+        when(repo.findByRut(supplier.getRut())).thenReturn(Optional.of(supplier));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,() -> service.createSupplier(supplier));
+        assertEquals("Proveedor Existente: Hay un registro de este proveedor.", exception.getMessage());
+        verify(validator, never()).validate(supplier);
+        verify(repo, never()).save(supplier);
+    }
+
+    //Prueba para obtener todos los proveedores
+    @Test
+    public void getAll_valid(){
+        service.getAll();
+        verify(repo).findAll();
+    }
+}
