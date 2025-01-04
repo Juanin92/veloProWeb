2e71d7988ab05121d906b16bcafa15bf0cc981d2
Juanin92 <j.claveria19@com>
2025-01-04 13:36:54 -0300

    Add method to supplier component

--- veloProweb-frontend/src/app/components/purchase/supplier/supplier/supplier.component.ts
+++ veloProweb-frontend/src/app/components/purchase/supplier/supplier/supplier.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SupplierService } from '../../../../services/Purchase/supplier.service';
+import { Supplier } from '../../../../models/Entity/Purchase/supplier';
 
 @Component({
   selector: 'app-supplier',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './supplier.component.html',
   styleUrl: './supplier.component.css'
 })
-export class SupplierComponent {
+export class SupplierComponent implements OnInit{
 
+  suppliers: Supplier[] = [];
+
+  constructor(
+    private supplierService: SupplierService,
+  ){}
+
+  ngOnInit(): void {
+    this.getSuppliers();
+  }
+
+  getSuppliers(): void{
+    this.supplierService.getSuppliers().subscribe( (supplierList) =>{
+      this.suppliers = supplierList;
+    }, (error) =>{
+      console.log('Error no se encontró ningún proveedor', error);
+    });
+  }
 }
