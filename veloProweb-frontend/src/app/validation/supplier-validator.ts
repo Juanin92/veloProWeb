6b33bd27bc23aebcc2d90371adbb0e85ca462fef
Juanin92 <j.claveria19@com>
2025-01-04 16:58:54 -0300

    Create supplier validation

---
+++ veloProweb-frontend/src/app/validation/supplier-validator.ts
@@ -0,0 +1,27 @@
+import { Supplier } from "../models/Entity/Purchase/supplier";
+
+export class SupplierValidator {
+    static validateForm(selectedSupplier: Supplier): boolean{
+        return this.isFieldValid(selectedSupplier, 'name') &&
+            this.isFieldValid(selectedSupplier, 'rut') &&
+            this.isFieldValid(selectedSupplier, 'phone') &&
+            this.isFieldValid(selectedSupplier, 'email');
+    }
+
+    static isFieldValid(supplier: Supplier, fieldName: keyof Supplier): boolean{
+        const value = supplier[fieldName];
+        if (fieldName === 'name') {
+            return value !== null && typeof value === 'string' && value.trim().length > 3;
+        }
+        if (fieldName === 'rut') {
+            return value !== null && typeof value === 'string' && /^\d{7,8}-\d|[kK]$$/.test(value);
+        }
+        if (fieldName === 'phone') {
+            return value !== null && typeof value === 'string' && value.trim().length == 13;
+        }
+        if (fieldName === 'email') {
+            return !value || typeof value === 'string' && /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
+        }
+        return true;
+    }
+}
