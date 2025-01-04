50c1021f208d740b2c7c31cc5bc7c3d2656a59d9
Juanin92 <j.claveria19@com>
2025-01-04 17:37:18 -0300

    Add create method to supplier component

--- veloProweb-frontend/src/app/components/purchase/supplier/supplier/supplier.component.ts
+++ veloProweb-frontend/src/app/components/purchase/supplier/supplier/supplier.component.ts
@@ -2,26 +2,52 @@ import { Component, OnInit } from '@angular/core';
 import { SupplierService } from '../../../../services/Purchase/supplier.service';
 import { Supplier } from '../../../../models/Entity/Purchase/supplier';
 import { CommonModule } from '@angular/common';
+import { SupplierValidator } from '../../../../validation/supplier-validator';
+import { FormsModule } from '@angular/forms';
+import { NotificationService } from '../../../../utils/notification-service.service';
 
 @Component({
   selector: 'app-supplier',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './supplier.component.html',
   styleUrl: './supplier.component.css'
 })
 export class SupplierComponent implements OnInit{
 
   suppliers: Supplier[] = [];
+  newSupplier: Supplier;
   selectedSupplier: Supplier | null = null;
   status: boolean = false;
+  validator = SupplierValidator;
 
   constructor(
     private supplierService: SupplierService,
-  ){}
+    private notification: NotificationService){
+      this.newSupplier = this.initializeSupplier();
+    }
 
   ngOnInit(): void {
     this.getSuppliers();
+  }
+
+  createSupplier(): void{
+    if (this.validator.validateForm(this.newSupplier)) {
+      this.supplierService.createSupplier(this.newSupplier).subscribe((response) =>{
+        console.log('Proveedor agregado exitosamente:', response);
+        this.notification.showSuccessToast(`¡${this.newSupplier.name} fue agregado exitosamente!`, 'top', 3000);
+          this.initializeSupplier(); // Reinicia la variable del cliente vacío.
+          setTimeout(() => {
+            window.location.reload();
+          }, 3000);
+      }, (error) =>{
+        const message = error.error.error;
+        console.error('Error al agregar el proveedor:', error);
+        this.notification.showErrorToast(`Error al agregar proveedor \n${message}`, 'top', 5000);
+      })
+    }else {
+      this.notification.showWarning('Formulario incompleto', 'Por favor, complete correctamente todos los campos obligatorios.');
+    }
   }
 
   getSuppliers(): void{
@@ -34,10 +60,22 @@ export class SupplierComponent implements OnInit{
 
   getSelectedSupplier(supplier: Supplier): void{
     this.selectedSupplier = supplier;
+    this.status = false;
   }
 
   openFormat(statusClick: boolean): void{
     this.status = statusClick;
     this.selectedSupplier = null;
+    this.newSupplier = this.initializeSupplier();
+  }
+
+  initializeSupplier(): Supplier{
+    return this.newSupplier = {
+      id: 0,
+      name: '',
+      email: '',
+      rut: '',
+      phone: '+569 '
+    }
   }
 }
