57cc6b99094aac085bcff61242e386679407ae78
Juanin92 <j.claveria19@com>
2025-01-03 21:19:57 -0300

    Add supplier route

--- veloProweb-frontend/src/app/app.routes.ts
+++ veloProweb-frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import { CustomerComponent } from './components/customerView/customer.component';
 import { StockComponent } from './components/product/stock/stock.component';
+import { SupplierComponent } from './components/purchase/supplier/supplier/supplier.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
 export const routes: Routes = [
     {path: 'clientes', component: CustomerComponent},
-    {path: 'stock', component: StockComponent}
+    {path: 'stock', component: StockComponent},
+    {path: 'proveedores', component: SupplierComponent}
 ];
