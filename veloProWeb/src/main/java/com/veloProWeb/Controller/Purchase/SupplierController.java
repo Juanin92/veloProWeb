27e84e3fadc0fec7e0a5fc5a9721966e6656fd4e
Juanin92 <j.claveria19@com>
2025-01-05 18:17:36 -0300

    Add controller supplier documentation

--- veloProWeb/src/main/java/com/veloProWeb/Controller/Purchase/SupplierController.java
+++ veloProWeb/src/main/java/com/veloProWeb/Controller/Purchase/SupplierController.java
@@ -11,6 +11,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Controlador REST para gestionar operaciones relacionadas con proveedores.
+ * Este controlador proporciona endpoints para listar, agregar, actualizar proveedores.
+ */
 @RestController
 @RequestMapping("/proveedores")
 @CrossOrigin(origins = "http://localhost:4200")
@@ -18,11 +22,20 @@ public class SupplierController {
 
     @Autowired private ISupplierService supplierService;
 
+    /**
+     * Obtiene una lista de proveedores.
+     * @return - ResponseEntity con una lista de proveedores
+     */
     @GetMapping
     public ResponseEntity<List<Supplier>> getListSupplier(){
         return ResponseEntity.ok(supplierService.getAll());
     }
 
+    /**
+     * Agrega un nuevo proveedor
+     * @param supplier - Proveedor con los datos que se desea agregar
+     * @return - ResponseEntity con un mensaje de éxito o error según sea el caso
+     */
     @PostMapping
     public ResponseEntity<Map<String, String>> createSupplier(@RequestBody Supplier supplier){
         Map<String, String> response = new HashMap<>();
@@ -36,6 +49,11 @@ public class SupplierController {
         }
     }
 
+    /**
+     * Actualiza los datos de un proveedor
+     * @param supplier - Proveedor con los datos que se desea actualizar
+     * @return - ResponseEntity con un mensaje de éxito o error según sea el caso
+     */
     @PutMapping
     public ResponseEntity<Map<String, String>> updateSupplier(@RequestBody Supplier supplier){
         Map<String, String> response = new HashMap<>();
