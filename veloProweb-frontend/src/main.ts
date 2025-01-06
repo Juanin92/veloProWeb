708ccfd8363d3721811f6d28bf2398a74b88df92
Juanin92 <j.claveria19@com>
2025-01-06 16:32:24 -0300

    Configure currency pipe

--- veloProweb-frontend/src/main.ts
+++ veloProweb-frontend/src/main.ts
@@ -2,12 +2,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeEsCl from '@angular/common/locales/es-CL';
+
+registerLocaleData(localeEsCl, 'es-CL');
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     provideHttpClient(),
-  ...appConfig.providers
+  ...appConfig.providers,
+    {provide: LOCALE_ID, useValue: 'es-CL'},
   ]
 })
   .catch((err) => console.error(err));
