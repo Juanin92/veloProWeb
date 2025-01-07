4965db6a4aae6149290a1132488fad0e0934bba9
Juanin92 <j.claveria19@com>
2025-01-07 20:16:02 -0300

    Add endpoints purchase

--- veloProWeb/src/main/java/com/veloProWeb/Controller/Purchase/PurchaseController.java
+++ veloProWeb/src/main/java/com/veloProWeb/Controller/Purchase/PurchaseController.java
@@ -1,12 +1,12 @@
 package com.veloProWeb.Controller.Purchase;
 
+import com.veloProWeb.Model.DTO.PurchaseRequestDTO;
+import com.veloProWeb.Model.Entity.Purchase.Purchase;
 import com.veloProWeb.Service.Purchase.Interfaces.IPurchaseDetailService;
 import com.veloProWeb.Service.Purchase.Interfaces.IPurchaseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/compras")
@@ -16,5 +16,31 @@ public class PurchaseController {
     @Autowired private IPurchaseService purchaseService;
     @Autowired private IPurchaseDetailService purchaseDetailService;
 
+    @PostMapping("/crear")
+    public ResponseEntity<Purchase> createPurchase(@RequestBody PurchaseRequestDTO dto){
+        try {
+            Purchase purchase = purchaseService.createPurchase(dto.getDate(),dto.getSupplier(), dto.getDocumentType(), dto.getDocument(), dto.getTax(), dto.getTotal());
+            return ResponseEntity.ok(purchase);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
 
+    @GetMapping("/total_compras")
+    public ResponseEntity<Long> getTotalPurchase(){
+        try{
+            return ResponseEntity.ok(purchaseService.totalPurchase());
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/total_Precio_Compras")
+    public ResponseEntity<Long> getTotalPricePurchase(){
+        try{
+            return ResponseEntity.ok(purchaseService.totalPricePurchase());
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
