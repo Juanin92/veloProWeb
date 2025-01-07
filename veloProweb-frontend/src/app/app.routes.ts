f388663873f8b0d4fff2f9f136a87e863f8968ca
Juanin92 <j.claveria19@com>
2025-01-07 20:29:12 -0300

    Add purchaseComponents to routes

--- veloProweb-frontend/src/app/app.routes.ts
+++ veloProweb-frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { StockComponent } from './components/product/stock/stock.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { SupplierComponent } from './components/purchase/supplier/supplier.component';
+import { PurchaseComponent } from './components/purchase/purchase/purchase.component';
 
 export const routes: Routes = [
     {path: 'clientes', component: CustomerComponent},
     {path: 'stock', component: StockComponent},
-    {path: 'proveedores', component: SupplierComponent}
+    {path: 'proveedores', component: SupplierComponent},
+    {path: 'compras', component: PurchaseComponent}
 ];
