195d6bdf2638bba13bed57a82d5aa2c652343f74
Juanin92 <j.claveria19@com>
2025-01-07 20:26:57 -0300

    fix path directory

--- veloProweb-frontend/src/app/app.routes.ts
+++ veloProweb-frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { StockComponent } from './components/product/stock/stock.component';
-import { SupplierComponent } from './components/purchase/supplier/supplier/supplier.component';
 import { CustomerComponent } from './components/customer/customer.component';
+import { SupplierComponent } from './components/purchase/supplier/supplier.component';
 
 export const routes: Routes = [
     {path: 'clientes', component: CustomerComponent},
