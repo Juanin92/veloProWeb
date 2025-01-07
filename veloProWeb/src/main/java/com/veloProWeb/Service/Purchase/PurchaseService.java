557ef222f104c5a4683b9a0cc928221102ac59e3
Juanin92 <j.claveria19@com>
2025-01-07 15:50:16 -0300

    Add purchase service methods

--- veloProWeb/src/main/java/com/veloProWeb/Service/Purchase/PurchaseService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/Purchase/PurchaseService.java
@@ -4,6 +4,7 @@ import com.veloProWeb.Model.Entity.Purchase.Purchase;
 import com.veloProWeb.Model.Entity.Purchase.Supplier;
 import com.veloProWeb.Repository.Purchase.PurchaseRepo;
 import com.veloProWeb.Service.Purchase.Interfaces.IPurchaseService;
+import com.veloProWeb.Validation.PurchaseValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +14,24 @@ import java.time.LocalDate;
 public class PurchaseService implements IPurchaseService {
 
     @Autowired private PurchaseRepo purchaseRepo;
+    @Autowired private PurchaseValidator validator;
 
     @Override
     public Purchase save(LocalDate date, Supplier supplier, String documentType, String document, int tax, int total) {
-        return null;
+        Purchase purchase =  new Purchase();
+        purchase.setDate(date);
+        purchase.setSupplier(supplier);
+        purchase.setDocumentType(documentType);
+        purchase.setDocument(document);
+        purchase.setIva(tax);
+        purchase.setPurchaseTotal(total);
+        validator.validate(purchase);
+        purchaseRepo.save(purchase);
+        return purchase;
     }
 
     @Override
     public Long totalPurchase() {
-        return 0L;
+        return purchaseRepo.count();
     }
 }
