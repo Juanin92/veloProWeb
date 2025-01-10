c433322dafb75663b8db9b69165b009c005ddeb0
Juanin92 <j.claveria19@com>
2025-01-10 17:41:22 -0300

    Modify createPurchase()

--- veloProWeb/src/main/java/com/veloProWeb/Service/Purchase/Interfaces/IPurchaseService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/Purchase/Interfaces/IPurchaseService.java
@@ -1,14 +1,13 @@
 package com.veloProWeb.Service.Purchase.Interfaces;
 
 import com.veloProWeb.Model.DTO.DetailPurchaseDTO;
+import com.veloProWeb.Model.DTO.PurchaseRequestDTO;
 import com.veloProWeb.Model.Entity.Purchase.Purchase;
-import com.veloProWeb.Model.Entity.Purchase.Supplier;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface IPurchaseService {
-    Purchase createPurchase(LocalDate date, Supplier supplier, String documentType, String document, int tax, int total);
+    Purchase createPurchase(PurchaseRequestDTO dto);
     Long totalPurchase();
     int totalPricePurchase(List<DetailPurchaseDTO> dto);
 }
--- veloProWeb/src/main/java/com/veloProWeb/Service/Purchase/PurchaseService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/Purchase/PurchaseService.java
@@ -1,9 +1,11 @@
 package com.veloProWeb.Service.Purchase;
 
 import com.veloProWeb.Model.DTO.DetailPurchaseDTO;
+import com.veloProWeb.Model.DTO.PurchaseRequestDTO;
 import com.veloProWeb.Model.Entity.Purchase.Purchase;
 import com.veloProWeb.Model.Entity.Purchase.Supplier;
 import com.veloProWeb.Repository.Purchase.PurchaseRepo;
+import com.veloProWeb.Repository.Purchase.SupplierRepo;
 import com.veloProWeb.Service.Purchase.Interfaces.IPurchaseService;
 import com.veloProWeb.Validation.PurchaseValidator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,34 +13,32 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PurchaseService implements IPurchaseService {
 
     @Autowired private PurchaseRepo purchaseRepo;
+    @Autowired private SupplierRepo supplierRepo;
     @Autowired private PurchaseValidator validator;
 
     /**
      * Crea una nueva compra.
      * Inicializa y registra la compra en la BD
-     * @param date - Fecha de la compra
-     * @param supplier - Proveedor al que se realiza la compra.
-     * @param documentType - Tipo de documento (Ej: boleta o factura)
-     * @param document - Número de documento de la compra
-     * @param tax - Impuesto aplicado a la compra
-     * @param total - Total del monto de la compra
+     * @param dto -
      * @return - Compra creada con todos sus valores registrados
      */
     @Override
-    public Purchase createPurchase(LocalDate date, Supplier supplier, String documentType, String document, int tax, int total) {
+    public Purchase createPurchase(PurchaseRequestDTO dto) {
         Purchase purchase =  new Purchase();
         purchase.setId(null);
-        purchase.setDate(date);
-        purchase.setSupplier(supplier);
-        purchase.setDocumentType(documentType);
-        purchase.setDocument(document);
-        purchase.setIva(tax);
-        purchase.setPurchaseTotal(total);
+        purchase.setDocument(dto.getDocument());
+        purchase.setDocumentType(dto.getDocumentType());
+        purchase.setIva(dto.getTax());
+        purchase.setPurchaseTotal(dto.getTotal());
+        purchase.setDate(dto.getDate());
+        Optional<Supplier> supplier = supplierRepo.findById(dto.getIdSupplier());
+        purchase.setSupplier(supplier.orElseThrow(() -> new IllegalArgumentException("No ha seleccionado un proveedor")));
         validator.validate(purchase);
         purchaseRepo.save(purchase);
         return purchase;
