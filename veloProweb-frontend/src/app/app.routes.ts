42cf0b30dfe02eb8f9741217d3beedda2db94c91
Juanin92 <j.claveria19@com>
2025-01-11 19:11:35 -0300

    Add report component to app.route and adding route in menu template

--- veloProweb-frontend/src/app/app.routes.ts
+++ veloProweb-frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { StockComponent } from './components/product/stock/stock.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { SupplierComponent } from './components/purchase/supplier/supplier.component';
 import { PurchaseComponent } from './components/purchase/purchase/purchase.component';
+import { ReportComponent } from './components/Report/report/report.component';
 
 export const routes: Routes = [
     {path: 'clientes', component: CustomerComponent},
     {path: 'stock', component: StockComponent},
     {path: 'proveedores', component: SupplierComponent},
-    {path: 'compras', component: PurchaseComponent}
+    {path: 'compras', component: PurchaseComponent},
+    {path: 'reportes', component: ReportComponent}
 ];
