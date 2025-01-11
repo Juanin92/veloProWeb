dd13ae39f7fccb83c00be4bb199f6d612bcb55cf
Juanin92 <j.claveria19@com>
2025-01-10 22:56:18 -0300

    Modify createPurchase test

--- veloProWeb/src/test/java/com/veloProWeb/Service/Purchase/PurchaseServiceTest.java
+++ veloProWeb/src/test/java/com/veloProWeb/Service/Purchase/PurchaseServiceTest.java
@@ -1,55 +1,75 @@
 package com.veloProWeb.Service.Purchase;
 
-import com.veloProWeb.Model.DTO.DetailPurchaseDTO;
+import com.veloProWeb.Model.DTO.PurchaseRequestDTO;
 import com.veloProWeb.Model.Entity.Purchase.Purchase;
 import com.veloProWeb.Model.Entity.Purchase.Supplier;
 import com.veloProWeb.Repository.Purchase.PurchaseRepo;
+import com.veloProWeb.Repository.Purchase.SupplierRepo;
 import com.veloProWeb.Validation.PurchaseValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PurchaseServiceTest {
 
     @InjectMocks private PurchaseService purchaseService;
     @Mock private PurchaseRepo purchaseRepo;
+    @Mock private SupplierRepo supplierRepo;
     @Mock private PurchaseValidator validator;
-    @Mock private Supplier supplier;
-    private Purchase purchase;
+    private PurchaseRequestDTO purchaseDTO;
+    private Supplier supplier;
 
     @BeforeEach
     void setUp(){
-        purchase = new Purchase();
-        purchase.setId(1L);
-        purchase.setDocument("A001");
-        purchase.setDocumentType("Boleta");
-        purchase.setIva(1500);
-        purchase.setPurchaseTotal(20000);
-        purchase.setDate(LocalDate.now());
-        purchase.setSupplier(supplier);
-        purchase.setPurchaseDetails(new ArrayList<>());
+        purchaseDTO = new PurchaseRequestDTO();
+        purchaseDTO.setId(1L);
+        purchaseDTO.setDate(LocalDate.now());
+        purchaseDTO.setIdSupplier(2L);
+        purchaseDTO.setDocument("A001");
+        purchaseDTO.setDocumentType("Boleta");
+        purchaseDTO.setTax(1500);
+        purchaseDTO.setTotal(20000);
+        purchaseDTO.setDetailList(new ArrayList<>());
+
+        supplier =  new Supplier();
+        supplier.setId(3L);
     }
 
     //Prueba para crear una nueva compra
     @Test
-    public void save_valid(){
-        purchase.setId(null);
-        purchaseService.createPurchase(LocalDate.now(),supplier,"Boleta","A001",1500, 20000);
-        verify(validator).validate(purchase);
-        verify(purchaseRepo).save(purchase);
+    public void createPurchase_valid() {
+        when(supplierRepo.findById(purchaseDTO.getIdSupplier())).thenReturn(Optional.of(supplier));
+        Purchase createdPurchase = purchaseService.createPurchase(purchaseDTO);
+        ArgumentCaptor<Purchase> purchaseCaptor = ArgumentCaptor.forClass(Purchase.class);
+        verify(purchaseRepo).save(purchaseCaptor.capture());
+        Purchase savedPurchase = purchaseCaptor.getValue();
+        assertEquals("A001", savedPurchase.getDocument());
+        assertEquals(3L, savedPurchase.getSupplier().getId());
+        verify(supplierRepo).findById(purchaseDTO.getIdSupplier());
+        verify(validator).validate(savedPurchase);
+    }
+    @Test
+    public void createPurchase_supplierNotFound() {
+        when(supplierRepo.findById(purchaseDTO.getIdSupplier())).thenReturn(Optional.empty());
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,() -> purchaseService.createPurchase(purchaseDTO));
+
+        assertEquals("No ha seleccionado un proveedor", exception.getMessage());
+        verify(supplierRepo).findById(purchaseDTO.getIdSupplier());
+        verifyNoInteractions(purchaseRepo);
     }
 
     //Prueba para obtener el total de compras realizadas
@@ -59,30 +79,5 @@ public class PurchaseServiceTest {
         Long totalPurchase =  purchaseService.totalPurchase();
         verify(purchaseRepo).count();
         assertEquals(1L, totalPurchase);
-    }
-
-    //Prueba para obtener el total monetario de una lista de DTO
-    @Test
-    public void totalPricePurchase_valid(){
-        DetailPurchaseDTO dto = new DetailPurchaseDTO();
-        dto.setIdProduct(1L);
-        dto.setBrand("Samsung");
-        dto.setDescription("phone");
-        dto.setPrice(25000);
-        dto.setTax(2500);
-        dto.setQuantity(1);
-        dto.setTotal(27500);
-
-        DetailPurchaseDTO dto2 = new DetailPurchaseDTO();
-        dto2.setIdProduct(1L);
-        dto2.setBrand("Samsung");
-        dto2.setDescription("tv");
-        dto2.setPrice(250000);
-        dto2.setTax(25000);
-        dto2.setQuantity(1);
-        dto2.setTotal(275000);
-        List<DetailPurchaseDTO> dtoList = Arrays.asList(dto,dto2);
-        int total = purchaseService.totalPricePurchase(dtoList);
-        assertEquals(302500, total);
     }
 }
