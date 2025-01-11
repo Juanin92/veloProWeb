a41872dc3b695c665b822b408d170d3af2d7030a
Juanin92 <j.claveria19@com>
2025-01-10 22:12:42 -0300

    Modify createDetailurchase(), delete deleteProduct method

--- veloProWeb/src/main/java/com/veloProWeb/Service/Purchase/Interfaces/IPurchaseDetailService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/Purchase/Interfaces/IPurchaseDetailService.java
@@ -1,15 +1,12 @@
 package com.veloProWeb.Service.Purchase.Interfaces;
 
 import com.veloProWeb.Model.DTO.DetailPurchaseDTO;
-import com.veloProWeb.Model.Entity.Product.Product;
 import com.veloProWeb.Model.Entity.Purchase.Purchase;
 import com.veloProWeb.Model.Entity.Purchase.PurchaseDetail;
 
 import java.util.List;
 
 public interface IPurchaseDetailService {
-    void createDetailPurchase(DetailPurchaseDTO dto, Purchase purchase, Product product);
+    void createDetailPurchase(List<DetailPurchaseDTO> dtoList, Purchase purchase);
     List<PurchaseDetail> getAll();
-    DetailPurchaseDTO createDTO(Product product);
-    boolean deleteProduct(Long id, List<DetailPurchaseDTO> dto);
 }
--- veloProWeb/src/main/java/com/veloProWeb/Service/Purchase/PurchaseDetailService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/Purchase/PurchaseDetailService.java
@@ -5,36 +5,34 @@ import com.veloProWeb.Model.Entity.Product.Product;
 import com.veloProWeb.Model.Entity.Purchase.Purchase;
 import com.veloProWeb.Model.Entity.Purchase.PurchaseDetail;
 import com.veloProWeb.Repository.Purchase.PurchaseDetailRepo;
+import com.veloProWeb.Service.Product.Interfaces.IProductService;
 import com.veloProWeb.Service.Purchase.Interfaces.IPurchaseDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @Service
 public class PurchaseDetailService implements IPurchaseDetailService {
 
     @Autowired private PurchaseDetailRepo purchaseDetailRepo;
+    @Autowired private IProductService productService;
 
-    /**
-     * Crea un nuevo detalle de compra.
-     * Inicializa y registra el detalle de compra en la BD
-     * @param dto - DTO que contiene los detalles de la compra
-     * @param purchase - Compra relacionada con el detalle
-     * @param product - Producto relacionado con la compra
-     */
     @Override
-    public void createDetailPurchase(DetailPurchaseDTO dto, Purchase purchase, Product product) {
-        PurchaseDetail purchaseDetail =  new PurchaseDetail();
-        purchaseDetail.setPrice(dto.getPrice());
-        purchaseDetail.setQuantity(dto.getQuantity());
-        purchaseDetail.setTax(dto.getTax());
-        purchaseDetail.setTotal(dto.getTotal());
-        purchaseDetail.setProduct(product);
-        purchaseDetail.setPurchase(purchase);
-        purchaseDetailRepo.save(purchaseDetail);
+    public void createDetailPurchase(List<DetailPurchaseDTO> dtoList, Purchase purchase) {
+        for (DetailPurchaseDTO dto : dtoList){
+            PurchaseDetail purchaseDetail =  new PurchaseDetail();
+            purchaseDetail.setId(null);
+            purchaseDetail.setPrice(dto.getPrice());
+            purchaseDetail.setQuantity(dto.getQuantity());
+            purchaseDetail.setTax(dto.getTax());
+            purchaseDetail.setTotal(dto.getTotal());
+            Product product = productService.getProductById(dto.getIdProduct());
+            purchaseDetail.setProduct(product);
+            purchaseDetail.setPurchase(purchase);
+            purchaseDetailRepo.save(purchaseDetail);
+            productService.updateStockPurchase(product, purchaseDetail.getPrice(), purchaseDetail.getQuantity());
+        }
     }
 
     /**
@@ -44,46 +42,5 @@ public class PurchaseDetailService implements IPurchaseDetailService {
     @Override
     public List<PurchaseDetail> getAll() {
         return purchaseDetailRepo.findAll();
-    }
-
-    /**
-     * Crear un DTO con los detalles necesarios de un producto
-     * @param product - Producto relacionado con el detalle de compra
-     * @return - Retorna un objeto DTO con valores de producto o nulo sea el caso
-     */
-    @Override
-    public DetailPurchaseDTO createDTO(Product product) {
-        if (product != null) {
-            DetailPurchaseDTO dto =new DetailPurchaseDTO();
-            dto.setIdProduct(product.getId());
-            dto.setBrand(product.getBrand().getName());
-            dto.setDescription(product.getDescription());
-            dto.setPrice(0);
-            dto.setTax(0);
-            dto.setTotal(0);
-            dto.setQuantity(0);
-            return dto;
-        }
-        return null;
-    }
-
-    /**
-     * Elimina un producto de la lista de detalles de compras
-     * Busca un producto en la lista de detalles de compra por su ID y lo elimina si lo encuentra.
-     * @param id - Identificador del producto a borrar
-     * @param dtoList - lista de detalle de compra donde buscar
-     * @return - True si el producto fue encontrado y eliminado, false en caso contrario.
-     */
-    @Override
-    public boolean deleteProduct(Long id, List<DetailPurchaseDTO> dtoList) {
-        Optional<DetailPurchaseDTO> optionalDto = dtoList.stream()
-                .filter(dto -> Objects.equals(dto.getIdProduct(), id))
-                .findFirst();
-
-        if (optionalDto.isPresent()) {
-            dtoList.remove(optionalDto.get());
-            return true;
-        }
-        return false;
     }
 }
