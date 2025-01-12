58fd7bd70272dbe61b7c7894cdfe18e092c67f0f
Juanin92 <j.claveria19@com>
2025-01-12 12:48:15 -0300

    Add methods

--- veloProWeb/src/main/java/com/veloProWeb/Service/Report/ReportService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/Report/ReportService.java
@@ -8,7 +8,10 @@ import com.veloProWeb.Repository.ReportRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.sql.Date;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,21 +21,77 @@ public class ReportService implements IReportService{
 
     @Override
     public List<DailySaleCountDTO> getDailySale(LocalDate start, LocalDate end) {
-        return List.of();
+        try{
+            if (end.isAfter(start)) {
+                throw new IllegalArgumentException("La fecha de inicio no puede ser posterior a la fecha de fin.");
+            }else {
+                Date startDate = Date.valueOf(start);
+                Date endDate = Date.valueOf(end);
+                List<Object[]> results = reportRepo.findSalesByDateRange(endDate, startDate);
+                List<DailySaleCountDTO> dtoList = new ArrayList<>();
+                for (Object[] result : results){
+                    Date date = (Date) result[0];
+                    Long count = (Long) result[1];
+                    dtoList.add(new DailySaleCountDTO(date.toLocalDate(), count));
+                }
+                return dtoList;
+            }
+        }catch (Exception e){
+            throw new IllegalArgumentException("Debe ingresar fechas válidas en los campos");
+        }
     }
 
     @Override
     public List<DailySaleSumDTO> getTotalSaleDaily(LocalDate start, LocalDate end) {
-        return List.of();
+        try{
+            Date startDate = Date.valueOf(start);
+            Date endDate = Date.valueOf(end);
+            List<Object[]> results = reportRepo.findTotalSalesByDateRange(endDate, startDate);
+            List<DailySaleSumDTO> dtoList = new ArrayList<>();
+            for (Object[] result : results) {
+                Date date = (Date) result[0];
+                BigDecimal sum = (BigDecimal) result[1];
+                dtoList.add(new DailySaleSumDTO(date.toLocalDate(), sum));
+            }
+            return dtoList;
+        }catch (Exception e){
+            throw new IllegalArgumentException("Debe ingresar fechas válidas en los campos");
+        }
     }
 
     @Override
     public List<DailySaleAvgDTO> getAverageTotalSaleDaily(LocalDate start, LocalDate end) {
-        return List.of();
+        try{
+            Date startDate = Date.valueOf(start);
+            Date endDate = Date.valueOf(end);
+            List<Object[]> results = reportRepo.findAverageSalesPerDay(endDate, startDate);
+            List<DailySaleAvgDTO> dtoList = new ArrayList<>();
+            for (Object[] result : results) {
+                Date date = (Date) result[0];
+                BigDecimal avg = (BigDecimal) result[1];
+                dtoList.add(new DailySaleAvgDTO(date.toLocalDate(), avg));
+            }
+            return dtoList;
+        }catch (Exception e){
+            throw new IllegalArgumentException("Debe ingresar fechas válidas en los campos");
+        }
     }
 
     @Override
     public List<DailySaleEarningDTO> getEarningSale(LocalDate start, LocalDate end) {
-        return List.of();
+        try{
+            Date startDate = Date.valueOf(start);
+            Date endDate = Date.valueOf(end);
+            List<Object[]> results = reportRepo.findEarningPerDay(endDate, startDate);
+            List<DailySaleEarningDTO> dtoList = new ArrayList<>();
+            for (Object[] result : results) {
+                Date date = (Date) result[0];
+                BigDecimal profit = (BigDecimal) result[1];
+                dtoList.add(new DailySaleEarningDTO(date.toLocalDate(), profit));
+            }
+            return dtoList;
+        }catch (Exception e){
+            throw new IllegalArgumentException("Debe ingresar fechas válidas en los campos");
+        }
     }
 }
