53529c5a021f80eda3fd802fe03d8d2bc02f08bf
Juanin92 <j.claveria19@com>
2025-01-13 18:47:26 -0300

    Create PaymentRequestDTO dto class

---
+++ veloProWeb/src/main/java/com/veloProWeb/Model/DTO/PaymentRequestDTO.java
@@ -0,0 +1,14 @@
+package com.veloProWeb.Model.DTO;
+
+import com.veloProWeb.Model.Entity.Customer.Customer;
+import com.veloProWeb.Model.Entity.Customer.TicketHistory;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class PaymentRequestDTO {
+    private List<TicketHistory> tickets;
+    private Customer customer;
+    private int amount;
+}
