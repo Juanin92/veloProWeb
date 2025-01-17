ebd6924b404eb62cde724b7573e41a7a5af322b9
Juanin92 <j.claveria19@com>
2025-01-16 21:59:31 -0300

    Change folders productList

--- veloProweb-frontend/src/app/components/product/Test/product-list.component.spec.ts
+++ veloProweb-frontend/src/app/components/product/Test/product-list.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductListComponent } from '../productList/product-list/product-list.component';
+import { ProductListComponent } from '../productList/product-list.component';
 
 describe('ProductListComponent', () => {
   let component: ProductListComponent;
