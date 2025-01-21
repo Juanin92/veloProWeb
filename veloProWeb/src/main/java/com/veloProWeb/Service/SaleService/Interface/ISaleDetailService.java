1eeacb92445cf6162a218323fd6f1650d79fe56a
Juanin92 <j.claveria19@com>
2025-01-21 18:48:03 -0300

    Delete ISaleDetailService methods,Modify parameters from createSaleDetails method

--- veloProWeb/src/main/java/com/veloProWeb/Service/SaleService/Interface/ISaleDetailService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/SaleService/Interface/ISaleDetailService.java
@@ -1,16 +1,12 @@
 package com.veloProWeb.Service.SaleService.Interface;
 
 import com.veloProWeb.Model.DTO.DetailSaleDTO;
-import com.veloProWeb.Model.Entity.Product.Product;
 import com.veloProWeb.Model.Entity.Sale.Sale;
 import com.veloProWeb.Model.Entity.Sale.SaleDetail;
 
 import java.util.List;
 
 public interface ISaleDetailService {
-    void createSaleDetails(DetailSaleDTO dto, Sale sale, Product product);
+    void createSaleDetails(List<DetailSaleDTO> dto, Sale sale);
     List<SaleDetail> getAll();
-    DetailSaleDTO createDTO(Product product);
-    int deleteProduct(List<DetailSaleDTO> dtoList, Long id, int total);
-    List<DetailSaleDTO> findDetailSaleBySaleId(Long id);
 }
