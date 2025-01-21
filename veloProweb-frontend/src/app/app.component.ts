956059b2f0896ef8e2a7ef5c87c6408e92cacfb0
Juanin92 <j.claveria19@com>
2025-01-20 21:05:36 -0300

    Adding getDate method and text in app template

--- veloProweb-frontend/src/app/app.component.ts
+++ veloProweb-frontend/src/app/app.component.ts
@@ -1,20 +1,33 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./components/menu/menu.component";
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, MenuComponent],
+  providers: [DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent{
+  
   title = 'veloProweb-frontend';
-
   isMenuActive: boolean = true;
+  currentDate: string = '';
+
+  constructor(private datePipe: DatePipe){
+    this.getDate();
+  }
 
   toggleMenu(): void {
     this.isMenuActive = !this.isMenuActive;
+  }
+
+  getDate(): void{
+    const now = new Date();
+    const formatDate = this.datePipe.transform(now, 'dd-MM-yyyy  HH:mm');
+    this.currentDate = formatDate ? formatDate : '';
   }
 }
