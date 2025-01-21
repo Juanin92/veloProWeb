66f94cedf1fe429a2172dbed992fe0f926e81281
Juanin92 <j.claveria19@com>
2025-01-21 18:46:59 -0300

    Delete DetailSaleDTO variables, adding idProduct variable

--- veloProWeb/src/main/java/com/veloProWeb/Model/DTO/DetailSaleDTO.java
+++ veloProWeb/src/main/java/com/veloProWeb/Model/DTO/DetailSaleDTO.java
@@ -5,12 +5,6 @@ import lombok.Data;
 @Data
 public class DetailSaleDTO {
     private Long id;
-    private String description;
-    private String category;
-    private String unit;
-    private int stock;
-    private int salePrice;
-    private int tax;
-    private int total;
+    private Long idProduct;
     private int quantity;
 }
