005388a75215beb8261d63c5f50ea1a83ef1eb48
Juanin92 <j.claveria19@com>
2025-01-23 21:04:30 -0300

    Adding getSalesList, getDocumentAndCommentSale method to saleReport component

--- veloProweb-frontend/src/app/components/Report/Sale/sale-report.component.ts
+++ veloProweb-frontend/src/app/components/Report/Sale/sale-report.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Sale } from '../../../models/Entity/Sale/sale';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SaleService } from '../../../services/Sale/sale.service';
+import { SaleRequestDTO } from '../../../models/DTO/sale-request-dto';
 
 @Component({
   selector: 'app-sale-report',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './sale-report.component.html',
   styleUrl: './sale-report.component.css'
 })
-export class SaleReportComponent {
+export class SaleReportComponent implements OnInit{
 
+  saleReportList: SaleRequestDTO[] = [];
+  saleList: Sale[] = [];
+
+  constructor(
+    private saleService: SaleService
+  ){}
+
+  ngOnInit(): void {
+    this.getSalesList();
+  }
+
+  getSalesList(): void{
+    this.saleService.getAllSales().subscribe((List) =>{
+      this.saleReportList = List;
+      this.getDocumentAndCommentSale(this.saleReportList);
+    });
+  }
+
+  getDocumentAndCommentSale(list: SaleRequestDTO[]): void{
+    list.forEach(dto => {
+      const documentMatch = dto.comment.match(/# (BO_\d+)/);
+      const documentValue = documentMatch ? documentMatch[1] : null;
+
+      const sale: Sale = {
+        id: dto.id,
+        date: dto.date,
+        paymentMethod: dto.paymentMethod,
+        document: documentValue ? documentValue : '',
+        comment: dto.comment.replace(/# (BO_\d+)/, ''),
+        discount: dto.discount,
+        tax: dto.tax,
+        totalSale: dto.total,
+        status: dto.comment.includes('ANULADO') ? false : true,
+        customer: null,
+      };
+      this.saleList.push(sale);
+    });
+  } 
 }
