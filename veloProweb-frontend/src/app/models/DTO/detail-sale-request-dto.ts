8ed72d29510bcc648facb9443f13dc630a86e1fc
Juanin92 <j.claveria19@com>
2025-01-25 11:34:57 -0300

    Create detailSaleRequestDTO interface

---
+++ veloProweb-frontend/src/app/models/DTO/detail-sale-request-dto.ts
@@ -0,0 +1,8 @@
+export interface DetailSaleRequestDTO {
+    descriptionProduct: string;
+    quantity: number;
+    price: number;
+    customer: string;
+    notification: string;
+    ticketStatus: boolean;
+}
