6590cbe15e827f68f24e544b23158b8a97f81879
Juanin92 <j.claveria19@com>
2025-01-25 11:37:05 -0300

    Adding getSaleDetailsData method sale component

--- veloProweb-frontend/src/app/components/Report/Sale/sale-report.component.ts
+++ veloProweb-frontend/src/app/components/Report/Sale/sale-report.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SaleService } from '../../../services/Sale/sale.service';
 import { SaleRequestDTO } from '../../../models/DTO/sale-request-dto';
+import { DetailSaleRequestDTO } from '../../../models/DTO/detail-sale-request-dto';
 
 @Component({
   selector: 'app-sale-report',
@@ -12,27 +13,33 @@ import { SaleRequestDTO } from '../../../models/DTO/sale-request-dto';
   templateUrl: './sale-report.component.html',
   styleUrl: './sale-report.component.css'
 })
-export class SaleReportComponent implements OnInit{
+export class SaleReportComponent implements OnInit {
 
   saleReportList: SaleRequestDTO[] = [];
   saleList: Sale[] = [];
+  saleDetailList: DetailSaleRequestDTO[] = [];
+  saleSelected: Sale | null = null;
+  customerSale: string = '';
+  notificationSale: string = '';
+  statusTicket: boolean = true;
+  detailSelected: DetailSaleRequestDTO | null = null;
 
   constructor(
     private saleService: SaleService
-  ){}
+  ) { }
 
   ngOnInit(): void {
     this.getSalesList();
   }
 
-  getSalesList(): void{
-    this.saleService.getAllSales().subscribe((List) =>{
+  getSalesList(): void {
+    this.saleService.getAllSales().subscribe((List) => {
       this.saleReportList = List;
       this.getDocumentAndCommentSale(this.saleReportList);
     });
   }
 
-  getDocumentAndCommentSale(list: SaleRequestDTO[]): void{
+  getDocumentAndCommentSale(list: SaleRequestDTO[]): void {
     list.forEach(dto => {
       const documentMatch = dto.comment.match(/# (BO_\d+)/);
       const documentValue = documentMatch ? documentMatch[1] : null;
@@ -51,5 +58,24 @@ export class SaleReportComponent implements OnInit{
       };
       this.saleList.push(sale);
     });
-  } 
+  }
+
+  getSaleDetailsData(sale: Sale): void {
+    this.saleSelected = sale;
+    this.saleService.getDetailSale(this.saleSelected.id).subscribe({
+      next: (list) => {
+        this.saleDetailList = list;
+        if(this.saleDetailList.length > 0){
+          console.log('fecha ',this.saleDetailList[0].notification);
+          this.customerSale = this.saleDetailList[0].customer;
+          this.notificationSale = this.saleDetailList[0].notification;
+          this.statusTicket = this.saleDetailList[0].ticketStatus;
+        }
+      },
+      error: (error) =>{
+        console.log('Error al obtener detalles de la venta: ', error);
+      } 
+    });
+  }
+
 }
