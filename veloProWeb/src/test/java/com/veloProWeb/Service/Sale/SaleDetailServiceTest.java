b9de813f6d53f9114adab647d65553591b18df6f
Juanin92 <j.claveria19@com>
2025-01-25 12:30:36 -0300

    Adding getSaleDetails Tests

--- veloProWeb/src/test/java/com/veloProWeb/Service/Sale/SaleDetailServiceTest.java
+++ veloProWeb/src/test/java/com/veloProWeb/Service/Sale/SaleDetailServiceTest.java
@@ -1,10 +1,15 @@
 package com.veloProWeb.Service.Sale;
 
 import com.veloProWeb.Model.DTO.DetailSaleDTO;
+import com.veloProWeb.Model.DTO.DetailSaleRequestDTO;
+import com.veloProWeb.Model.Entity.Customer.Customer;
+import com.veloProWeb.Model.Entity.Customer.TicketHistory;
 import com.veloProWeb.Model.Entity.Product.Product;
 import com.veloProWeb.Model.Entity.Sale.Sale;
 import com.veloProWeb.Model.Entity.Sale.SaleDetail;
+import com.veloProWeb.Repository.Customer.TicketHistoryRepo;
 import com.veloProWeb.Repository.Sale.SaleDetailRepo;
+import com.veloProWeb.Service.Customer.CustomerService;
 import com.veloProWeb.Service.Product.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,21 +19,31 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class SaleDetailServiceTest {
     @InjectMocks private SaleDetailService saleDetailService;
     @Mock private SaleDetailRepo saleDetailRepo;
+    @Mock private TicketHistoryRepo ticketHistoryRepo;
+    @Mock private SaleService saleService;
     @Mock private ProductService productService;
+    @Mock private CustomerService customerService;
     private DetailSaleDTO detailSaleDTO;
+    private DetailSaleRequestDTO detailSaleRequestDTO;
+    private SaleDetail saleDetail;
     private Sale sale;
     private Product product;
+    private Customer customer;
+    private TicketHistory ticketHistory;
 
     @BeforeEach
     void setUp(){
@@ -37,11 +52,32 @@ public class SaleDetailServiceTest {
         detailSaleDTO.setIdProduct(2L);
         detailSaleDTO.setQuantity(1);
 
+        detailSaleRequestDTO =  new DetailSaleRequestDTO();
+
+        saleDetail = new SaleDetail();
+        saleDetail.setId(3L);
+        saleDetail.setQuantity(2);
+        saleDetail.setPrice(100);
+
         sale = new Sale();
         sale.setId(1L);
+        sale.setDocument("BO1");
+        sale.setTotalSale(100);
 
         product =  new Product();
         product.setId(2L);
+        product.setDescription("Prueba descripción");
+
+        customer = new Customer();
+        customer.setId(20L);
+        customer.setName("Juan");
+        customer.setSurname("Perez");
+
+        ticketHistory = new TicketHistory();
+        ticketHistory.setId(1L);
+        ticketHistory.setStatus(true);
+        ticketHistory.setNotificationsDate(null);
+        ticketHistory.setDocument("BO1");
     }
 
     //Prueba para crear un detalle de venta
@@ -77,5 +113,61 @@ public class SaleDetailServiceTest {
         List<SaleDetail> result = saleDetailService.getAll();
         verify(saleDetailRepo).findAll();
         assertEquals(1, result.size());
+    }
+
+    //Prueba para obtener una lista de DTO con detalle de ventas
+    @Test
+    public void getSaleDetails_validWithCustomer(){
+        saleDetail.setProduct(product);
+        sale.setCustomer(customer);
+        saleDetail.setSale(sale);
+        ticketHistory.setCustomer(customer);
+        List<SaleDetail> saleDetails = Collections.singletonList(saleDetail);
+        List<TicketHistory> ticketHistoryList = Collections.singletonList(ticketHistory);
+        when(saleDetailRepo.findBySaleId(1L)).thenReturn(saleDetails);
+        when(saleService.getSaleById(1L)).thenReturn(Optional.of(sale));
+        when(customerService.getCustomerById(20L)).thenReturn(customer);
+        when(ticketHistoryRepo.findByCustomerId(20L)).thenReturn(ticketHistoryList);
+
+        List<DetailSaleRequestDTO> result = saleDetailService.getSaleDetails(1L);
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        DetailSaleRequestDTO dto = result.getFirst();
+        assertEquals(2, dto.getQuantity());
+        assertEquals(100, dto.getPrice());
+        assertEquals("Prueba descripción", dto.getDescriptionProduct());
+        assertEquals("Juan Perez", dto.getCustomer());
+        assertTrue(dto.isTicketStatus());
+        assertNull(dto.getNotification());
+
+        verify(saleDetailRepo).findBySaleId(1L);
+        verify(saleService).getSaleById(1L);
+        verify(customerService).getCustomerById(20L);
+        verify(ticketHistoryRepo).findByCustomerId(20L);
+    }
+    @Test
+    public void getSaleDetails_validWithoutCustomer(){
+        saleDetail.setProduct(product);
+        sale.setCustomer(null);
+        saleDetail.setSale(sale);
+        List<SaleDetail> saleDetails = Collections.singletonList(saleDetail);
+        when(saleDetailRepo.findBySaleId(1L)).thenReturn(saleDetails);
+        when(saleService.getSaleById(1L)).thenReturn(Optional.of(sale));
+
+        List<DetailSaleRequestDTO> result = saleDetailService.getSaleDetails(1L);
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        DetailSaleRequestDTO dto = result.getFirst();
+        assertEquals(2, dto.getQuantity());
+        assertEquals(100, dto.getPrice());
+        assertEquals("Prueba descripción", dto.getDescriptionProduct());
+        assertEquals("", dto.getCustomer());
+        assertTrue(dto.isTicketStatus());
+        assertNull(dto.getNotification());
+
+        verify(saleDetailRepo).findBySaleId(1L);
+        verify(saleService).getSaleById(1L);
+        verify(customerService, never()).getCustomerById(20L);
+        verify(ticketHistoryRepo, never()).findByCustomerId(20L);
     }
 }
