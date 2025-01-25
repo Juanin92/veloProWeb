5928f019ed92e350e16dcb4d082dbde63f5795cb
Juanin92 <j.claveria19@com>
2025-01-25 11:32:38 -0300

    Delete getCustomer endpoint

--- veloProWeb/src/main/java/com/veloProWeb/Controller/Customer/CustomerController.java
+++ veloProWeb/src/main/java/com/veloProWeb/Controller/Customer/CustomerController.java
@@ -96,16 +96,4 @@ public class CustomerController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
     }
-
-    @GetMapping("/cliente")
-    public ResponseEntity<Map<String, Customer>> getCustomer(@RequestParam Long id){
-        Map<String, Customer> response = new HashMap<>();
-        try{
-            response.put("OK", customerService.getCustomerById(id));
-            return ResponseEntity.ok(response);
-        }catch (Exception e){
-            response.put(e.getMessage(), null);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-        }
-    }
 }
