270d918aabf90d16dae534e85164818f3b91fad0
Juanin92 <j.claveria19@com>
2025-01-25 11:30:01 -0300

    Create DetailSaleRequestDTO

---
+++ veloProWeb/src/main/java/com/veloProWeb/Model/DTO/DetailSaleRequestDTO.java
@@ -0,0 +1,15 @@
+package com.veloProWeb.Model.DTO;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class DetailSaleRequestDTO {
+    private String descriptionProduct;
+    private int quantity;
+    private int price;
+    private String customer;
+    private LocalDate notification;
+    private boolean ticketStatus;
+}
