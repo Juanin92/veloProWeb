52696b97032e1e03de768e36f8edd2f0b649c8dd
Juanin92 <j.claveria19@com>
2025-01-26 12:54:10 -0300

    Adding getAllPurchases method to purchase service, adding convertToPurchaseRequestDTO private method to purchase

--- veloProWeb/src/main/java/com/veloProWeb/Service/Purchase/PurchaseService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/Purchase/PurchaseService.java
@@ -1,8 +1,14 @@
 package com.veloProWeb.Service.Purchase;
 
+import com.veloProWeb.Model.DTO.DetailPurchaseDTO;
+import com.veloProWeb.Model.DTO.DetailSaleDTO;
 import com.veloProWeb.Model.DTO.PurchaseRequestDTO;
+import com.veloProWeb.Model.DTO.SaleRequestDTO;
 import com.veloProWeb.Model.Entity.Purchase.Purchase;
+import com.veloProWeb.Model.Entity.Purchase.PurchaseDetail;
 import com.veloProWeb.Model.Entity.Purchase.Supplier;
+import com.veloProWeb.Model.Entity.Sale.Sale;
+import com.veloProWeb.Model.Entity.Sale.SaleDetail;
 import com.veloProWeb.Repository.Purchase.PurchaseRepo;
 import com.veloProWeb.Repository.Purchase.SupplierRepo;
 import com.veloProWeb.Service.Purchase.Interfaces.IPurchaseService;
@@ -10,6 +16,8 @@ import com.veloProWeb.Validation.PurchaseValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -48,5 +56,34 @@ public class PurchaseService implements IPurchaseService {
     @Override
     public Long totalPurchase() {
         return purchaseRepo.count();
+    }
+
+    @Override
+    public List<PurchaseRequestDTO> getAllPurchases() {
+        List<Purchase> purchases = purchaseRepo.findAll();
+        List<PurchaseRequestDTO> dtoList = new ArrayList<>();
+        for (Purchase purchase : purchases){
+            PurchaseRequestDTO dto = convertToPurchaseRequestDTO(purchase);
+            dtoList.add(dto);
+        }
+        return dtoList;
+    }
+
+    /**
+     *  Convierte un objeto Purchase a un DTO
+     * @param purchase - la compra que se va convertir
+     * @return - objeto DTO que contiene los detalle de la compra
+     */
+    private PurchaseRequestDTO convertToPurchaseRequestDTO(Purchase purchase){
+        PurchaseRequestDTO dto = new PurchaseRequestDTO();
+        dto.setId(purchase.getId());
+        dto.setDate(purchase.getDate());
+        dto.setIdSupplier(purchase.getSupplier().getId());
+        dto.setDocumentType(purchase.getDocumentType());
+        dto.setDocument(purchase.getDocument());
+        dto.setTax(purchase.getIva());
+        dto.setTotal(purchase.getPurchaseTotal());
+        dto.setDetailList(null);
+        return dto;
     }
 }
