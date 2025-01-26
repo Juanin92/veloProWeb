90bee7bb7130bd59b63ec92f9888810881fd542e
Juanin92 <j.claveria19@com>
2025-01-26 12:00:17 -0300

    Modify tooltip service completely

--- veloProweb-frontend/src/app/utils/tooltip.service.ts
+++ veloProweb-frontend/src/app/utils/tooltip.service.ts
@@ -7,9 +7,41 @@ import * as bootstrap from 'bootstrap';
 export class TooltipService {
 
   initializeTooltips(): void {
-      const toolTipElementList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-      toolTipElementList.forEach((element) => {
-        new bootstrap.Tooltip(element);
+    const tooltipTriggerList: HTMLElement[] = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach((tooltipTriggerEl: HTMLElement) => {
+      // Destruir todas las instancias anteriores si existen
+      let existingInstance = bootstrap.Tooltip.getInstance(tooltipTriggerEl);
+      if (existingInstance) {
+        existingInstance.dispose();
+      }
+
+      // Inicializar el nuevo tooltip
+      const tooltipInstance = new bootstrap.Tooltip(tooltipTriggerEl, {
+        trigger: 'manual'
       });
-    }
+
+      // Evitar duplicación de eventos
+      if (!tooltipTriggerEl.hasAttribute('data-tooltip-initialized')) {
+        tooltipTriggerEl.addEventListener('mouseenter', () => {
+          const instance = bootstrap.Tooltip.getInstance(tooltipTriggerEl);
+          if (instance) {
+            instance.show();
+          }
+        });
+
+        tooltipTriggerEl.addEventListener('mouseleave', () => {
+          const instance = bootstrap.Tooltip.getInstance(tooltipTriggerEl);
+          if (instance) {
+            setTimeout(() => {
+              if (!tooltipTriggerEl.matches(':hover')) {
+                instance.hide();
+              }
+            }, 2000);
+          }
+        });
+
+        tooltipTriggerEl.setAttribute('data-tooltip-initialized', 'true');
+      }
+    });
+  }
 }
