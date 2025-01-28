3027923527172084728e7b1bb9a002da24d5df6f
Juanin92 <j.claveria19@com>
2025-01-28 13:34:17 -0300

    Delete createKardex endpoint

--- veloProWeb/src/main/java/com/veloProWeb/Controller/Report/KardexController.java
+++ veloProWeb/src/main/java/com/veloProWeb/Controller/Report/KardexController.java
@@ -1,6 +1,5 @@
 package com.veloProWeb.Controller.Report;
 
-import com.veloProWeb.Model.DTO.KardexRequestDTO;
 import com.veloProWeb.Model.Entity.Kardex;
 import com.veloProWeb.Service.Report.IkardexService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Controlador REST para gestionar operaciones relacionadas con lso registros/movimientos del inventario.
- * Este controlador proporciona endpoints para obtener listas y creaciones de registros.
+ * Este controlador proporciona endpoints para obtener listas de registros.
  */
 @RestController
 @RequestMapping("/kardex")
@@ -32,23 +29,6 @@ public class KardexController {
             return ResponseEntity.ok(kardexService.getAll());
         }catch (Exception e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-    }
-
-    /**
-     * Crear un registro de kardex
-     * @param dto - Objeto con los datos necesarios
-     * @return - ResponseEntity emite un mensaje de confirmación o error del proceso
-     */
-    @PostMapping
-    public ResponseEntity<Map<String, String>> createKardex(@RequestBody KardexRequestDTO dto){
-        Map<String, String> response = new HashMap<>();
-        try{
-            response.put("Ok","Registro registrado");
-            return ResponseEntity.ok(response);
-        }catch (IllegalArgumentException e){
-            response.put("Error", e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
     }
 }
