f278b1f1a90019971c0fd5e3fae59c2338dcf59b
Juanin92 <j.claveria19@com>
2025-01-30 17:23:57 -0300

    Adding variables to user interface

--- veloProweb-frontend/src/app/models/Entity/user.ts
+++ veloProweb-frontend/src/app/models/Entity/user.ts
@@ -1,3 +1,15 @@
+import { Role } from "../enum/role";
+
 export interface User {
     id: number;
+    date: string;
+    name: string;
+    surname: string;
+    username: string;
+    rut: string;
+    email: string;
+    password: string;
+    token: string;
+    status: boolean;
+    role: Role;
 }
