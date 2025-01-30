3f211e588851b47960f18523e325f84e0a5d9b0c
Juanin92 <j.claveria19@com>
2025-01-30 18:46:19 -0300

    Adding methods to user component

--- veloProweb-frontend/src/app/components/user/user.component.ts
+++ veloProweb-frontend/src/app/components/user/user.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/User/user.service';
+import { User } from '../../models/Entity/user';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnInit{
 
+  userList: User[] = [];
+  userSelected: User | null = null;
+  addUserButton: boolean = false;
+
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void{
+    this.userService.getListUsers().subscribe({
+      next: (list) =>{
+        this.userList = list;
+      },
+      error: (error) =>{
+        console.log("Error, no se encontró una registro de usuarios");
+      }
+    });
+  }
+
+  createUser(): void{
+    this.addUserButton = true;
+  }
 }
