6a43ee1c5c5fed00e5ac0cb8fbfc383541e3b0bb
Juanin92 <j.claveria19@com>
2025-01-30 18:45:28 -0300

    Adding methods to user service

--- veloProweb-frontend/src/app/services/User/user.service.ts
+++ veloProweb-frontend/src/app/services/User/user.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../models/Entity/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/usuario';
+  
+  constructor(private httpClient: HttpClient) { }
+
+  getListUsers(): Observable<User[]>{
+    return this.httpClient.get<User[]>(this.apiUrl);
+  }
+
+  addUser(user: User): Observable<{message: string}>{
+    return this.httpClient.post<{message: string}>(`${this.apiUrl}/nuevo-usuario`, user);
+  }
+
+  updateUser(user: User): Observable<{message: string}>{
+    return this.httpClient.post<{message: string}>(`${this.apiUrl}/editar-usuario`, user);
+  }
+
+  deleteUser(user: User): Observable<{message: string}>{
+    return this.httpClient.post<{message: string}>(`${this.apiUrl}/eliminar-usuario`, user);
+  }
+
+  activeUser(user: User): Observable<{message: string}>{
+    return this.httpClient.post<{message: string}>(`${this.apiUrl}/activar-usuario`, user);
+  }
 }
