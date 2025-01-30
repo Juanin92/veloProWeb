a0f318e039a0efa02c4050bc40978e2ca7d9bd51
Juanin92 <j.claveria19@com>
2025-01-30 19:36:33 -0300

    Create Record repository

---
+++ veloProWeb/src/main/java/com/veloProWeb/Repository/RecordRepo.java
@@ -0,0 +1,9 @@
+package com.veloProWeb.Repository;
+
+import com.veloProWeb.Model.Entity.Record;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordRepo extends JpaRepository<Record, Long> {
+}
