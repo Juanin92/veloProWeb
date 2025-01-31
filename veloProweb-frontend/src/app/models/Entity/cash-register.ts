be65a9e9e35f2b644c55ff70135b4fd127597597
Juanin92 <j.claveria19@com>
2025-01-31 15:37:28 -0300

    Create cashRegister interface

--- veloProweb-frontend/src/app/models/Entity/cash-register.ts
+++ veloProweb-frontend/src/app/models/Entity/cash-register.ts
@@ -1,2 +1,13 @@
+import { User } from "./user";
+
 export interface CashRegister {
+    id: number;
+    dateOpening: string;
+    dateClosing: string;
+    amountOpening: number;
+    amountClosingCash: number;
+    amountClosingPos: number;
+    status: boolean;
+    comment: string;
+    user: User;
 }
