ed7648a42b3c1a0509d6c7d2ac1edc67effb41f0
Juanin92 <j.claveria19@com>
2025-01-31 15:24:35 -0300

    adding recordService and getRecords method to setting component

--- veloProweb-frontend/src/app/components/setting/setting.component.ts
+++ veloProweb-frontend/src/app/components/setting/setting.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecordService } from '../../services/record.service';
+import { Record } from '../../models/Entity/record';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-setting',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './setting.component.html',
   styleUrl: './setting.component.css'
 })
-export class SettingComponent {
+export class SettingComponent implements OnInit{
 
+  recordList: Record[] = [];
+
+  constructor(private recordService: RecordService){}
+
+  ngOnInit(): void {
+    this.getRecords();
+  }
+
+  getRecords(): void{
+    this.recordService.getRecords().subscribe({
+      next:(list)=>{
+        this.recordList = list;
+      },
+      error: (error)=>{
+        console.log('No se encontró información sobre los registros')
+      }
+    });
+  }
 }
