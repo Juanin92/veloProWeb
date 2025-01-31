392c8d5da5fd70aacde16b30a77304a21d303c15
Juanin92 <j.claveria19@com>
2025-01-31 15:23:53 -0300

    Create record service, adding method

---
+++ veloProweb-frontend/src/app/services/record.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Record } from '../models/Entity/record';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecordService {
+
+  private apiUrl = 'http://localhost:8080/registro';
+    
+  constructor(private httpClient: HttpClient) { }
+
+  getRecords(): Observable<Record[]>{
+    return this.httpClient.get<Record[]>(this.apiUrl);
+  }
+}
