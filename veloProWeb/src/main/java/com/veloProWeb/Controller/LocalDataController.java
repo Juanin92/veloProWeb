82b1161c71138fba799d618dab933efee5e91307
Juanin92 <j.claveria19@com>
2025-01-31 14:33:44 -0300

    Create localdata controller, adding endpoints

---
+++ veloProWeb/src/main/java/com/veloProWeb/Controller/LocalDataController.java
@@ -0,0 +1,51 @@
+package com.veloProWeb.Controller;
+
+import com.veloProWeb.Model.Entity.LocalData;
+import com.veloProWeb.Service.User.Interface.ILocalDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/data")
+@CrossOrigin(origins = "http://localhost:4200")
+public class LocalDataController {
+
+    @Autowired private ILocalDataService localDataService;
+
+    @GetMapping
+    public List<LocalData> getData(){
+        return localDataService.getData();
+    }
+
+    @PostMapping()
+    public ResponseEntity<Map<String, String>> saveData(@RequestBody LocalData data){
+        Map<String, String> response = new HashMap<>();
+        try{
+            localDataService.saveData(data);
+            response.put("message", "Información registrada");
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            response.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
+
+    @PutMapping()
+    public ResponseEntity<Map<String, String>> updateData(@RequestBody LocalData data){
+        Map<String, String> response = new HashMap<>();
+        try{
+            localDataService.updateData(data);
+            response.put("message", "Información actualizada");
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            response.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
+}
