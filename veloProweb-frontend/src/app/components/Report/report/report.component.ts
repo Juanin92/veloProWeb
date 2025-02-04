32e8751f822b1f4dbc3c48c63397d2eb3ec9be89
Juanin92 <j.claveria19@com>
2025-02-04 12:38:10 -0300

    Handle to button selection

--- veloProweb-frontend/src/app/components/Report/report/report.component.ts
+++ veloProweb-frontend/src/app/components/Report/report/report.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DailySaleCountDTO } from '../../../models/DTO/Report/daily-sale-count-dto';
 import { ReportServiceService } from '../../../services/Report/report-service.service';
 import { subDays, subMonths, subYears, format } from 'date-fns';
 import { RouterModule } from '@angular/router';
+import { NotificationService } from '../../../utils/notification-service.service';
+import { ReportDTO } from '../../../models/DTO/Report/report-dto';
 
 
 @Component({
@@ -19,16 +20,30 @@ export class ReportComponent implements OnInit {
   buttonClicked: boolean = false;
   selectedPeriod: string = '';
   startDate: string = '';
+  starDateInput: string = '';
   endDate: string = '';
-  dailySaleCountList: DailySaleCountDTO[] = [];
+  endDateInput: string = '';
+  dailySaleCountList: ReportDTO[] = [];
+  activeButton: string = '';
 
-  constructor(private reportService: ReportServiceService) { }
+  constructor(
+    private reportService: ReportServiceService,
+    private notification: NotificationService
+  ) { }
 
   ngOnInit(): void {
     this.buttonClicked = false;
   }
 
+  onButtonClick(button: string): void{
+    this.activeButton = button;
+    this.buttonClicked = true;
+    this.selectedPeriod = '';
+  }
+
   onPeriodChange(period: string): void {
+    this.starDateInput = '';
+    this.endDateInput = '';
     this.selectedPeriod = period;
     const now = new Date();
     let past: Date = now;
@@ -54,15 +69,55 @@ export class ReportComponent implements OnInit {
     }
     this.startDate = format(past, 'yyyy-MM-dd');
     this.endDate = format(now, 'yyyy-MM-dd');
+    this.callMethod('automatic');
+  }
+
+  callMethod(mode: string): void {
+    if(mode === 'manual'){
+      this.startDate = format(this.starDateInput, 'yyyy-MM-dd');
+      this.endDate = format(this.endDateInput, 'yyyy-MM-dd');
+      if (this.startDate > this.endDate) {
+        this.notification.showWarningToast('La fecha de inicio debe ser menor que la fecha de fin.','top', 3000);
+        return;
+      }
+    }
+    
+    if (this.startDate === '' || this.endDate === '') {
+      this.notification.showWarningToast('Fechas no disponibles','top', 3000);
+      return;
+    }
+
+    switch (this.activeButton) {
+      case 'totalSale':
+        this.getDailySale();
+        break;
+      case 'totalAmount':
+        // this.getTotalAmount(); 
+        break;
+      case 'averageSale':
+        // this.getAverageSale(); 
+        break;
+      case 'totalEarnings':
+        // this.getTotalEarnings(); 
+        break;
+      case 'productsSold':
+        // this.getProductsSold(); 
+        break;
+      case 'categoriesSold':
+        // this.getCategoriesSold();
+        break;
+      default:
+        break;
+    }
   }
 
   getDailySale(): void{
-    console.log('Fecha inicio', this.startDate);
-    console.log('Fecha final', this.endDate);
-    this.reportService.getDailySale(this.startDate, this.endDate).subscribe((list)=>{
-      this.dailySaleCountList = list;
-    }, (error: Error) => {
+    this.reportService.getDailySale(this.startDate, this.endDate).subscribe({
+      next:(list)=>{
+        this.dailySaleCountList = list;
+      }, error: (error: Error) => {
       console.error(error.message); 
+      }
     });
   }
 
