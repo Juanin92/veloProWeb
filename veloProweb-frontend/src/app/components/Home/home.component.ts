bd1f54649596b0561790b8bc7873dd9724e539a9
Juanin92 <j.claveria19@com>
2025-02-12 13:20:03 -0300

    adding toggleCardExpand method

--- veloProweb-frontend/src/app/components/Home/home.component.ts
+++ veloProweb-frontend/src/app/components/Home/home.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  expanded = {
+    notification: true,
+    dispatch: true,
+    task: false,
+    alert: false
+  };
+
+  toggleCardExpand(section: 'notification' | 'dispatch' | 'task' | 'alert'): void {
+    this.expanded[section] = !this.expanded[section];
+  }
 }
