c830e0d7e7047cabdb4f7c4843bef7477fd993a2
Juanin92 <j.claveria19@com>
2025-02-12 22:08:59 -0300

    Adding documentation to dispatch service

--- veloProWeb/src/main/java/com/veloProWeb/Service/DispatchService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/DispatchService.java
@@ -23,6 +23,11 @@ public class DispatchService implements IDispatchService{
         statusMap.put(4, "Eliminado");
     }
 
+    /**
+     * Obtiene una lista filtrada de registro de despacho
+     * Despachos eliminados no se toman en cuenta
+     * @return - Lista filtrada de despachos
+     */
     @Override
     public List<Dispatch> getDispatches() {
         List<Dispatch> dispatchList = dispatchRepo.findAll();
@@ -31,6 +36,11 @@ public class DispatchService implements IDispatchService{
                 .toList();
     }
 
+    /**
+     * Creación de un despacho.
+     * Verifica que el despacho no sea nulo y este contenga un objeto de Venta
+     * @param dispatch - Despacho con los datos necesarios
+     */
     @Override
     public void createDispatch(Dispatch dispatch) {
         if (dispatch != null) {
@@ -40,6 +50,7 @@ public class DispatchService implements IDispatchService{
                 dispatch.setCreated(LocalDate.now());
                 dispatch.setStatus(statusMap.get(1));
                 dispatch.setDeliveryDate(null);
+                dispatchRepo.save(dispatch);
             }else {
                 throw new IllegalArgumentException("Debe tener una venta asociada");
             }
@@ -48,23 +59,32 @@ public class DispatchService implements IDispatchService{
         }
     }
 
+    /**
+     * Manejo del estado de un despacho.
+     * Verifica que el despacho exista y que la acción exista.
+     * @param dispatchID - Identificador del despacho
+     * @param action - (número) Clave del Map para seleccionar estado
+     */
     @Override
     public void handleStatus(Long dispatchID, int action) {
         Dispatch dispatch = getDispatchExisting(dispatchID);
         if (dispatch != null) {
             switch (action){
                 case 2:
+                    //Verifica que solo cuando sea "En Preparación" quede "En Ruta"
                     if (dispatch.getStatus().equals(statusMap.get(1))){
                         dispatch.setStatus(statusMap.get(2));
                     }
                     break;
                 case 3:
+                    //Verifica que solo cuando sea "En Preparación" o "En Ruta" quede "Entregado"
                     if (dispatch.getStatus().equals(statusMap.get(1)) || dispatch.getStatus().equals(statusMap.get(2))){
                         dispatch.setStatus(statusMap.get(3));
                         dispatch.setDeliveryDate(LocalDate.now());
                     }
                     break;
                 case 4:
+                    //Verifica que solo cuando sea diferente "Eliminado" quede "Eliminado"
                     if (!dispatch.getStatus().equals(statusMap.get(4))){
                         dispatch.setStatus(statusMap.get(4));
                     }
@@ -78,6 +98,11 @@ public class DispatchService implements IDispatchService{
         }
     }
 
+    /**
+     * Obtiene un despacho existente en el registro
+     * @param dispatchID - Identificador de despacho
+     * @return - Si encuentra un despacho o valor nulo
+     */
     private Dispatch getDispatchExisting(Long dispatchID){
         return dispatchRepo.findById(dispatchID).orElse(null);
     }
