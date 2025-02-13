c7a6369138887617f49d647e176f9abdc05efeb0
Juanin92 <j.claveria19@com>
2025-02-13 16:30:09 -0300

    Adding documentation

--- veloProWeb/src/main/java/com/veloProWeb/Service/AlertService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/AlertService.java
@@ -22,6 +22,11 @@ public class AlertService implements IAlertService{
         statusMap.put(2, "Revisado");
     }
 
+    /**
+     * Obtiene todos los registros de alertas
+     * Filtra las alertas que ya se revisaron
+     * @return - Lista filtrada con alertas
+     */
     @Override
     public List<Alert> getAlerts() {
         List<Alert> alerts = alertRepo.findAll();
@@ -30,6 +35,12 @@ public class AlertService implements IAlertService{
                 .toList();
     }
 
+    /**
+     * Crear una alerta.
+     * Verifica que el producto no sea nulo
+     * @param product - Producto asociado a la alerta
+     * @param description - descripción para la alerta
+     */
     @Override
     public void createAlert(Product product, String description) {
         if (product != null) {
@@ -43,12 +54,20 @@ public class AlertService implements IAlertService{
         }
     }
 
+    /**
+     * Manejo del estado de la alerta
+     * @param alert - Alerta seleccionada
+     * @param action - Número de la acción a realizar
+     */
     @Override
     public void handleAlertStatus(Alert alert, int action) {
         Alert alertExisting = alertRepo.findById(alert.getId()).orElse(null);
         if (alertExisting != null) {
-            alertExisting.setStatus(statusMap.get(2));
-            alertRepo.save(alertExisting);
+            //Verifica que la alerta no haya si ya revisada
+            if (alertExisting.getStatus().equals(statusMap.get(1))){
+                alertExisting.setStatus(statusMap.get(2));
+                alertRepo.save(alertExisting);
+            }
         }else{
             throw new IllegalArgumentException("Alerta no encontrada");
         }
