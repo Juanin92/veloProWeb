3273d86f0abc2ceb70618e10008ebf7a7cf943fc
Juanin92 <j.claveria19@com>
2025-02-13 17:41:03 -0300

    ADding annotations, Change type String to boolean  status variable

--- veloProWeb/src/main/java/com/veloProWeb/Model/Entity/Task.java
+++ veloProWeb/src/main/java/com/veloProWeb/Model/Entity/Task.java
@@ -1,7 +1,9 @@
 package com.veloProWeb.Model.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -9,6 +11,8 @@ import java.time.LocalDate;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "tasks")
 public class Task {
 
@@ -16,7 +20,7 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
-    private String status;
+    private boolean status;
 
     @DateTimeFormat(pattern = "dd-MM-yyyy")
     @Temporal(TemporalType.DATE)
@@ -24,6 +28,6 @@ public class Task {
     private LocalDate created;
 
     @ManyToOne
-    @JoinColumn(name = "sale_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
