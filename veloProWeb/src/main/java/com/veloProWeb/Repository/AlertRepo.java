35f2ce67d65be47ed271131ccbb9d07c3ffb0fe7
Juanin92 <j.claveria19@com>
2025-02-13 15:52:59 -0300

    Create alert repository

---
+++ veloProWeb/src/main/java/com/veloProWeb/Repository/AlertRepo.java
@@ -0,0 +1,9 @@
+package com.veloProWeb.Repository;
+
+import com.veloProWeb.Model.Entity.Alert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlertRepo extends JpaRepository<Alert, Long> {
+}
