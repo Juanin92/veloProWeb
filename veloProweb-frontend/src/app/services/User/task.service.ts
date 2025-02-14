80fed5f1fca21ff5dbb07b172303a74d70ca79b9
Juanin92 <j.claveria19@com>
2025-02-14 11:15:53 -0300

    Create task service, adding methods

---
+++ veloProweb-frontend/src/app/services/User/task.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from '../../models/Entity/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private apiUrl = 'http://localhost:8080/tareas';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getTasks(userID: number): Observable<Task[]>{
+    return this.httpClient.get<Task[]>(this.apiUrl, {params: {userId: userID.toString()}});
+  }
+
+  createTask(task: Task): Observable<{message: string}>{
+    return this.httpClient.post<{message: string}>(this.apiUrl, task);
+  }
+
+  completeTask(taskID: number): Observable<{message: string}>{
+    return this.httpClient.put<{message: string}>(this.apiUrl, {}, {params: {taskID: taskID.toString()}});
+  }
+}
