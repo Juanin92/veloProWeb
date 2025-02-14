32b1e5af3db9840eef44c5f65d40714acf0a167e
Juanin92 <j.claveria19@com>
2025-02-14 17:58:09 -0300

    Change return message dto list

--- veloProWeb/src/main/java/com/veloProWeb/Service/User/Interface/IMessageService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/User/Interface/IMessageService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface IMessageService {
     List<Message> getAllMessages();
-    List<Message> getMessageByUser(Long userID);
+    List<MessageDTO> getMessageByUser(Long userID);
     void isReadMessage(Long id, String context);
     void isDeleteMessage(Long id, String context);
     void sendMessage(MessageDTO dto);
