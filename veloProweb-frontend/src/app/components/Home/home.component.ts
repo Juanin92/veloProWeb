ae23eb8d0becbce6405fe3598376e3d1399075c1
Juanin92 <j.claveria19@com>
2025-02-14 11:56:10 -0300

    import message complement

--- veloProweb-frontend/src/app/components/Home/home.component.ts
+++ veloProweb-frontend/src/app/components/Home/home.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../services/User/task.service';
 import { Task } from '../../models/Entity/task';
+import { MessageModalComponent } from "../user/message-modal/message-modal.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MessageModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
