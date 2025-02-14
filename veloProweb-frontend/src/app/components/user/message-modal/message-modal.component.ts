571b6e494b45e6f028bc5429cc4ac5e78d9a92b1
Juanin92 <j.claveria19@com>
2025-02-14 17:29:05 -0300

    Create messageModal component, adding methods

--- veloProweb-frontend/src/app/components/user/message-modal/message-modal.component.ts
+++ veloProweb-frontend/src/app/components/user/message-modal/message-modal.component.ts
@@ -1,12 +1,113 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { MessageService } from '../../../services/User/message.service';
+import { Message } from '../../../models/Entity/message';
+import { NotificationService } from '../../../utils/notification-service.service';
+import { UserService } from '../../../services/User/user.service';
+import { User } from '../../../models/Entity/user';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-message-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './message-modal.component.html',
   styleUrl: './message-modal.component.css'
 })
-export class MessageModalComponent {
+export class MessageModalComponent implements OnInit{
 
+  @Output() messagesUpdated = new EventEmitter<Message[]>();
+  userList: User[] = [];
+  messageList: Message[] = [];
+  message: Message = {
+    id: 0,
+    context: '',
+    isRead: false,
+    isDelete: false,
+    senderUser: 0,
+    receiverUser: 0
+  };
+
+  constructor(
+    private messageService: MessageService,
+    private userService: UserService,
+    private notification: NotificationService){}
+
+  ngOnInit(): void {
+    this.getUsers();
+    this.getMessages();
+  }
+
+  getMessages(): void{
+    this.messageService.getMessages(1).subscribe({
+      next:(list)=>{
+        this.messageList = list;
+        this.messagesUpdated.emit(this.messageList);
+      }
+    });
+  }
+
+  getUsers(): void{
+    this.userService.getListUsers().subscribe({
+      next:(list) =>{
+        this.userList = list;
+      }
+    })
+  }
+
+  sendMessage(message: Message): void{
+    message.senderUser = 1;
+    if(!message.receiverUser || !message.context){
+      this.notification.showWarningToast('Debe seleccionar un destinatario', 'top', 3000);
+      return;
+    }
+
+    this.messageService.sendMessage(message).subscribe({
+      next: (response)=>{
+        const message = response.message;
+        this.notification.showSuccessToast(message, 'top', 3000);
+      },error: (error)=>{
+        const message = error.error?.error || error.error?.message;
+        console.log('Error: ',message);
+        this.notification.showErrorToast('Error: ' + message, 'top', 3000);
+      }
+    });
+  }
+
+  isReadMessage(message: Message): void{
+    if(message.isRead && message.isDelete){
+      this.messageService.readMessage(message).subscribe({
+        next:(response)=>{
+          const message = response.message;
+          console.log('Ok: ', message);
+        },error: (error)=>{
+          const message = error.error?.error || error.error.message;
+          console.log('Error: ', message);
+        }
+      });
+    }else{
+      console.log('Mensaje se encuentra eliminado o leído.');
+    }
+  }
+
+  isDeleteMessage(message: Message): void{
+    if(!message.isRead){
+      this.messageService.deleteMessage(message).subscribe({
+        next:(response)=>{
+          const message = response.message;
+          console.log('Ok: ', message);
+        },error: (error)=>{
+          const message = error.error?.error || error.error.message;
+          console.log('Error: ', message);
+        }
+      });
+    }else{
+      console.log('Mensaje no se encuentra leído.');
+    }
+  }
+
+  resetModal(): void{
+    this.message.context = '';
+    this.message.receiverUser = 0;
+  }
 }
