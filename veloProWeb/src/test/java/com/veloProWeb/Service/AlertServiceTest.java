8f017140dd66e0b617bfae3c9e55b22e7102c017
Juanin92 <j.claveria19@com>
2025-02-15 17:37:57 -0300

    Adding isAlertActive and handleAlertStatus Test to alert service Test

--- veloProWeb/src/test/java/com/veloProWeb/Service/AlertServiceTest.java
+++ veloProWeb/src/test/java/com/veloProWeb/Service/AlertServiceTest.java
@@ -14,11 +14,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,14 +26,15 @@ public class AlertServiceTest {
 
     @InjectMocks private AlertService alertService;
     @Mock private AlertRepo alertRepo;
-    private Alert alert, alert2, alert3, alert4;
+    private Alert alert, alert2, alert3, alert4, alert5;
 
     @BeforeEach
     void setUp(){
         alert = new Alert(1L, "Test 1", "Alerta", LocalDate.now(), new Product());
-        alert2 = new Alert(2L, "Test 1", "Revisado", LocalDate.now(), new Product());
-        alert3 = new Alert(3L, "Test 1", "Revisado", LocalDate.now(), new Product());
-        alert4 = new Alert(4L, "Test 1", "Alerta", LocalDate.now(), new Product());
+        alert2 = new Alert(2L, "Test 2", "Revisado", LocalDate.now(), new Product());
+        alert3 = new Alert(3L, "Test 3", "Revisado", LocalDate.now(), new Product());
+        alert4 = new Alert(4L, "Test 4", "Alerta", LocalDate.now(), new Product());
+        alert5 = new Alert(5L, "Test 5", "Pendiente", LocalDate.now(), new Product());
     }
 
     //Prueba para obtener todos los registros de alertas
@@ -66,13 +67,31 @@ public class AlertServiceTest {
 
     //Prueba para manejar el estado de la alerta
     @Test
-    public void handleAlertStatus_valid(){
+    public void handleAlertStatus_validAlertToCheck(){
         when(alertRepo.findById(1L)).thenReturn(Optional.of(alert));
-        alertService.handleAlertStatus(alert, 1);
+        alertService.handleAlertStatus(alert, 2);
 
         verify(alertRepo, times(1)).findById(1L);
         verify(alertRepo, times(1)).save(alert);
         assertEquals("Revisado", alert.getStatus());
+    }
+    @Test
+    public void handleAlertStatus_validPendingToCheck(){
+        when(alertRepo.findById(5L)).thenReturn(Optional.of(alert5));
+        alertService.handleAlertStatus(alert5, 2);
+
+        verify(alertRepo, times(1)).findById(5L);
+        verify(alertRepo, times(1)).save(alert5);
+        assertEquals("Revisado", alert5.getStatus());
+    }
+    @Test
+    public void handleAlertStatus_validAlertToPending(){
+        when(alertRepo.findById(4L)).thenReturn(Optional.of(alert4));
+        alertService.handleAlertStatus(alert4, 3);
+
+        verify(alertRepo, times(1)).findById(4L);
+        verify(alertRepo, times(1)).save(alert4);
+        assertEquals("Pendiente", alert4.getStatus());
     }
     @Test
     public void handleAlertStatus_validNotFoundAlert(){
@@ -90,5 +109,27 @@ public class AlertServiceTest {
         alertService.handleAlertStatus(alert2, 1);
 
         verify(alertRepo, never()).save(alert);
+    }
+
+    //Prueba para verificar si una alerta activa
+    @Test
+    public void isAlertActive_validTrue(){
+        List<Alert> alerts = Collections.singletonList(alert);
+        List<String> status = Arrays.asList("Alerta", "Pendiente");
+        when(alertRepo.findByProductAndDescriptionAndStatus(new Product(), "Test 1", status)).thenReturn(alerts);
+
+        boolean result = alertService.isAlertActive(new Product(), "Test 1");
+        verify(alertRepo, times(1)).findByProductAndDescriptionAndStatus(new Product(), "Test 1", status);
+        assertTrue(result);
+    }
+    @Test
+    public void isAlertActive_validFalse(){
+        List<Alert> alerts = Collections.emptyList();
+        List<String> status = Arrays.asList("Alerta", "Pendiente");
+        when(alertRepo.findByProductAndDescriptionAndStatus(new Product(), "Test 6", status)).thenReturn(alerts);
+
+        boolean result = alertService.isAlertActive(new Product(), "Test 6");
+        verify(alertRepo, times(1)).findByProductAndDescriptionAndStatus(new Product(), "Test 6", status);
+        assertFalse(result);
     }
 }
