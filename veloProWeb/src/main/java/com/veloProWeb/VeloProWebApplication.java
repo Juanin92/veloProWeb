8063fc331a3b2d9e1635098afdc1440d383aa4f7
Juanin92 <j.claveria19@com>
2025-02-15 17:39:19 -0300

    Adding EnableScheduling annotation

--- veloProWeb/src/main/java/com/veloProWeb/VeloProWebApplication.java
+++ veloProWeb/src/main/java/com/veloProWeb/VeloProWebApplication.java
@@ -2,8 +2,10 @@ package com.veloProWeb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class VeloProWebApplication {
 
 	public static void main(String[] args) {
