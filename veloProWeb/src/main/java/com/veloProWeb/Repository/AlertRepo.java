e66a04dd8515d1e75ce8e1d9ee14e870aef9a73c
Juanin92 <j.claveria19@com>
2025-02-15 17:38:53 -0300

    Adding findByProductAndDescriptionAndStatus query method to alert repo

--- veloProWeb/src/main/java/com/veloProWeb/Repository/AlertRepo.java
+++ veloProWeb/src/main/java/com/veloProWeb/Repository/AlertRepo.java
@@ -1,9 +1,13 @@
 package com.veloProWeb.Repository;
 
+import com.veloProWeb.Model.Entity.Product.Product;
 import com.veloProWeb.Model.Entity.User.Alert;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AlertRepo extends JpaRepository<Alert, Long> {
+    List<Alert> findByProductAndDescriptionAndStatus(Product product, String description, List<String> status);
 }
