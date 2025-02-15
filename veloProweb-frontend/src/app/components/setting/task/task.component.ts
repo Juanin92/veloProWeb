77968e1fe34d0bf4a0605dc4ade8c4b31c551ed3
Juanin92 <j.claveria19@com>
2025-02-15 12:16:50 -0300

    Create Task component

---
+++ veloProweb-frontend/src/app/components/setting/task/task.component.ts
@@ -0,0 +1,52 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Task } from '../../../models/Entity/task';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TaskService } from '../../../services/User/task.service';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent implements OnInit{
+
+  @Output() taskUpdated = new EventEmitter<Task[]>();
+  taskList: Task[] = [];
+
+  constructor(private taskService: TaskService){}
+
+  ngOnInit(): void {
+    this.getTasks();
+  }
+
+  getTasks(): void {
+    this.taskService.getTasks(1).subscribe({
+      next: (list) => {
+        this.taskList = list;
+        this.taskUpdated.emit(this.taskList);
+      }, error: (error) => {
+        console.log('Error obtener tareas, ', error);
+      }
+    });
+  }
+
+  completeTask(task: Task) {
+    task.status = false;
+    this.taskService.completeTask(task.id).subscribe({
+      next:(response)=>{
+        const message = response.message;
+        console.log('Ok: ',message);
+      }, error: (error)=>{
+        const message = error.error?.message || error.error?.error;
+        console.log('Error: ',message);
+      }
+    });
+    setTimeout(() => {
+      this.taskList = this.taskList.filter(t => t !== task);
+      this.taskUpdated.emit(this.taskList);
+    }, 3000);
+  }
+}
