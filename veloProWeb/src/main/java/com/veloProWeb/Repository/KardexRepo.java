ca7871d99d3ba1b7d89401bcea2e9e0725499991
Juanin92 <j.claveria19@com>
2025-02-15 17:39:55 -0300

    Adding findByProductAndDateAfter query method to kardex repo

--- veloProWeb/src/main/java/com/veloProWeb/Repository/KardexRepo.java
+++ veloProWeb/src/main/java/com/veloProWeb/Repository/KardexRepo.java
@@ -1,9 +1,14 @@
 package com.veloProWeb.Repository;
 
 import com.veloProWeb.Model.Entity.Kardex;
+import com.veloProWeb.Model.Entity.Product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface KardexRepo extends JpaRepository<Kardex, Long> {
+    List<Kardex> findByProductAndDateAfter(Product product, LocalDate days);
 }
