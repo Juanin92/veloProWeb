3665166678d4733a5406330956f0be818d962d46
Juanin92 <j.claveria19@com>
2025-02-16 20:24:22 -0300

    Adding methods to dispatch service

--- veloProweb-frontend/src/app/services/Sale/dispatch.service.ts
+++ veloProweb-frontend/src/app/services/Sale/dispatch.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Dispatch } from '../../models/Entity/Sale/dispatch';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DispatchService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/despachos';
+
+  constructor(private http: HttpClient) { }
+
+  getDispatches(): Observable<Dispatch[]>{
+    return this.http.get<Dispatch[]>(this.apiUrl);
+  };
+
+  createDispatch(dispatch: Dispatch): Observable<{message: string}>{
+    return this.http.post<{message: string}>(this.apiUrl, dispatch);
+  }
+
+  handleStatusDispatch(dispatchID: number, action: number): Observable<{message: string}>{
+    return this.http.put<{message: string}>(this.apiUrl, {}, {
+      params:{
+        dispatchID: dispatchID.toString(),
+        action: action.toString()}
+    });
+  }
 }
