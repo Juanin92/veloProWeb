6157d11b5ef18afe8de6394ca43ccfd20c2e5689
Juanin92 <j.claveria19@com>
2025-02-16 17:53:49 -0300

    Adding methods to alert service

--- veloProweb-frontend/src/app/services/User/alert.service.ts
+++ veloProweb-frontend/src/app/services/User/alert.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AlertModel } from '../../models/Entity/alert-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/data';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAlerts(): Observable<AlertModel[]>{
+    return this.httpClient.get<AlertModel[]>(this.apiUrl);
+  }
+
+  handleStatusAlert(alert: AlertModel, action: number): Observable<{message: string}>{
+    return this.httpClient.put<{message: string}>(this.apiUrl, alert,{params:{action: action.toString()}});
+  }
 }
