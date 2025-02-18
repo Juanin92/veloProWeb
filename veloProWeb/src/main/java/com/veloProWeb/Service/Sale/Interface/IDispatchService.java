e82bf927389b68c9753e00f3e2dab09ffbf88b7c
Juanin92 <j.claveria19@com>
2025-02-17 22:17:39 -0300

    Modify getDispatches, createDispatch method to handle DTO class, adding getDispatchById method

--- veloProWeb/src/main/java/com/veloProWeb/Service/Sale/Interface/IDispatchService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/Sale/Interface/IDispatchService.java
@@ -1,11 +1,14 @@
 package com.veloProWeb.Service.Sale.Interface;
 
+import com.veloProWeb.Model.DTO.DispatchDTO;
 import com.veloProWeb.Model.Entity.Sale.Dispatch;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IDispatchService {
-    List<Dispatch> getDispatches();
-    void createDispatch(Dispatch dispatch);
+    List<DispatchDTO> getDispatches();
+    Dispatch createDispatch(DispatchDTO dto);
     void handleStatus(Long dispatchID, int action);
+    Optional<Dispatch> getDispatchById(Long id);
 }
