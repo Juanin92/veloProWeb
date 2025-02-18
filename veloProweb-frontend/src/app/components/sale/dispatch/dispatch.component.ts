57dd13b1c655b296be948829bc3fce9eec7b57ed
Juanin92 <j.claveria19@com>
2025-02-17 22:18:51 -0300

    Adding variable and method to dispatch components

--- veloProweb-frontend/src/app/components/sale/dispatch/dispatch.component.ts
+++ veloProweb-frontend/src/app/components/sale/dispatch/dispatch.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { DispatchService } from '../../../services/Sale/dispatch.service';
+import { Dispatch } from '../../../models/Entity/Sale/dispatch';
+import { Sale } from '../../../models/Entity/Sale/sale';
 
 @Component({
   selector: 'app-dispatch',
@@ -7,6 +10,38 @@ import { Component } from '@angular/core';
   templateUrl: './dispatch.component.html',
   styleUrl: './dispatch.component.css'
 })
-export class DispatchComponent {
+export class DispatchComponent implements OnInit{
 
+  @Output() dispatchUpdated = new EventEmitter<Dispatch[]>();
+  dispatchList: Dispatch[] = [];
+  dispatch: Dispatch = {
+    id: 0,
+    trackingNumber: '',
+    status: '',
+    address: '',
+    comment: '',
+    created: '',
+    deliveryDate: '',
+    saleDetail: null,
+  }
+
+  constructor(private dispatchService: DispatchService){}
+
+  ngOnInit(): void {
+    this.getDispatches();
+  }
+
+  getDispatches(): void{
+    this.dispatchService.getDispatches().subscribe({
+      next:(list)=>{
+        this.dispatchList = list;
+      }, error: (error)=>{
+        console.log('Error al obtener despachos', error.error?.error);
+      }
+    });
+  }
+
+  createNewDispatch(): void{
+    this.dispatchService.createDispatch(this.dispatch).subscribe();
+  }
 }
