c799cce270e94b17f70825076b87288a1c245e7e
Juanin92 <j.claveria19@com>
2025-02-17 22:16:01 -0300

    Create dispatch DTO class

---
+++ veloProWeb/src/main/java/com/veloProWeb/Model/DTO/DispatchDTO.java
@@ -0,0 +1,23 @@
+package com.veloProWeb.Model.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DispatchDTO {
+    private Long id;
+    private String trackingNumber;
+    private String status;
+    private String address;
+    private String comment;
+    private LocalDate created;
+    private LocalDate deliveryDate;
+    private List<DetailSaleDTO> detailSaleDTOS = new ArrayList<>();
+}
