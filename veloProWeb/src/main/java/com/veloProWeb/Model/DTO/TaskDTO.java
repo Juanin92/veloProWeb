9a8f601755972b36bb7c69a734b5386c60e462bd
Juanin92 <j.claveria19@com>
2025-03-05 18:39:57 -0300

    feat(task): add TaskDTO DTO for task data transfer

---
+++ veloProWeb/src/main/java/com/veloProWeb/Model/DTO/TaskDTO.java
@@ -0,0 +1,17 @@
+package com.veloProWeb.Model.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskDTO {
+    private String description;
+    private boolean status;
+    private LocalDate created;
+    private String user;
+}
