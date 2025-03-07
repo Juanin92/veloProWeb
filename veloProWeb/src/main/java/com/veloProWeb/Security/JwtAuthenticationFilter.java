41dad861fd2b5f7f073f6062e02f38545f4e4a50
Juanin92 <j.claveria19@com>
2025-03-06 21:28:19 -0300

    feat(security): Add JwtAuthenticationFilter class

---
+++ veloProWeb/src/main/java/com/veloProWeb/Security/JwtAuthenticationFilter.java
@@ -0,0 +1,76 @@
+package com.veloProWeb.Security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * Filtro de autenticación JWT que intercepta las solicitudes HTTP,
+ * verifica la presencia y validez del token JWT en el encabezado Authorization,
+ * y establece la autenticación en el contexto de seguridad de Spring.
+ */
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired private JwUtil jwtUtil;
+    @Autowired private UserDetailsServiceImpl userDetailsService;
+
+    /**
+     * Intercepta las solicitudes HTTP y realiza la autenticación basada en JWT.
+     * @param request     La solicitud HTTP.
+     * @param response    La respuesta HTTP.
+     * @param filterChain La cadena de filtros.
+     * @throws ServletException Si ocurre un error en el servlet.
+     * @throws IOException      Si ocurre un error de E/S.
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull  FilterChain filterChain) throws ServletException, IOException {
+        // Obtiene el encabezado Authorization de la solicitud.
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+
+        // Verifica si el encabezado Authorization existe y comienza con "Bearer ".
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            // Extrae el token JWT del encabezado
+            jwt = authorizationHeader.substring(7);
+            try{
+                // extraer el nombre de usuario del token.
+                username = jwtUtil.extractUsername(jwt);
+            } catch (Exception e){
+                System.out.println("Error extracting username from token");
+            }
+
+        }
+        // Verifica si el nombre de usuario se extrajo correctamente y no hay autenticación en el contexto de seguridad.
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // Carga los detalles del usuario desde UserDetailsService.
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+            // Válida el token JWT.
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+                // Crea un objeto UsernamePasswordAuthenticationToken con los detalles del usuario.
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                // Establece los detalles de la solicitud en el objeto de autenticación.
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                // Establece la autenticación en el contexto de seguridad.
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        // Continúa con la cadena de filtros.
+        filterChain.doFilter(request, response);
+    }
+}
