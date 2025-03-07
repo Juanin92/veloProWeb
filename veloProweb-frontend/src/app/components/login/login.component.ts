222b69ef2f8d0f2a268cd79e2724f82c263f3d4c
Juanin92 <j.claveria19@com>
2025-03-06 22:19:19 -0300

    feat(login): Add isLoginUser method and userLogin interface

--- veloProweb-frontend/src/app/components/login/login.component.ts
+++ veloProweb-frontend/src/app/components/login/login.component.ts
@@ -1,19 +1,42 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HomeComponent } from '../Home/home.component';
+import { LoginRequest } from '../../models/DTO/login-request';
+import { AuthService } from '../../services/User/auth.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NotificationService } from '../../utils/notification-service.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  constructor(private router: Router){}
+  userLogin: LoginRequest = {
+    username: '',
+    password: ''
+  };
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private notification: NotificationService){}
 
   isLoginUser(){
-    this.router.navigate(['/home']);
+    if(this.userLogin){
+      this.authService.login(this.userLogin).subscribe({
+        next:(token)=>{
+          this.authService.saveToken(token);
+          this.router.navigate(['/home']);
+        },error: (error) =>{
+          console.error('Error de inicio de sesión:', error);
+          this.notification.showWarning('Error!', 'Las credenciales no son correcta, intente de nuevo');
+        }
+      });
+    }
   }
 }
