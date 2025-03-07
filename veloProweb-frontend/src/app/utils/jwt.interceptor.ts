71d5a1d8c1e1087c311409a1b89a5c5837dc8850
Juanin92 <j.claveria19@com>
2025-03-06 22:17:21 -0300

    feat: Create JWT interceptor for HTTP requests.

---
+++ veloProweb-frontend/src/app/utils/jwt.interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
+import { AuthService } from "../services/User/auth.service";
+import { inject } from "@angular/core";
+import { Observable } from "rxjs";
+
+export const jwtInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
+    const authService = inject(AuthService); 
+    const token = authService.getToken();
+
+    if (token) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+    return next(req);
+  };
