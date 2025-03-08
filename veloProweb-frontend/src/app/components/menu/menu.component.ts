02b8bb0ac48ae621e795d9b483ea86bdee7759f9
Juanin92 <j.claveria19@com>
2025-03-08 15:30:00 -0300

    feat(menu): add authService, Role field, commonModule to menu component

--- veloProweb-frontend/src/app/components/menu/menu.component.ts
+++ veloProweb-frontend/src/app/components/menu/menu.component.ts
@@ -1,19 +1,23 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input} from '@angular/core';
 import { RouterModule } from '@angular/router';
-import * as bootstrap from 'bootstrap';
 import { TooltipService } from '../../utils/tooltip.service';
+import { AuthService } from '../../services/User/auth.service';
+import { Role } from '../../models/enum/role';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent implements AfterViewInit{
   @Input() isActive: boolean = true;
+  Role = Role;
 
-  constructor(private tooltipService: TooltipService){}
+  constructor(private tooltipService: TooltipService, public auth: AuthService){}
 
   ngAfterViewInit(): void {
     this.tooltipService.initializeTooltips();
