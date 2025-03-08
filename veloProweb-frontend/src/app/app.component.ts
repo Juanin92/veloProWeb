d6b568560bcc27ec3621785d7def4fa27a7cb4df
Juanin92 <j.claveria19@com>
2025-03-08 20:46:03 -0300

    feat(app): Delete and modify components

--- veloProweb-frontend/src/app/app.component.ts
+++ veloProweb-frontend/src/app/app.component.ts
@@ -1,86 +1,14 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
-import { MenuComponent } from "./components/menu/menu.component";
-import { CommonModule, DatePipe } from '@angular/common';
-import { LocalDataService } from './services/local-data.service';
-import { LocalData } from './models/Entity/local-data';
-import * as bootstrap from 'bootstrap';
-import { UpdateUserModalComponent } from "./components/user/update-user-modal/update-user-modal.component";
-import { MessageModalComponent } from "./components/user/message-modal/message-modal.component";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent, UpdateUserModalComponent, MessageModalComponent, CommonModule],
-  providers: [DatePipe],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent {
   
   title = 'veloProweb-frontend';
-  isMenuActive: boolean = true;
-  currentDate: string = '';
-  localData: LocalData | null = null;
-  @ViewChild('userDropdown') dropdownButton!: ElementRef;
-  dropdownInstance!: bootstrap.Dropdown;
-
-  constructor(
-    private datePipe: DatePipe, 
-    private localDataService: LocalDataService,
-    private router: Router){
-    this.getDate();
-    this.loadDataFromLocalStorage();
-  }
-
-  ngAfterViewInit(): void {
-    if (this.dropdownButton) {
-      this.dropdownInstance = new bootstrap.Dropdown(this.dropdownButton.nativeElement);
-    }
-  }
-
-  isLoginPage(): boolean {
-    return this.router.url === '/';
-  }
-
-  toggleDropdown() {
-    if (this.dropdownInstance) {
-      this.dropdownInstance.toggle();
-    }
-  }
-
-  toggleMenu(): void {
-    this.isMenuActive = !this.isMenuActive;
-  }
-
-  getDate(): void{
-    const now = new Date();
-    const formatDate = this.datePipe.transform(now, 'dd-MM-yyyy  HH:mm');
-    this.currentDate = formatDate ? formatDate : '';
-  }
-
-  loadDataFromLocalStorage(): void {
-    const savedData = sessionStorage.getItem('companyData');
-    if (savedData) {
-      this.localData = JSON.parse(savedData); 
-    } else {
-      this.localDataService.getData().subscribe({
-        next: (list) => {
-          if (list.length === 1) {
-            const simplifiedData = {
-              name: list[0].name,
-              phone: list[0].phone,
-              email: list[0].email,
-              address: list[0].address
-            };
-            this.saveDataToLocalStorage(simplifiedData);
-          }
-        }
-      });
-    }
-  }
-
-  saveDataToLocalStorage(data: { name: string, phone: string, email: string, address: string }): void {
-    sessionStorage.setItem('companyData', JSON.stringify(data));
-  }
 }
