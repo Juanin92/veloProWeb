3f8a2dc790560f0ac8e05112b90f5d377d1ad9c5
Juanin92 <j.claveria19@com>
2025-03-08 22:35:31 -0300

    feat(message-modal): Delete methods and fields

--- veloProweb-frontend/src/app/components/user/message-modal/message-modal.component.ts
+++ veloProweb-frontend/src/app/components/user/message-modal/message-modal.component.ts
@@ -16,9 +16,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class MessageModalComponent implements OnInit{
 
-  @Output() messagesUpdated = new EventEmitter<Message[]>();
   userList: User[] = [];
-  messageList: Message[] = [];
   message: Message = {
     id: 0,
     context: '',
@@ -37,17 +35,6 @@ export class MessageModalComponent implements OnInit{
 
   ngOnInit(): void {
     this.getUsers();
-    this.getMessages();
-  }
-
-  getMessages(): void{
-    this.messageService.getMessages(1).subscribe({
-      next:(list)=>{
-        this.messageList = list;
-        const filteredList = list.filter(message => !message.read)
-        this.messagesUpdated.emit(filteredList);
-      }
-    });
   }
 
   getUsers(): void{
@@ -76,43 +63,6 @@ export class MessageModalComponent implements OnInit{
         this.notification.showErrorToast('Error: ' + message, 'top', 3000);
       }
     });
-  }
-
-  isReadMessage(message: Message): void{
-    if(!message.read && !message.delete){
-      this.messageService.readMessage(message).subscribe({
-        next:(response)=>{
-          const message = response.message;
-          console.log('Ok: ', message);
-          this.getMessages();
-        },error: (error)=>{
-          const message = error.error?.error || error.error.message;
-          console.log('Error: ', message);
-        }
-      });
-    }else{
-      console.log('Mensaje se encuentra eliminado o leído.');
-    }
-  }
-
-  isDeleteMessage(message: Message): void{
-    if(message.read){
-      this.messageService.deleteMessage(message).subscribe({
-        next:(response)=>{
-          const messageResponse = response.message;
-          console.log('Ok: ', messageResponse);
-          message.delete = true;
-          setTimeout(() => {
-            this.getMessages();
-          }, 3000);
-        },error: (error)=>{
-          const message = error.error?.error || error.error.message;
-          console.log('Error: ', message);
-        }
-      });
-    }else{
-      console.log('Mensaje no se encuentra leído.');
-    }
   }
 
   resetModal(): void{
