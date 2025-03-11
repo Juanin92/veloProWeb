3a0f78c53039e4e0a7ab5f8fe0caa6c57cb807dc
Juanin92 <j.claveria19@com>
2025-03-11 13:11:07 -0300

    feat(user): Create UpdateUserDTO class

---
+++ veloProWeb/src/main/java/com/veloProWeb/Model/DTO/UpdateUserDTO.java
@@ -0,0 +1,16 @@
+package com.veloProWeb.Model.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateUserDTO {
+    private String username;
+    private String email;
+    private String currentPassword;
+    private String newPassword;
+    private String confirmPassword;
+}
