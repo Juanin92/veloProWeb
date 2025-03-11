6f6e63ca555facb1c8d3cbd4d97fc5dadfaf0727
Juanin92 <j.claveria19@com>
2025-03-11 20:01:06 -0300

    feat(user): Create User DTO

---
+++ veloProWeb/src/main/java/com/veloProWeb/Model/DTO/UserDTO.java
@@ -0,0 +1,18 @@
+package com.veloProWeb.Model.DTO;
+
+import com.veloProWeb.Model.Enum.Rol;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private String name;
+    private String surname;
+    private String username;
+    private String rut;
+    private String email;
+    private Rol role;
+}
