0726dc029bda5547a1af012457da3ad246c36957
Juanin92 <j.claveria19@com>
2025-03-11 13:12:32 -0300

    feat(user): Create updateUserData method

--- veloProWeb/src/main/java/com/veloProWeb/Service/User/Interface/IUserService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/User/Interface/IUserService.java
@@ -1,6 +1,8 @@
 package com.veloProWeb.Service.User.Interface;
 
+import com.veloProWeb.Model.DTO.UpdateUserDTO;
 import com.veloProWeb.Model.Entity.User.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.List;
 
@@ -15,4 +17,5 @@ public interface IUserService {
     User getAuthUserToken(String username, String token);
     void sendEmailCode(User user);
     void sendEmailUpdatePassword(User user);
+    void updateUserData(UpdateUserDTO dto, String username);
 }
