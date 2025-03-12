551d28741bbc51045b590042138e804e9a573ddb
Juanin92 <j.claveria19@com>
2025-03-11 21:01:19 -0300

    feat(user): Create userDTO class

---
+++ veloProweb-frontend/src/app/models/DTO/user-dto.ts
@@ -0,0 +1,12 @@
+import { Role } from "../enum/role";
+
+export interface UserDTO {
+    name: string;
+    surname: string;
+    username: string;
+    rut: string;
+    email: string;
+    token: string;
+    status: boolean;
+    role: Role | null;
+}
