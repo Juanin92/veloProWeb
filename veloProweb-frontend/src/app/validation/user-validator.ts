8cda1641d37f422a1d3af9a7aa28963c13284fc1
Juanin92 <j.claveria19@com>
2025-03-12 11:51:08 -0300

    feat(validator): adding validateUsername, validateEmail, validatePassword method in user validator class

--- veloProweb-frontend/src/app/validation/user-validator.ts
+++ veloProweb-frontend/src/app/validation/user-validator.ts
@@ -1,39 +1,49 @@
 import { User } from "../models/Entity/user";
 
 export class UserValidator {
-    static validateForm(selectedUser: User): boolean{
-            return this.isFieldValid(selectedUser, 'name') &&
-                this.isFieldValid(selectedUser, 'surname') &&
-                this.isFieldValid(selectedUser, 'username') &&
-                this.isFieldValid(selectedUser, 'rut') &&
-                this.isFieldValid(selectedUser, 'password') &&
-                this.isFieldValid(selectedUser, 'email') && 
-                this.isFieldValid(selectedUser, 'role');
+    static validateForm(selectedUser: User): boolean {
+        return this.isFieldValid(selectedUser, 'name') &&
+            this.isFieldValid(selectedUser, 'surname') &&
+            this.isFieldValid(selectedUser, 'username') &&
+            this.isFieldValid(selectedUser, 'rut') &&
+            this.isFieldValid(selectedUser, 'password') &&
+            this.isFieldValid(selectedUser, 'email') &&
+            this.isFieldValid(selectedUser, 'role');
+    }
+
+    static isFieldValid(user: User, fieldName: keyof User): boolean {
+        const value = user[fieldName];
+        if (fieldName === 'name') {
+            return value !== null && typeof value === 'string' && value.trim().length > 3 && /^[a-zA-Z ]+$/.test(value.trim());
         }
-    
-        static isFieldValid(user: User, fieldName: keyof User): boolean{
-            const value = user[fieldName];
-            if (fieldName === 'name') {
-                return value !== null && typeof value === 'string' && value.trim().length > 3 && /^[a-zA-Z ]+$/.test(value.trim());
-            }
-            if (fieldName === 'surname') {
-                return value !== null && typeof value === 'string' && value.trim().length > 3 && /^[a-zA-Z ]+$/.test(value.trim());
-            }
-            if (fieldName === 'username') {
-                return value !== null && typeof value === 'string' && value.trim().length > 3;
-            }
-            if (fieldName === 'rut') {
-                return value !== null && typeof value === 'string' && /^\d{7,8}-\d|[kK]$$/.test(value);
-            }
-            if (fieldName === 'email') {
-                return value !== null && typeof value === 'string' && /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
-            }
-            if (fieldName === 'password') {
-                return value !== null && typeof value === 'string' && value.trim().length >= 7;
-            }
-            if (fieldName === 'role') {
-                return value !== null;
-            }
-            return true;
+        if (fieldName === 'surname') {
+            return value !== null && typeof value === 'string' && value.trim().length > 3 && /^[a-zA-Z ]+$/.test(value.trim());
         }
+        if (fieldName === 'username') {
+            return value !== null && typeof value === 'string' && value.trim().length > 3;
+        }
+        if (fieldName === 'rut') {
+            return value !== null && typeof value === 'string' && /^\d{7,8}-\d|[kK]$$/.test(value);
+        }
+        if (fieldName === 'email') {
+            return value !== null && typeof value === 'string' && /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
+        }
+        if (fieldName === 'password') {
+            return value !== null && typeof value === 'string' && value.trim().length >= 7;
+        }
+        if (fieldName === 'role') {
+            return value !== null;
+        }
+        return true;
+    }
+
+    static validateUsername(username: string): boolean{
+        return username !== null && typeof username === 'string' && username.trim().length > 3;
+    }
+    static validateEmail(email: string): boolean{
+        return email !== null && typeof email === 'string' && /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+    }
+    static validatePassword(password: string): boolean{
+        return password !== null && typeof password === 'string' && password.trim().length > 3;
+    }
 }
