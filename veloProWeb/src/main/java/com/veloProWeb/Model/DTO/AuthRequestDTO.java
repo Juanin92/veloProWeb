9e17b26baf48fd024d26fef0d7a7f373cca0fe44
Juanin92 <j.claveria19@com>
2025-03-13 16:36:17 -0300

    feat(authRequest): Create AuthRequestDTO class

---
+++ veloProWeb/src/main/java/com/veloProWeb/Model/DTO/AuthRequestDTO.java
@@ -0,0 +1,13 @@
+package com.veloProWeb.Model.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequestDTO {
+    private String identifier;
+    private String token;
+}
