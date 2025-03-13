a19ce983c033cb6699ebcadbc907dd71e256f7be
Juanin92 <j.claveria19@com>
2025-03-13 19:56:16 -0300

    feat(task): import record service to endpoints in task controller

--- veloProWeb/src/main/java/com/veloProWeb/Controller/User/TaskController.java
+++ veloProWeb/src/main/java/com/veloProWeb/Controller/User/TaskController.java
@@ -1,6 +1,7 @@
 package com.veloProWeb.Controller.User;
 
 import com.veloProWeb.Model.DTO.TaskDTO;
+import com.veloProWeb.Service.Record.IRecordService;
 import com.veloProWeb.Service.User.Interface.ITaskService;
 import com.veloProWeb.Service.User.Interface.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,7 @@ public class TaskController {
 
     @Autowired private ITaskService taskService;
     @Autowired private IUserService userService;
+    @Autowired private IRecordService recordService;
 
     /**
      * Obtiene tareas asignadas de un usuario
@@ -30,6 +32,7 @@ public class TaskController {
     @GetMapping()
     public ResponseEntity<List<TaskDTO>> getTasks(@AuthenticationPrincipal UserDetails userDetails){
         try{
+            recordService.registerAction(userDetails, "TASK", "Tareas vista");
             return ResponseEntity.ok(taskService.getTaskByUser(userDetails.getUsername()));
         }catch (IllegalArgumentException e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
@@ -42,14 +45,17 @@ public class TaskController {
      * @return - ResponseEntity con un mensaje de éxito o error según sea el caso
      */
     @PostMapping
-    public ResponseEntity<Map<String, String>> createTask(@RequestBody TaskDTO task){
+    public ResponseEntity<Map<String, String>> createTask(@RequestBody TaskDTO task,
+                                                          @AuthenticationPrincipal UserDetails userDetails){
         HashMap<String, String> response = new HashMap<>();
         try{
             taskService.createTask(task);
+            recordService.registerAction(userDetails, "TASK", "Creación de tarea para " + task.getUser());
             response.put("message", "Tarea asignada correctamente");
             return ResponseEntity.ok(response);
         }catch (IllegalArgumentException e){
             response.put("Error", e.getMessage());
+            recordService.registerAction(userDetails, "TASK_FAILURE", "ERROR: crear tarea : " + e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
     }
@@ -60,14 +66,17 @@ public class TaskController {
      * @return - ResponseEntity con un mensaje de éxito o error según sea el caso
      */
     @PutMapping
-    public ResponseEntity<Map<String, String>> completeTask(@RequestParam Long taskID){
+    public ResponseEntity<Map<String, String>> completeTask(@RequestParam Long taskID,
+                                                            @AuthenticationPrincipal UserDetails userDetails){
         HashMap<String, String> response = new HashMap<>();
         try{
             taskService.completeTask(taskID);
+            recordService.registerAction(userDetails, "TASK", "Tarea completada");
             response.put("message", "Tarea Completada");
             return ResponseEntity.ok(response);
         }catch (IllegalArgumentException e){
             response.put("Error", e.getMessage());
+            recordService.registerAction(userDetails, "TASK_FAILURE", "ERROR: Completar tarea : " + e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
     }
