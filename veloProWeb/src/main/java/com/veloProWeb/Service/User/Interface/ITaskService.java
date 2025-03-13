c5388f263078ee3b3995740f53a1dad7beb7bf55
Juanin92 <j.claveria19@com>
2025-03-13 19:27:48 -0300

    feat(task): modify getTaskByUser method

--- veloProWeb/src/main/java/com/veloProWeb/Service/User/Interface/ITaskService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/User/Interface/ITaskService.java
@@ -1,12 +1,11 @@
 package com.veloProWeb.Service.User.Interface;
 
 import com.veloProWeb.Model.DTO.TaskDTO;
-import com.veloProWeb.Model.Entity.User.Task;
 
 import java.util.List;
 
 public interface ITaskService {
-    List<Task> getTaskByUser(Long userID);
+    List<TaskDTO> getTaskByUser(String username);
     void createTask(TaskDTO dto);
     void completeTask(Long taskID);
     List<TaskDTO> getAllTasks();
