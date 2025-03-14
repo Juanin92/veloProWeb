dabfe8a09d51d5b1ff890a687afc37fbfcc36c9c
Juanin92 <j.claveria19@com>
2025-03-13 21:23:07 -0300

    feat(message): adding authService

--- veloProweb-frontend/src/app/services/User/message.service.ts
+++ veloProweb-frontend/src/app/services/User/message.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Message } from '../../models/Entity/message';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +11,21 @@ export class MessageService {
 
   private apiUrl = 'http://localhost:8080/mensajes';
   
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private auth: AuthService) { }
 
   getMessages(userID: number): Observable<Message[]>{
     return this.httpClient.get<Message[]>(this.apiUrl,{params:{userId: userID.toString()}});
   }
 
   readMessage(message: Message): Observable<{message: string}>{
-    return this.httpClient.put<{message: string}>(this.apiUrl, message);
+    return this.httpClient.put<{message: string}>(this.apiUrl, message, {headers: this.auth.getAuthHeaders()});
   }
 
   deleteMessage(message: Message): Observable<{message: string}>{
-    return this.httpClient.put<{message: string}>(`${this.apiUrl}/eliminar`, message);
+    return this.httpClient.put<{message: string}>(`${this.apiUrl}/eliminar`, message, {headers: this.auth.getAuthHeaders()});
   }
 
   sendMessage(message: Message): Observable<{message: string}>{
-    return this.httpClient.post<{message: string}>(this.apiUrl, message);
+    return this.httpClient.post<{message: string}>(this.apiUrl, message, {headers: this.auth.getAuthHeaders()});
   }
 }
