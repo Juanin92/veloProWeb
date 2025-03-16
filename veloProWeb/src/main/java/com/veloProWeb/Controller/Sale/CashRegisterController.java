2491610a9ce698626d49e83c25036856e06c38f9
Juanin92 <j.claveria19@com>
2025-03-16 17:19:24 -0300

    feat(cashierRegister): Add recordService, preAuthorize and AuthenticationPrincipal annotation in cashierRegister controller

--- veloProWeb/src/main/java/com/veloProWeb/Controller/Sale/CashRegisterController.java
+++ veloProWeb/src/main/java/com/veloProWeb/Controller/Sale/CashRegisterController.java
@@ -1,8 +1,12 @@
 package com.veloProWeb.Controller.Sale;
 
 import com.veloProWeb.Model.Entity.Sale.CashRegister;
+import com.veloProWeb.Service.Record.IRecordService;
 import com.veloProWeb.Service.Sale.Interface.ICashRegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +24,16 @@ import java.util.List;
 public class CashRegisterController {
 
     @Autowired private ICashRegisterService cashRegisterService;
+    @Autowired private IRecordService recordService;
 
     /**
      * Obtiene una lista con los registros de caja
      * @return - lista con los registros
      */
     @GetMapping
-    public List<CashRegister> getCashRegisters(){
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'MASTER')")
+    public List<CashRegister> getCashRegisters(@AuthenticationPrincipal UserDetails userDetails){
+        recordService.registerAction(userDetails, "VIEW", "Lista de registro de la caja");
         return cashRegisterService.getAll();
     }
 }
