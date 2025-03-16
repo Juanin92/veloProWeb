f0ba4df0dff74c1b4880b23675a4ae6ff3702b29
Juanin92 <j.claveria19@com>
2025-03-16 17:56:06 -0300

    feat(cashRegister): Add authService in cashRegister service

--- veloProweb-frontend/src/app/services/Sale/cash-register.service.ts
+++ veloProweb-frontend/src/app/services/Sale/cash-register.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CashRegister } from '../../models/Entity/cash-register';
+import { AuthService } from '../User/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,9 @@ export class CashRegisterService {
 
   private apiUrl = 'http://localhost:8080/caja';
   
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private auth: AuthService) { }
 
     getCashRegisters(): Observable<CashRegister[]>{
-      return this.http.get<CashRegister[]>(this.apiUrl);
+      return this.http.get<CashRegister[]>(this.apiUrl, {headers: this.auth.getAuthHeaders()});
     }
 }
