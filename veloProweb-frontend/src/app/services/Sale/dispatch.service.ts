5cc7ac7e3aafdbfe0156e3e6102b34e04e2908c0
Juanin92 <j.claveria19@com>
2025-03-16 17:55:48 -0300

    feat(dispatch): Add authService in dispatch service

--- veloProweb-frontend/src/app/services/Sale/dispatch.service.ts
+++ veloProweb-frontend/src/app/services/Sale/dispatch.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Dispatch } from '../../models/Entity/Sale/dispatch';
 import { DetailSaleRequestDTO } from '../../models/DTO/detail-sale-request-dto';
+import { AuthService } from '../User/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,21 +12,22 @@ export class DispatchService {
 
   private apiUrl = 'http://localhost:8080/despachos';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private auth: AuthService) { }
 
   getDispatches(): Observable<Dispatch[]>{
     return this.http.get<Dispatch[]>(this.apiUrl);
   };
 
   createDispatch(dispatch: Dispatch): Observable<{message: string}>{
-    return this.http.post<{message: string}>(this.apiUrl, dispatch);
+    return this.http.post<{message: string}>(this.apiUrl, dispatch, {headers: this.auth.getAuthHeaders()});
   }
 
   handleStatusDispatch(dispatchID: number, action: number): Observable<{message: string}>{
     return this.http.put<{message: string}>(this.apiUrl, {}, {
       params:{
         dispatchID: dispatchID.toString(),
-        action: action.toString()}
+        action: action.toString()},
+        headers: this.auth.getAuthHeaders()
     });
   }
 
