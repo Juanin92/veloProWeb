11fe3998bb7dc1e6d9023a2254343103c022fd4f
Juanin92 <j.claveria19@com>
2025-03-15 22:01:13 -0300

    feat(brand): adding auth service to methods

--- veloProweb-frontend/src/app/services/Product/brand.service.ts
+++ veloProweb-frontend/src/app/services/Product/brand.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Brand } from '../../models/Entity/Product/brand';
 import { Observable } from 'rxjs';
+import { AuthService } from '../User/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,14 @@ export class BrandService {
 
   private apiUrl = 'http://localhost:8080/marcas';
   
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private auth: AuthService) { }
 
   /**
    * Obtiene una lista de todas las marcas desde el endpoint
    * @returns observable que emite una lista de marcas
    */
   getBrands(): Observable<Brand[]>{
-    return this.httpClient.get<Brand[]>(this.apiUrl);
+    return this.httpClient.get<Brand[]>(this.apiUrl, {headers: this.auth.getAuthHeaders()});
   }
 
   /**
@@ -26,6 +27,6 @@ export class BrandService {
    * @returns - Observable que emite un mensaje de confirmación o error
    */
   createBrand(brand: Brand): Observable<{message: string}>{
-    return this.httpClient.post<{message: string}>(`${this.apiUrl}`, brand);
+    return this.httpClient.post<{message: string}>(`${this.apiUrl}`, brand, {headers: this.auth.getAuthHeaders()});
   }
 }
