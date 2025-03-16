0234a892134f0753c219dc20d1b4039e6a9c9ccd
Juanin92 <j.claveria19@com>
2025-03-15 22:02:18 -0300

    feat(brand): adding userService, RecordService, AuthenticationPrincipal annotation to all endpoints

--- veloProWeb/src/main/java/com/veloProWeb/Controller/Product/BrandController.java
+++ veloProWeb/src/main/java/com/veloProWeb/Controller/Product/BrandController.java
@@ -2,9 +2,13 @@ package com.veloProWeb.Controller.Product;
 
 import com.veloProWeb.Model.Entity.Product.BrandProduct;
 import com.veloProWeb.Service.Product.Interfaces.IBrandService;
+import com.veloProWeb.Service.Record.IRecordService;
+import com.veloProWeb.Service.User.Interface.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -21,14 +25,20 @@ import java.util.Map;
 public class BrandController {
 
     @Autowired private IBrandService brandService;
+    @Autowired private IUserService userService;
+    @Autowired private IRecordService recordService;
 
     /**
      * Obtiene una lista de todas las marcas
      * @return - ResponseEntity con una lista de las marcas
      */
     @GetMapping
-    public ResponseEntity<List<BrandProduct>> getAllBrands(){
-        return ResponseEntity.ok(brandService.getAll());
+    public ResponseEntity<List<BrandProduct>> getAllBrands(@AuthenticationPrincipal UserDetails userDetails){
+        if (userService.hasRequiredRole(userDetails, "ADMIN", "MASTER")){
+            return ResponseEntity.ok(brandService.getAll());
+        }else{
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     /**
@@ -37,14 +47,24 @@ public class BrandController {
      * @return - ResponseEntity con un mensaje de éxito o error según sea el caso
      */
     @PostMapping
-    public ResponseEntity<Map<String, String>> createBrand(@RequestBody BrandProduct brand){
+    public ResponseEntity<Map<String, String>> createBrand(@RequestBody BrandProduct brand,
+                                                           @AuthenticationPrincipal UserDetails userDetails){
         Map<String, String> response = new HashMap<>();
-        try {
-            brandService.save(brand);
-            response.put("message", "Marca registrada correctamente");
-            return ResponseEntity.ok(response);
+        try{
+            if(userService.hasRequiredRole(userDetails, "ADMIN", "MASTER")){
+                brandService.save(brand);
+                response.put("message", "Marca registrada correctamente");
+                recordService.registerAction(userDetails, "CREATE", "Marca creada: " + brand.getName());
+                return ResponseEntity.ok(response);
+            }else {
+                recordService.registerAction(userDetails, "CREATE_FAILURE",
+                        "Error: " + userDetails.getUsername() + " ingreso indebido");
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            }
         }catch (IllegalArgumentException e){
-            response.put("message", e.getMessage());
+            response.put("message",e.getMessage());
+            recordService.registerAction(userDetails, "CREATE_FAILURE",
+                    "ERROR: crear marca(" + brand.getName() + "): " + e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
     }
