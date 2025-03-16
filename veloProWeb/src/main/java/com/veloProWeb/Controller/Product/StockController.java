5ea7a23d110c024c2db12377fc6d337764d6397f
Juanin92 <j.claveria19@com>
2025-03-16 13:04:14 -0300

    feat(stock): Adding PreAuthorize annotation, Delete userService

--- veloProWeb/src/main/java/com/veloProWeb/Controller/Product/StockController.java
+++ veloProWeb/src/main/java/com/veloProWeb/Controller/Product/StockController.java
@@ -3,10 +3,10 @@ package com.veloProWeb.Controller.Product;
 import com.veloProWeb.Model.Entity.Product.Product;
 import com.veloProWeb.Service.Product.Interfaces.IProductService;
 import com.veloProWeb.Service.Record.IRecordService;
-import com.veloProWeb.Service.User.Interface.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +21,6 @@ import java.util.Map;
 public class StockController {
 
     @Autowired private IProductService productService;
-    @Autowired private IUserService userService;
     @Autowired private IRecordService recordService;
 
     /**
@@ -29,12 +28,9 @@ public class StockController {
      * @return - ResponseEntity con una lista de los productos
      */
     @GetMapping()
-    public ResponseEntity<List<Product>> getListProducts(@AuthenticationPrincipal UserDetails userDetails){
-        if (userService.hasRequiredRole(userDetails, "ADMIN", "MASTER", "SELLER")){
-            return ResponseEntity.ok(productService.getAll());
-        }else{
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
+    @PreAuthorize("hasAnyAuthority('ADMIN','MASTER','SELLER')")
+    public ResponseEntity<List<Product>> getListProducts(){
+        return ResponseEntity.ok(productService.getAll());
     }
 
     /**
@@ -43,20 +39,15 @@ public class StockController {
      * @return - ResponseEntity con un mensaje de éxito o error según sea el caso
      */
     @PostMapping()
+    @PreAuthorize("hasAnyAuthority('ADMIN','MASTER')")
     public ResponseEntity<Map<String, String>> createProduct(@RequestBody Product product,
                                                              @AuthenticationPrincipal UserDetails userDetails){
         Map<String, String> response =  new HashMap<>();
         try{
-            if(userService.hasRequiredRole(userDetails, "ADMIN", "MASTER")){
-                productService.create(product);
-                response.put("message","Producto creado exitosamente!");
-                recordService.registerAction(userDetails, "CREATE", "Producto creado: " + product.getDescription());
-                return ResponseEntity.ok(response);
-            }else {
-                recordService.registerAction(userDetails, "CREATE_FAILURE",
-                        "Error: " + userDetails.getUsername() + " ingreso indebido");
-                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-            }
+            productService.create(product);
+            response.put("message","Producto creado exitosamente!");
+            recordService.registerAction(userDetails, "CREATE", "Producto creado: " + product.getDescription());
+            return ResponseEntity.ok(response);
         }catch (IllegalArgumentException e){
             response.put("message",e.getMessage());
             recordService.registerAction(userDetails, "CREATE_FAILURE",
@@ -71,20 +62,15 @@ public class StockController {
      * @return - ResponseEntity con un mensaje de éxito o error según sea el caso
      */
     @PutMapping()
+    @PreAuthorize("hasAnyAuthority('ADMIN','MASTER')")
     public ResponseEntity<Map<String, String>> updateProduct(@RequestBody Product product,
                                                              @AuthenticationPrincipal UserDetails userDetails){
         Map<String, String> response = new HashMap<>();
         try{
-            if(userService.hasRequiredRole(userDetails, "ADMIN", "MASTER")){
-                productService.update(product);
-                response.put("message", "Producto actualizado exitosamente!");
-                recordService.registerAction(userDetails, "UPDATE", "Producto actualizado: " + product.getDescription());
-                return ResponseEntity.ok(response);
-            }else {
-                recordService.registerAction(userDetails, "UPDATE_FAILURE",
-                        "Error: " + userDetails.getUsername() + " ingreso indebido");
-                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-            }
+            productService.update(product);
+            response.put("message", "Producto actualizado exitosamente!");
+            recordService.registerAction(userDetails, "UPDATE", "Producto actualizado: " + product.getDescription());
+            return ResponseEntity.ok(response);
         }catch (IllegalArgumentException e){
             response.put("message",e.getMessage());
             recordService.registerAction(userDetails, "UPDATE_FAILURE",
@@ -99,20 +85,15 @@ public class StockController {
      * @return - ResponseEntity con un mensaje de éxito o error según sea el caso
      */
     @PutMapping("/eliminar_producto")
+    @PreAuthorize("hasAnyAuthority('ADMIN','MASTER')")
     public ResponseEntity<Map<String, String>> deleteProduct(@RequestBody Product product,
                                                              @AuthenticationPrincipal UserDetails userDetails){
         Map<String, String> response = new HashMap<>();
         try{
-            if(userService.hasRequiredRole(userDetails, "ADMIN", "MASTER")){
-                productService.delete(product);
-                response.put("message", "Producto eliminado exitosamente!");
-                recordService.registerAction(userDetails, "DELETE", "Producto eliminado: " + product.getDescription());
-                return ResponseEntity.ok(response);
-            }else {
-                recordService.registerAction(userDetails, "DELETE_FAILURE",
-                        "Error: " + userDetails.getUsername() + " ingreso indebido");
-                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-            }
+            productService.delete(product);
+            response.put("message", "Producto eliminado exitosamente!");
+            recordService.registerAction(userDetails, "DELETE", "Producto eliminado: " + product.getDescription());
+            return ResponseEntity.ok(response);
         }catch (IllegalArgumentException e){
             response.put("message",e.getMessage());
             recordService.registerAction(userDetails, "DELETE_FAILURE",
@@ -127,20 +108,15 @@ public class StockController {
      * @return - ResponseEntity con un mensaje de éxito o error según sea el caso
      */
     @PutMapping("/activar_producto")
+    @PreAuthorize("hasAnyAuthority('ADMIN','MASTER')")
     public ResponseEntity<Map<String, String>> activeProduct(@RequestBody Product product,
                                                              @AuthenticationPrincipal UserDetails userDetails){
         Map<String, String> response = new HashMap<>();
         try{
-            if(userService.hasRequiredRole(userDetails, "ADMIN", "MASTER")){
-                productService.active(product);
-                response.put("message", "Producto activado exitosamente!");
-                recordService.registerAction(userDetails, "ACTIVATE", "Producto activado: " + product.getDescription());
-                return ResponseEntity.ok(response);
-            }else {
-                recordService.registerAction(userDetails, "ACTIVATE_FAILURE",
-                        "Error: " + userDetails.getUsername() + " ingreso indebido");
-                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-            }
+            productService.active(product);
+            response.put("message", "Producto activado exitosamente!");
+            recordService.registerAction(userDetails, "ACTIVATE", "Producto activado: " + product.getDescription());
+            return ResponseEntity.ok(response);
         }catch (IllegalArgumentException e){
             response.put("message",e.getMessage());
             recordService.registerAction(userDetails, "ACTIVATE_FAILURE",
