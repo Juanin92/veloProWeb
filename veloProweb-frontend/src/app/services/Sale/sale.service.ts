f6039d0f152d5e0e9944c70fb92b28369237f77d
Juanin92 <j.claveria19@com>
2025-03-16 17:55:10 -0300

    feat(sale): Add authService in sale service

--- veloProweb-frontend/src/app/services/Sale/sale.service.ts
+++ veloProweb-frontend/src/app/services/Sale/sale.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SaleRequestDTO } from '../../models/DTO/sale-request-dto';
 import { DetailSaleRequestDTO } from '../../models/DTO/detail-sale-request-dto';
+import { AuthService } from '../User/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class SaleService {
 
   private apiUrl = 'http://localhost:8080/ventas';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private auth: AuthService) { }
 
   /**
      * Crea un nueva venta realizando una petición POST a la API
@@ -19,7 +20,7 @@ export class SaleService {
      * @returns - Observable emite un mensaje de confirmación o error
      */
     createSale(saleRequest: SaleRequestDTO): Observable<{message: string}>{
-      return this.http.post<{message: string}>(`${this.apiUrl}`, saleRequest);
+      return this.http.post<{message: string}>(`${this.apiUrl}`, saleRequest, {headers: this.auth.getAuthHeaders()});
     }
   
   /**
@@ -35,7 +36,7 @@ export class SaleService {
    * @returns - Observable emite una lista DTO con los datos de ventas registradas
    */
   getAllSales(): Observable<SaleRequestDTO[]>{
-    return this.http.get<SaleRequestDTO[]>(`${this.apiUrl}/lista-venta`);
+    return this.http.get<SaleRequestDTO[]>(`${this.apiUrl}/lista-venta`, {headers: this.auth.getAuthHeaders()});
   }
 
   /**
@@ -50,6 +51,6 @@ export class SaleService {
   }
 
   createSaleFromDispatch(saleRequest: SaleRequestDTO): Observable<{message: string}>{
-    return this.http.post<{message: string}>(`${this.apiUrl}/venta_despacho`, saleRequest);
+    return this.http.post<{message: string}>(`${this.apiUrl}/venta_despacho`, saleRequest, {headers: this.auth.getAuthHeaders()});
   }
 }
