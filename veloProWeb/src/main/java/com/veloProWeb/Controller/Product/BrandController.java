e38af4770ed055216ccc68c9e6a53784ea7324ff
Juanin92 <j.claveria19@com>
2025-03-16 13:04:05 -0300

    feat(brand): Adding PreAuthorize annotation, Delete userService

--- veloProWeb/src/main/java/com/veloProWeb/Controller/Product/BrandController.java
+++ veloProWeb/src/main/java/com/veloProWeb/Controller/Product/BrandController.java
@@ -3,10 +3,10 @@ package com.veloProWeb.Controller.Product;
 import com.veloProWeb.Model.Entity.Product.BrandProduct;
 import com.veloProWeb.Service.Product.Interfaces.IBrandService;
 import com.veloProWeb.Service.Record.IRecordService;
-import com.veloProWeb.Service.User.Interface.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +25,6 @@ import java.util.Map;
 public class BrandController {
 
     @Autowired private IBrandService brandService;
-    @Autowired private IUserService userService;
     @Autowired private IRecordService recordService;
 
     /**
@@ -33,12 +32,9 @@ public class BrandController {
      * @return - ResponseEntity con una lista de las marcas
      */
     @GetMapping
-    public ResponseEntity<List<BrandProduct>> getAllBrands(@AuthenticationPrincipal UserDetails userDetails){
-        if (userService.hasRequiredRole(userDetails, "ADMIN", "MASTER")){
-            return ResponseEntity.ok(brandService.getAll());
-        }else{
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
+    @PreAuthorize("hasAnyAuthority('ADMIN','MASTER')")
+    public ResponseEntity<List<BrandProduct>> getAllBrands(){
+        return ResponseEntity.ok(brandService.getAll());
     }
 
     /**
@@ -47,20 +43,15 @@ public class BrandController {
      * @return - ResponseEntity con un mensaje de éxito o error según sea el caso
      */
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN','MASTER')")
     public ResponseEntity<Map<String, String>> createBrand(@RequestBody BrandProduct brand,
                                                            @AuthenticationPrincipal UserDetails userDetails){
         Map<String, String> response = new HashMap<>();
         try{
-            if(userService.hasRequiredRole(userDetails, "ADMIN", "MASTER")){
-                brandService.save(brand);
-                response.put("message", "Marca registrada correctamente");
-                recordService.registerAction(userDetails, "CREATE", "Marca creada: " + brand.getName());
-                return ResponseEntity.ok(response);
-            }else {
-                recordService.registerAction(userDetails, "CREATE_FAILURE",
-                        "Error: " + userDetails.getUsername() + " ingreso indebido");
-                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-            }
+            brandService.save(brand);
+            response.put("message", "Marca registrada correctamente");
+            recordService.registerAction(userDetails, "CREATE", "Marca creada: " + brand.getName());
+            return ResponseEntity.ok(response);
         }catch (IllegalArgumentException e){
             response.put("message",e.getMessage());
             recordService.registerAction(userDetails, "CREATE_FAILURE",
