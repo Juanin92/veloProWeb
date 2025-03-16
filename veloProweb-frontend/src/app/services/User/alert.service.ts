41008e775d8015eac3c79ccef39a3f4918d0cfcb
Juanin92 <j.claveria19@com>
2025-03-16 17:56:16 -0300

    feat(alert): Add authService in alert service

--- veloProweb-frontend/src/app/services/User/alert.service.ts
+++ veloProweb-frontend/src/app/services/User/alert.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AlertModel } from '../../models/Entity/alert-model';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,14 @@ export class AlertService {
 
   private apiUrl = 'http://localhost:8080/alertas';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private auth: AuthService) { }
 
   getAlerts(): Observable<AlertModel[]>{
     return this.httpClient.get<AlertModel[]>(this.apiUrl);
   }
 
   handleStatusAlert(alert: AlertModel, action: number): Observable<{message: string}>{
-    return this.httpClient.put<{message: string}>(this.apiUrl, alert,{params:{action: action.toString()}});
+    return this.httpClient.put<{message: string}>(this.apiUrl, alert,
+      {params:{action: action.toString()}, headers: this.auth.getAuthHeaders()});
   }
 }
