2cbb1cb9046d654365c8f62333a097d65e793dc6
Juanin92 <j.claveria19@com>
2025-03-16 17:55:34 -0300

    feat(kardex): Add authService in kardex service

--- veloProweb-frontend/src/app/services/Report/kardex-service.service.ts
+++ veloProweb-frontend/src/app/services/Report/kardex-service.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Kardex } from '../../models/Entity/kardex';
+import { AuthService } from '../User/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,9 @@ export class KardexServiceService {
 
   private apiUrl = 'http://localhost:8080/kardex';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private auth: AuthService) { }
 
   getAllKardex(): Observable<Kardex[]> {
-    return this.http.get<Kardex[]>(this.apiUrl);
+    return this.http.get<Kardex[]>(this.apiUrl, {headers: this.auth.getAuthHeaders()});
   }
 }
