0f4fb88d7b099ad07138bae674c6cadd8b6fc218
Juanin92 <j.claveria19@com>
2025-03-16 16:57:39 -0300

    feat(kardex): Add recordService, preAuthorize and AuthenticationPrincipal annotation in kardex controller

--- veloProWeb/src/main/java/com/veloProWeb/Controller/Report/KardexController.java
+++ veloProWeb/src/main/java/com/veloProWeb/Controller/Report/KardexController.java
@@ -1,10 +1,14 @@
 package com.veloProWeb.Controller.Report;
 
 import com.veloProWeb.Model.Entity.Kardex;
+import com.veloProWeb.Service.Record.IRecordService;
 import com.veloProWeb.Service.Report.IkardexService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,14 +22,17 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 public class KardexController {
     @Autowired private IkardexService kardexService;
+    @Autowired private IRecordService recordService;
 
     /**
      * Obtener todos los registros
      * @return - ResponseEntity emite una lista de los registros
      */
     @GetMapping
-    public ResponseEntity<List<Kardex>> getAllKardex(){
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'MASTER', 'WAREHOUSE')")
+    public ResponseEntity<List<Kardex>> getAllKardex(@AuthenticationPrincipal UserDetails userDetails){
         try{
+            recordService.registerAction(userDetails, "VIEW_KARDEX", "Observo Reporte producto");
             return ResponseEntity.ok(kardexService.getAll());
         }catch (Exception e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
