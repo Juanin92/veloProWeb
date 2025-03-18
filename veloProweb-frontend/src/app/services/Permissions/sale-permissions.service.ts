33313ccc61f4872fea34b2ab91bd04a9e867d993
Juanin92 <j.claveria19@com>
2025-03-18 15:13:34 -0300

    feat(Sale): Create productPermissionService

---
+++ veloProweb-frontend/src/app/services/Permissions/sale-permissions.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from '../User/auth.service';
+import { Role } from '../../models/enum/role';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SalePermissionsService {
+
+  constructor(private auth: AuthService) { }
+
+  canViewSale(): boolean{
+    return this.auth.getRole() !== Role.WAREHOUSE;
+  }
+
+  canLoanPayment(): boolean{
+    return this.auth.getRole() !== Role.GUEST;
+  }
+}
