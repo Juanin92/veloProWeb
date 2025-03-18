1187f40d8a8f1bc9e8926fb4869813386cf48034
Juanin92 <j.claveria19@com>
2025-03-18 15:56:30 -0300

    feat(Setting): Create settlingPermissionService class

---
+++ veloProweb-frontend/src/app/services/Permissions/setting-permissions.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from '../User/auth.service';
+import { Role } from '../../models/enum/role';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingPermissionsService {
+
+  constructor(private auth: AuthService) { }
+
+  canViewSetting(): boolean {
+    return this.auth.getRole() !== Role.WAREHOUSE && this.auth.getRole() !== Role.GUEST;
+  }
+  canViewCashierMovements(): boolean {
+    return this.auth.getRole() === Role.MASTER && this.auth.getRole() === Role.ADMIN;
+  }
+  canViewRegisters(): boolean {
+    return this.auth.getRole() === Role.MASTER;
+  }
+  canViewTaskLayout(): boolean {
+    return this.auth.getRole() === Role.MASTER && this.auth.getRole() === Role.ADMIN;
+  }
+  canViewDispatchLayout(): boolean{
+    return this.auth.getRole() !== Role.WAREHOUSE && this.auth.getRole() !== Role.GUEST;
+  }
+}
