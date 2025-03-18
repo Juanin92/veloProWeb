004239bf19b28a40a7a49e322208311de1132686
Juanin92 <j.claveria19@com>
2025-03-18 13:45:16 -0300

    feat(Product): Create productPermissionService class

---
+++ veloProweb-frontend/src/app/services/Permissions/product-permissions.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from '../User/auth.service';
+import { Role } from '../../models/enum/role';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductPermissionsService {
+
+  constructor(private auth: AuthService) { }
+
+  canAddProduct(): boolean{
+    return this.auth.getRole() !== Role.GUEST && this.auth.getRole() !== Role.SELLER;
+  }
+  canUpdateProduct(): boolean{
+    return this.auth.getRole() !== Role.GUEST && this.auth.getRole() !== Role.SELLER;
+  }
+
+  canAddCategories(): boolean{
+    return this.auth.getRole() !== Role.GUEST && this.auth.getRole() !== Role.SELLER;
+  }
+
+}
