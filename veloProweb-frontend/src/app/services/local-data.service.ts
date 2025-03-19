faee7f898f925570d40896e8947762f706fdea9d
Juanin92 <j.claveria19@com>
2025-03-19 18:10:02 -0300

    feat(localData): adding updateData, import auth Service

--- veloProweb-frontend/src/app/services/local-data.service.ts
+++ veloProweb-frontend/src/app/services/local-data.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LocalData } from '../models/Entity/local-data';
 import { Observable } from 'rxjs';
+import { AuthService } from './User/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,13 @@ export class LocalDataService {
 
   private apiUrl = 'http://localhost:8080/data';
       
-    constructor(private httpClient: HttpClient) { }
+    constructor(private httpClient: HttpClient, private auth: AuthService) { }
   
     getData(): Observable<LocalData[]>{
       return this.httpClient.get<LocalData[]>(this.apiUrl);
+    }
+    
+    updateData(data: LocalData): Observable<{message: string}>{
+      return this.httpClient.put<{message: string}>(this.apiUrl, data, {headers: this.auth.getAuthHeaders()});
     }
 }
