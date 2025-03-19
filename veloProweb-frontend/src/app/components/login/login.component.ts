0544d3b793783a1896113c7f7dde61604de28c20
Juanin92 <j.claveria19@com>
2025-03-19 14:53:11 -0300

    feat(login): adding getEncryptedKey method in login component

--- veloProweb-frontend/src/app/components/login/login.component.ts
+++ veloProweb-frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HomeComponent } from '../Home/home.component';
 import { LoginRequest } from '../../models/DTO/login-request';
@@ -6,6 +6,7 @@ import { AuthService } from '../../services/User/auth.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NotificationService } from '../../utils/notification-service.service';
+import { EncryptionService } from '../../security/encryption.service';
 
 @Component({
   selector: 'app-login',
@@ -14,8 +15,9 @@ import { NotificationService } from '../../utils/notification-service.service';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  encryptionKey: string = '';
   userLogin: LoginRequest = {
     username: '',
     password: ''
@@ -24,21 +26,40 @@ export class LoginComponent {
   constructor(
     private router: Router,
     private authService: AuthService,
-    private notification: NotificationService){}
+    private encryptionService: EncryptionService,
+    private notification: NotificationService) { }
 
-    isLoginUser() {
-      if (this.userLogin) {
-          this.authService.login(this.userLogin).subscribe({
-              next: (response) => {
-                  this.authService.saveToken(response.token);
-                  this.authService.saveRole(response.role);
-                  this.router.navigate(['/main/home']);
-              },
-              error: (error) => {
-                  console.error('login.component: login error', error);
-                  this.notification.showWarning('Error!', 'Las credenciales no son correctas, intente de nuevo');
-              }
-          });
+  ngOnInit(): void {
+    this.getEncryptedKey();
+  }
+
+  getEncryptedKey(): void{
+    this.authService.getEncryptionKey().subscribe({
+      next: (key) => { this.encryptionKey = key;},
+      error: (error) => {
+        const message = error.error?.error || error?.error;
+        console.log('Error: ', message);
       }
+    });
+  }
+
+  isLoginUser() {
+    if (this.userLogin && this.encryptionKey) {
+      const encryptedUser: LoginRequest = {
+        username: this.userLogin.username,
+        password: this.encryptionService.encryptPassword(this.userLogin.password, this.encryptionKey)
+      }
+      this.authService.login(encryptedUser).subscribe({
+        next: (response) => {
+          this.authService.saveToken(response.token);
+          this.authService.saveRole(response.role);
+          this.router.navigate(['/main/home']);
+        },
+        error: (error) => {
+          console.error('login.component: login error', error);
+          this.notification.showWarning('Error!', 'Las credenciales no son correctas, intente de nuevo');
+        }
+      });
+    }
   }
 }
