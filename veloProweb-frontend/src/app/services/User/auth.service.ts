2cc4ce8d164d5e2e007c555e85fb3ac58f82d709
Juanin92 <j.claveria19@com>
2025-03-19 14:52:44 -0300

    feat(auth): adding getEncryptionKey method in auth service

--- veloProweb-frontend/src/app/services/User/auth.service.ts
+++ veloProweb-frontend/src/app/services/User/auth.service.ts
@@ -9,22 +9,26 @@ import { Observable } from 'rxjs';
 export class AuthService {
 
   private apiUrl = 'http://localhost:8080/auth';
-    
+
   constructor(private httpClient: HttpClient) { }
 
-  login(loginRequest: LoginRequest): Observable<{token: string, role: string}> {
-    return this.httpClient.post<{token: string, role: string}>(`${this.apiUrl}/login`, loginRequest);
+  getEncryptionKey(): Observable<string> {
+    return this.httpClient.get(`${this.apiUrl}/encriptado`, { responseType: 'text' });
   }
 
-  logout(): Observable<{message: string}>{
-    return this.httpClient.post<{message: string}>(`${this.apiUrl}/logout`, {}, {headers: this.getAuthHeaders()});
+  login(loginRequest: LoginRequest): Observable<{ token: string, role: string }> {
+    return this.httpClient.post<{ token: string, role: string }>(`${this.apiUrl}/login`, loginRequest);
+  }
+
+  logout(): Observable<{ message: string }> {
+    return this.httpClient.post<{ message: string }>(`${this.apiUrl}/logout`, {}, { headers: this.getAuthHeaders() });
   }
 
   saveToken(token: string): void {
     localStorage.setItem('token', token);
   }
   saveRole(role: string): void {
-    localStorage.setItem('role', role); 
+    localStorage.setItem('role', role);
   }
 
   getToken(): string | null {
