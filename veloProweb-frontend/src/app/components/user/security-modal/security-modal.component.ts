635af916bcbf5a6770fb2a19078e4383fc6f4a15
Juanin92 <j.claveria19@com>
2025-03-19 14:56:19 -0300

    feat(security): Create security-modal component

---
+++ veloProweb-frontend/src/app/components/user/security-modal/security-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-security-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './security-modal.component.html',
+  styleUrl: './security-modal.component.css'
+})
+export class SecurityModalComponent {
+
+}
