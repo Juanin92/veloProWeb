b1b0c1a878cf1279ae4b2b708696bc8610a3df60
Juanin92 <j.claveria19@com>
2025-03-20 19:34:37 -0300

    feat(DTO): Create CashRegisterDTO class

---
+++ veloProWeb/src/main/java/com/veloProWeb/Model/DTO/CashRegisterDTO.java
@@ -0,0 +1,27 @@
+package com.veloProWeb.Model.DTO;
+
+import com.veloProWeb.Model.Entity.User.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CashRegisterDTO {
+
+    private Long id;
+    private LocalDateTime dateOpening;
+    private LocalDateTime dateClosing;
+    private int amountOpening;
+    private int amountClosingCash;
+    private int amountClosingPos;
+    private String status;
+    private String comment;
+    private String user;
+}
