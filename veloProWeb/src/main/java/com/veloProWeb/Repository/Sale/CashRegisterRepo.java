6355de34b6087debeafc4beb7c2cbf4d50ad45dd
Juanin92 <j.claveria19@com>
2025-03-26 18:48:43 -0300

    feat(CashRegister): Adding existsOpenRegisterByUserAndDate repo method

--- veloProWeb/src/main/java/com/veloProWeb/Repository/Sale/CashRegisterRepo.java
+++ veloProWeb/src/main/java/com/veloProWeb/Repository/Sale/CashRegisterRepo.java
@@ -6,10 +6,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface CashRegisterRepo extends JpaRepository<CashRegister, Long> {
 
     @Query(value = "SELECT * FROM cash_register WHERE id_user = :userId AND status = 'OPEN' " +
             "ORDER BY date_opening DESC LIMIT 1", nativeQuery = true)
     CashRegister findLatestOpenRegisterByUser(@Param("userId") Long userId);
+
+    @Query("SELECT COUNT(c) > 0 FROM CashRegister c WHERE c.user.id = :userId AND " +
+            "DATE(c.dateOpening) = :dateOpening AND c.status = 'OPEN AND c.dateClosing IS NULL'")
+    boolean existsOpenRegisterByUserAndDate(@Param("userId") Long userId, @Param("dateOpening") LocalDate dateOpening);
 }
