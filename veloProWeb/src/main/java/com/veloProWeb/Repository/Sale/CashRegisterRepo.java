bfacf1d3551464e8597507da92772c49913cf88a
Juanin92 <j.claveria19@com>
2025-03-26 18:02:37 -0300

    feat(CashRegister): fixing query

--- veloProWeb/src/main/java/com/veloProWeb/Repository/Sale/CashRegisterRepo.java
+++ veloProWeb/src/main/java/com/veloProWeb/Repository/Sale/CashRegisterRepo.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CashRegisterRepo extends JpaRepository<CashRegister, Long> {
 
-    @Query(value = "SELECT * FROM cash_register WHERE id_user = :userId AND status = 'OPEN' ORDER BY date_opening DESC LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM cash_register WHERE id_user = :userId AND status = 'OPEN' " +
+            "ORDER BY date_opening DESC LIMIT 1", nativeQuery = true)
     CashRegister findLatestOpenRegisterByUser(@Param("userId") Long userId);
 }
