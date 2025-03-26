de2f39aac4a42c330e5c977423187f759d7d5e64
Juanin92 <j.claveria19@com>
2025-03-26 13:13:08 -0300

    feat(cashRegister): Adding methods and variables in cashier component

--- veloProweb-frontend/src/app/components/setting/cashier/cashier.component.ts
+++ veloProweb-frontend/src/app/components/setting/cashier/cashier.component.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
+import { CashRegisterService } from '../../../services/Sale/cash-register.service';
+import { CashRegister } from '../../../models/Entity/cash-register';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cashier',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './cashier.component.html',
   styleUrl: './cashier.component.css'
 })
 export class CashierComponent {
 
-  constructor(){}
+  cashier: CashRegister = this.initializeCashier();
+  canSale: boolean = false;
+  isOpening: boolean = true;
+  isClosing: boolean = false;
 
+  constructor(
+    private cashierService: CashRegisterService){}
+
+  saveOpeningRegister(): void{
+    this.isOpening = false;
+    this.isClosing = true;
+  }
+
+  saveClosingRegister(): void{}
+
+  initializeCashier(): CashRegister{
+    return {
+      dateOpening: '',
+      dateClosing: '',
+      amountOpening: 0,
+      amountClosingCash: 0,
+      amountClosingPos: 0,
+      status: false,
+      comment: '',
+      user: ''
+    }
+  }
 }
