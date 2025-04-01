2ffc74c58a652e937b391cdeb31a9deb8273890c
Juanin92 <j.claveria19@com>
2025-04-01 19:19:37 -0300

    feat(CashRegister): Delete addRegisterValidateComment method

--- veloProWeb/src/main/java/com/veloProWeb/Service/Sale/Interface/ICashRegisterService.java
+++ veloProWeb/src/main/java/com/veloProWeb/Service/Sale/Interface/ICashRegisterService.java
@@ -1,13 +1,13 @@
 package com.veloProWeb.Service.Sale.Interface;
 
 import com.veloProWeb.Model.DTO.CashRegisterDTO;
+import com.veloProWeb.Model.Entity.Sale.CashRegister;
 
 import java.util.List;
 
 public interface ICashRegisterService {
     void addRegisterOpening(String username, int amount);
     void addRegisterClosing(CashRegisterDTO cashRegister);
-    void addRegisterValidateComment(CashRegisterDTO cashRegister);
     List<CashRegisterDTO> getAll();
     void updateRegister(CashRegisterDTO cashRegister);
     boolean hasOpenRegisterOnDate(String username);
