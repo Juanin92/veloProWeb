c026f8c0f60775ad60b17ae1618b38f8a7a49972
Juanin92 <j.claveria19@com>
2025-04-01 21:18:41 -0300

    feat(menu): Enhance logout functionality with register validation and error notifications

--- veloProweb-frontend/src/app/components/menu/menu.component.ts
+++ veloProweb-frontend/src/app/components/menu/menu.component.ts
@@ -5,6 +5,9 @@ import { CommonModule } from '@angular/common';
 import { MenuPermissionsService } from '../../services/Permissions/menu-permissions.service';
 import { AuthService } from '../../services/User/auth.service';
 import { Role } from '../../models/enum/role';
+import { NotificationService } from '../../utils/notification-service.service';
+import { firstValueFrom } from 'rxjs';
+import { CashRegisterService } from '../../services/Sale/cash-register.service';
 
 @Component({
   selector: 'app-menu',
@@ -19,7 +22,9 @@ export class MenuComponent implements AfterViewInit{
   constructor(private tooltipService: TooltipService, 
     protected permission: MenuPermissionsService,
     private auth: AuthService,
-    private router: Router){}
+    private cashRegisterService: CashRegisterService,
+    private router: Router,
+    private notification: NotificationService){}
 
   ngAfterViewInit(): void {
     this.tooltipService.initializeTooltips();
@@ -31,16 +36,32 @@ export class MenuComponent implements AfterViewInit{
     } 
   }
 
-  logout(): void{
-    this.auth.logout().subscribe({
-      next:(response) =>{
-        console.log(response.message);
-        this.auth.removeToken();
-        this.router.navigate(['/login']);
-      }, error: (error) =>{
-        const message = error.error?.error || error.error?.message || error?.error;
-        console.log('Error: ', message);
+  async logout(): Promise<void> {
+      const hasOpenerRegister = await this.validateOpeningCashierRegister();
+      if(hasOpenerRegister) {
+        this.notification.showErrorToast('Debes registrar el cierre de caja para cerrar sesión', 'center', 3000);
+        return;
       }
-    });
-  }
+      this.auth.logout().subscribe({
+        next:(response) =>{
+          console.log(response.message);
+          this.auth.removeToken();
+          sessionStorage.clear();
+          localStorage.clear();
+          this.router.navigate(['/login']);
+        }, error: (error) =>{
+          const message = error.error?.error || error.error?.message || error?.error;
+          console.log('Error: ', message);
+        }
+      });
+    }
+  
+    async validateOpeningCashierRegister(): Promise<boolean> {
+      try {
+        return await firstValueFrom(this.cashRegisterService.hasOpenRegisterOnDate());
+      } catch (error) {
+        console.error("Error en la solicitud:", error);
+        return false;
+      }
+    }
 }
