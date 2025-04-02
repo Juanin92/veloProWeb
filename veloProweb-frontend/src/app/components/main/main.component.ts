33fdaf0f686d62d97574b6d4ad760ba5719e1188
Juanin92 <j.claveria19@com>
2025-04-01 21:12:07 -0300

    feat(main): Modify logout method, Adding validateOpeningCashierRegister and ngOnInit metohd in component

--- veloProweb-frontend/src/app/components/main/main.component.ts
+++ veloProweb-frontend/src/app/components/main/main.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { UpdateUserModalComponent } from "../user/update-user-modal/update-user-modal.component";
 import { MessageModalComponent } from "../user/message-modal/message-modal.component";
 import { MenuComponent } from "../menu/menu.component";
@@ -10,6 +10,9 @@ import * as bootstrap from 'bootstrap';
 import { AuthService } from '../../services/User/auth.service';
 import { CashierComponent } from "../setting/cashier/cashier.component";
 import { MenuPermissionsService } from '../../services/Permissions/menu-permissions.service';
+import { NotificationService } from '../../utils/notification-service.service';
+import { CashRegisterService } from '../../services/Sale/cash-register.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -19,21 +22,30 @@ import { MenuPermissionsService } from '../../services/Permissions/menu-permissi
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
-export class MainComponent implements AfterViewInit {
+export class MainComponent implements AfterViewInit, OnInit {
   isMenuActive: boolean = true;
   currentDate: string = '';
   localData: LocalData | null = null;
+  isOpen: boolean = false;
+  isOpeningRegister: boolean = false;
   @ViewChild('userDropdown') dropdownButton!: ElementRef;
   dropdownInstance!: bootstrap.Dropdown;
 
   constructor(
     private auth: AuthService,
+    private cashRegisterService: CashRegisterService,
     private datePipe: DatePipe,
     private localDataService: LocalDataService,
     private router: Router,
-    protected permission: MenuPermissionsService) {
+    protected permission: MenuPermissionsService,
+    private notification: NotificationService) {
     this.getDate();
     this.loadDataFromLocalStorage();
+  }
+
+  ngOnInit(): void {
+    const isOpenString = sessionStorage.getItem('isOpen');
+    this.isOpen = isOpenString !== null;
   }
 
   ngAfterViewInit(): void {
@@ -46,17 +58,33 @@ export class MainComponent implements AfterViewInit {
     return this.router.url === '/main/home';
   }
 
-  logout(): void{
+  async logout(): Promise<void> {
+    const hasOpenerRegister = await this.validateOpeningCashierRegister();
+    if(hasOpenerRegister) {
+      this.notification.showErrorToast('Debes registrar el cierre de caja para cerrar sesión', 'center', 3000);
+      return;
+    }
     this.auth.logout().subscribe({
       next:(response) =>{
         console.log(response.message);
         this.auth.removeToken();
+        sessionStorage.clear();
+        localStorage.clear();
         this.router.navigate(['/login']);
       }, error: (error) =>{
         const message = error.error?.error || error.error?.message || error?.error;
         console.log('Error: ', message);
       }
     });
+  }
+
+  async validateOpeningCashierRegister(): Promise<boolean> {
+    try {
+      return await firstValueFrom(this.cashRegisterService.hasOpenRegisterOnDate());
+    } catch (error) {
+      console.error("Error en la solicitud:", error);
+      return false;
+    }
   }
 
   toggleDropdown() {
