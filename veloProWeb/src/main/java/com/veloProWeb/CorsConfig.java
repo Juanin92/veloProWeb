8fddb555b466d9d010461e5b8c81561636f19423
Juanin92 <j.claveria19@com>
2025-05-03 15:08:40 -0400

    feat(CorsConfig): add CORS configuration to allow access from frontend

---
+++ veloProWeb/src/main/java/com/veloProWeb/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.veloProWeb;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${app.frontend.url}")
+    private String url;
+
+    /**
+     * Configuración de CORS para permitir el acceso desde el frontend
+     * @param corsRegistry - registro de CORS
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        corsRegistry.addMapping("/**")
+                .allowedOrigins(url)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
