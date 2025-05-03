56eeee8aed95031b53b2e23b88fab9e53bac3219
Juanin92 <j.claveria19@com>
2025-05-03 19:21:04 -0400

    feat(exceptions): add exception handlers for customer and validation errors

---
+++ veloProWeb/src/main/java/com/veloProWeb/exceptions/Handlers/CustomerExceptionsHandler.java
@@ -0,0 +1,33 @@
+package com.veloProWeb.exceptions.Handlers;
+
+import com.veloProWeb.exceptions.BaseExceptionHandler;
+import com.veloProWeb.exceptions.Customer.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class CustomerExceptionsHandler extends BaseExceptionHandler {
+
+    @ExceptionHandler({
+            CustomerAlreadyExistsException.class,
+            CustomerAlreadyActivatedException.class,
+            CustomerAlreadyDeletedException.class
+    })
+    public ResponseEntity<Map<String, String>> handleCustomerException(Exception e){
+        return buildResponse(e.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleCustomerNotFound(CustomerNotFoundException e) {
+        return buildResponse(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CustomerNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleCustomerNotValid(CustomerNotValidException e) {
+        return buildResponse(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ veloProWeb/src/main/java/com/veloProWeb/exceptions/Handlers/ValidationExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.veloProWeb.exceptions.Handlers;
+
+import com.veloProWeb.exceptions.BaseExceptionHandler;
+import com.veloProWeb.exceptions.Validation.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ValidationExceptionHandler extends BaseExceptionHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<Map<String, String>> handleValidation(ValidationException e) {
+        return buildResponse(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleSpringValidation(MethodArgumentNotValidException e){
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
