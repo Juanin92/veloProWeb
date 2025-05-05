ee5c971d47a1b6623b1079f28196d503047b9399
Juanin92 <j.claveria19@com>
2025-05-05 18:46:45 -0400

    refactor(customer): update CustomerValidator to use CustomerDTO for validation

--- veloProweb-frontend/src/app/validation/customer-validator.ts
+++ veloProweb-frontend/src/app/validation/customer-validator.ts
@@ -1,14 +1,16 @@
+import { CustomerDTO } from "../models/Entity/Customer/dto/customer-dto";
 import { Customer } from "../models/Entity/Customer/customer.model";
 
 export class CustomerValidator {
-    static validateForm(selectedCustomer: Customer): boolean {
+
+    static validateForm(selectedCustomer: CustomerDTO): boolean {
         return this.isFieldValid(selectedCustomer, 'name') &&
             this.isFieldValid(selectedCustomer, 'surname') &&
             this.isFieldValid(selectedCustomer, 'phone') &&
             this.isFieldValid(selectedCustomer, 'email');
     }
 
-    static isFieldValid(customer: Customer, fieldName: keyof Customer): boolean {
+    static isFieldValid(customer: CustomerDTO, fieldName: keyof CustomerDTO): boolean {
         const value = customer[fieldName];
         if (fieldName === 'email') {
             return !value || typeof value === 'string' && /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
