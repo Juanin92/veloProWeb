ac0f51d15dc6b19384ca783edffbd19f2186a078
Juanin92 <j.claveria19@com>
2025-05-05 18:49:17 -0400

    feat(customer): add CustomerDTO and CustomerResponseDTO for customer data management

---
+++ veloProWeb/src/main/java/com/veloProWeb/model/dto/customer/CustomerDTO.java
@@ -0,0 +1,36 @@
+package com.veloProWeb.model.dto.customer;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CustomerDTO {
+
+    @NotNull(message = "El id es obligatorio")
+    private Long id;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 3, message = "El nombre debe tener al menos 3 caracteres")
+    @Pattern(regexp = "^(?!null|NULL$)[\\p{L} ]+$", message = "El nombre solo puede contener letras")
+    private String name;
+
+    @NotBlank(message = "El apellido es obligatorio")
+    @Size(min = 3, message = "El apellido debe tener al menos 3 caracteres")
+    @Pattern(regexp = "^(?!null|NULL$)[\\p{L} ]+$", message = "El apellido solo puede contener letras")
+    private String surname;
+
+    @NotBlank(message = "El Teléfono es obligatorio")
+    @Pattern(regexp = "^\\+569 \\d{8}$", message = "El número debe tener el formato: +569 12345678")
+    private String phone;
+
+    private String email;
+}
