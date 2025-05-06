a9144b56739e85e27650c51a7ceceaa8c6f755fd
Juanin92 <j.claveria19@com>
2025-05-06 18:42:58 -0400

    feat(payment): add PaymentResponseDTO for handling payment response data

---
+++ veloProWeb/src/main/java/com/veloProWeb/model/dto/customer/PaymentResponseDTO.java
@@ -0,0 +1,19 @@
+package com.veloProWeb.model.dto.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentResponseDTO {
+    private int amount;
+    private String comment;
+    private LocalDate date;
+    private String document;
+}
