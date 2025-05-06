fd2b1ad841fcf41b02a58fcbeb568dc34e933661
Juanin92 <j.claveria19@com>
2025-05-06 18:05:34 -0400

    feat(exception): add NoTicketSelectedException for handling ticket selection errors

---
+++ veloProWeb/src/main/java/com/veloProWeb/exceptions/Customer/NoTicketSelectedException.java
@@ -0,0 +1,5 @@
+package com.veloProWeb.exceptions.Customer;
+
+public class NoTicketSelectedException extends RuntimeException{
+    public NoTicketSelectedException(String message){ super(message);}
+}
