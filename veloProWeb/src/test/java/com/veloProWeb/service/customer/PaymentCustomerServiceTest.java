233a93a310059f838a6949da2c17234fc47b0102
Juanin92 <j.claveria19@com>
2025-05-07 14:48:46 -0400

    test(service): enhance PaymentCustomerServiceTest with additional payment process validations and refactor test cases

--- veloProWeb/src/test/java/com/veloProWeb/service/customer/PaymentCustomerServiceTest.java
+++ veloProWeb/src/test/java/com/veloProWeb/service/customer/PaymentCustomerServiceTest.java
@@ -1,15 +1,21 @@
 package com.veloProWeb.service.customer;
 
+import com.veloProWeb.exceptions.Customer.InvalidPaymentAmountException;
+import com.veloProWeb.mapper.PaymentCustomerMapper;
 import com.veloProWeb.model.dto.customer.PaymentRequestDTO;
+import com.veloProWeb.model.dto.customer.PaymentResponseDTO;
 import com.veloProWeb.model.entity.customer.Customer;
 import com.veloProWeb.model.entity.customer.PaymentCustomer;
 import com.veloProWeb.model.entity.customer.TicketHistory;
 import com.veloProWeb.repository.customer.PaymentCustomerRepo;
+import com.veloProWeb.validation.PaymentCustomerValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
@@ -26,119 +32,220 @@ public class PaymentCustomerServiceTest {
     @Mock private PaymentCustomerRepo paymentCustomerRepo;
     @Mock private TicketHistoryService ticketService;
     @Mock private CustomerService customerService;
-    @Mock private TicketHistory ticketHistory;
+    @Spy private PaymentCustomerValidator validator;
+    @Spy private PaymentCustomerMapper mapper = new PaymentCustomerMapper();
     private PaymentCustomer paymentCustomer;
     private PaymentRequestDTO dto;
-    private TicketHistory ticketHistoryReal;
-    private TicketHistory ticketHistoryReal2;
-    private Customer realCustomer;
+    private TicketHistory ticketFirst, ticketSecond;
+    private Customer customer;
 
     @BeforeEach
     void setUp(){
-        paymentCustomer = new PaymentCustomer(1L,1000,"Test Comment", LocalDate.now(), realCustomer, ticketHistory);
-        realCustomer = new Customer();
-        realCustomer.setId(1L);
-        ticketHistoryReal = new TicketHistory();
-        ticketHistoryReal.setId(1L);
-        ticketHistoryReal.setCustomer(realCustomer);
-        ticketHistoryReal.setTotal(5000);
-        ticketHistoryReal2 = new TicketHistory();
-        ticketHistoryReal2.setId(2L);
-        ticketHistoryReal2.setCustomer(realCustomer);
-        ticketHistoryReal2.setTotal(5000);
-        dto = new PaymentRequestDTO();
-        dto.setAmount(10000);
-        dto.setCustomerID(realCustomer.getId());
-        dto.setComment("Efectivo");
-        dto.setTotalPaymentPaid(0);
-    }
-
-    //Prueba para crear un proceso de pagos de tickets
-    @Test
-    public void createPaymentProcess_validMultiplesTickets(){
-        dto.setTicketIDs(Arrays.asList(ticketHistoryReal.getId(),ticketHistoryReal2.getId()));
-        when(customerService.getCustomerById(1L)).thenReturn(realCustomer);
-        when(ticketService.getTicketByID(1L)).thenReturn(ticketHistoryReal);
-        when(ticketService.getTicketByID(2L)).thenReturn(ticketHistoryReal2);
-        paymentCustomerService.createPaymentProcess(dto);
-
-        verify(customerService, times(2)).paymentDebt(eq(realCustomer), eq("5000"));
-        verify(ticketService, times(1)).updateStatus(ticketHistoryReal);
-        verify(ticketService, times(1)).updateStatus(ticketHistoryReal2);
-
-    }
-    @Test
-    public void createPaymentProcess_invalidAmountMultiplesTickets() {
-        dto.setTicketIDs(Arrays.asList(ticketHistoryReal.getId(), ticketHistoryReal2.getId()));
-        dto.setAmount(8000);
-        when(customerService.getCustomerById(1L)).thenReturn(realCustomer);
-        when(ticketService.getTicketByID(1L)).thenReturn(ticketHistoryReal);
-        when(ticketService.getTicketByID(2L)).thenReturn(ticketHistoryReal2);
-
-        IllegalArgumentException exception = assertThrows(
-                IllegalArgumentException.class,
-                () -> paymentCustomerService.createPaymentProcess(dto)
-        );
-        assertEquals("El monto no es correcto para el pago de la deuda", exception.getMessage());
-    }
-    @Test
-    public void createPaymentProcess_adjustPaymentForSingleTicket() {
-        dto.setAmount(3000);
-        dto.setTotalPaymentPaid(2000);
-        dto.setTicketIDs(Collections.singletonList(ticketHistoryReal.getId()));
-        when(customerService.getCustomerById(1L)).thenReturn(realCustomer);
-        when(ticketService.getTicketByID(1L)).thenReturn(ticketHistoryReal);
-        paymentCustomerService.createPaymentProcess(dto);
-
-        verify(paymentCustomerRepo, times(1)).save(any(PaymentCustomer.class));
-        verify(customerService, times(1)).paymentDebt(realCustomer, "3000");
-    }
-    @Test
-    public void createPaymentProcess_emptyTicketList() {
-        dto.setTicketIDs(Collections.emptyList());
-        when(customerService.getCustomerById(1L)).thenReturn(realCustomer);
-
-        IllegalArgumentException exception = assertThrows(
-                IllegalArgumentException.class,
-                () -> paymentCustomerService.createPaymentProcess(dto)
-        );
-        assertEquals("No ha seleccionado ninguna boleta", exception.getMessage());
-        verifyNoInteractions(paymentCustomerRepo);
+        customer = Customer.builder().id(1L).name("John").surname("Doe").build();
+        ticketFirst = TicketHistory.builder()
+                .id(1L).total(5000).customer(customer).document("Ticket_1").build();
+        ticketSecond = TicketHistory.builder()
+                .id(2L).total(5000).customer(customer).document("Ticket_2").build();
+        dto = PaymentRequestDTO.builder()
+                .amount(10000)
+                .customerID(customer.getId())
+                .comment("Efectivo")
+                .totalPaymentPaid(0).build();
+        paymentCustomer = PaymentCustomer.builder()
+                .id(1L).amount(1000).comment("Test Comment")
+                .date(LocalDate.now()).customer(customer)
+                .document(ticketFirst).build();
     }
 
     //Prueba para obtener una lista de todos los abonos
     @Test
     public void getAll_valid(){
-        paymentCustomerService.getAll();
-        verify(paymentCustomerRepo).findAll();
+        PaymentResponseDTO paymentResponseDTO = mapper.toDto(paymentCustomer);
+        List<PaymentResponseDTO> dtoList = Collections.singletonList(paymentResponseDTO);
+        List<PaymentCustomer> paymentCustomers = Collections.singletonList(paymentCustomer);
+
+        when(paymentCustomerRepo.findAll()).thenReturn(paymentCustomers);
+
+        List<PaymentResponseDTO> result = paymentCustomerService.getAll();
+
+        verify(paymentCustomerRepo, times(1)).findAll();
+        assertEquals(result.size(), dtoList.size());
+        assertEquals(result.getFirst(), dtoList.getFirst());
+        assertEquals("Test Comment", result.getFirst().getComment());
+        assertEquals(1000, result.getFirst().getAmount());
+        assertEquals("Ticket_1", result.getFirst().getDocument());
+    }
+
+    //Prueba para crear un proceso de pagos de tickets
+    @Test
+    public void createPaymentProcess_validMultiplesTickets(){
+        dto.setTicketIDs(List.of(ticketFirst.getId(), ticketSecond.getId()));
+
+        when(ticketService.getTicketByID(ticketFirst.getId())).thenReturn(ticketFirst);
+        when(ticketService.getTicketByID(ticketSecond.getId())).thenReturn(ticketSecond);
+
+        paymentCustomerService.createPaymentProcess(dto);
+
+        ArgumentCaptor<PaymentCustomer> paymentCaptor = ArgumentCaptor.forClass(PaymentCustomer.class);
+        verify(ticketService, times(1)).updateStatus(ticketFirst);
+        verify(ticketService, times(1)).updateStatus(ticketSecond);
+        verify(paymentCustomerRepo, times(2)).save(paymentCaptor.capture());
+
+        List<PaymentCustomer> savedPayments = paymentCaptor.getAllValues();
+        PaymentCustomer firstSaved = savedPayments.getFirst();
+        PaymentCustomer secondSaved = savedPayments.get(1);
+        assertEquals(2, savedPayments.size());
+        assertEquals(ticketFirst.getCustomer(), firstSaved.getCustomer());
+        assertEquals(ticketFirst.getTotal(), firstSaved.getAmount());
+        assertEquals(ticketFirst, firstSaved.getDocument());
+        assertEquals("Efectivo", firstSaved.getComment());
+        assertEquals(ticketSecond.getCustomer(), secondSaved.getCustomer());
+        assertEquals(ticketSecond.getTotal(), secondSaved.getAmount());
+        assertEquals(ticketSecond, secondSaved.getDocument());
     }
     @Test
-    public void getAll_validGetValue(){
-        List<PaymentCustomer> mockPayment = Collections.singletonList(paymentCustomer);
-        when(paymentCustomerRepo.findAll()).thenReturn(mockPayment);
+    public void createPaymentProcess_invalidPaymentAmountException() {
+        dto.setAmount(800);
+        dto.setTicketIDs(Arrays.asList(ticketFirst.getId(), ticketSecond.getId()));
 
-        List<PaymentCustomer> result = paymentCustomerService.getAll();
-        verify(paymentCustomerRepo).findAll();
-        assertEquals(mockPayment, result);
+        when(ticketService.getTicketByID(1L)).thenReturn(ticketFirst);
+        when(ticketService.getTicketByID(2L)).thenReturn(ticketSecond);
+        doThrow(new InvalidPaymentAmountException("El monto no es correcto para el pago de la deuda")).when(validator)
+                .validateExactPaymentForMultipleTickets(dto.getAmount(), Arrays.asList(ticketFirst, ticketSecond));
+
+        InvalidPaymentAmountException exception = assertThrows(
+                InvalidPaymentAmountException.class, () -> paymentCustomerService.createPaymentProcess(dto));
+
+        verify(ticketService, never()).updateStatus(ticketFirst);
+        verify(ticketService, never()).updateStatus(ticketSecond);
+        verify(paymentCustomerRepo, never()).save(paymentCustomer);
+        assertEquals("El monto no es correcto para el pago de la deuda", exception.getMessage());
+    }
+
+    //Prueba para crear un proceso de pago de un solo ticket
+    @Test
+    public void createPaymentProcess_validSingleTicket(){
+        dto.setAmount(5000);
+        ticketFirst.setTotal(5000);
+        dto.setTicketIDs(List.of(ticketFirst.getId()));
+
+        when(ticketService.getTicketByID(ticketFirst.getId())).thenReturn(ticketFirst);
+        when(customerService.getCustomerById(dto.getCustomerID())).thenReturn(customer);
+
+        paymentCustomerService.createPaymentProcess(dto);
+
+        ArgumentCaptor<PaymentCustomer> paymentCaptor = ArgumentCaptor.forClass(PaymentCustomer.class);
+        verify(ticketService, times(1)).updateStatus(ticketFirst);
+        verify(paymentCustomerRepo, times(1)).save(paymentCaptor.capture());
+
+        List<PaymentCustomer> savedPayments = paymentCaptor.getAllValues();
+        PaymentCustomer firstSaved = savedPayments.getFirst();
+        assertEquals(1, savedPayments.size());
+        assertEquals(ticketFirst.getCustomer(), firstSaved.getCustomer());
+        assertEquals(ticketFirst.getTotal(), firstSaved.getAmount());
+        assertEquals(ticketFirst, firstSaved.getDocument());
+        assertEquals("Efectivo", firstSaved.getComment());
+    }
+    @Test
+    public void createPaymentProcess_validatePaymentNotExceedDebt(){
+        dto.setAmount(10000);
+        ticketFirst.setTotal(5000);
+        dto.setTicketIDs(List.of(ticketFirst.getId()));
+
+        when(ticketService.getTicketByID(ticketFirst.getId())).thenReturn(ticketFirst);
+        when(customerService.getCustomerById(dto.getCustomerID())).thenReturn(customer);
+        doThrow(new InvalidPaymentAmountException("El monto supera el valor de la deuda.")).when(validator)
+                        .validatePaymentNotExceedDebt(dto.getAmount(), ticketFirst.getTotal());
+
+        InvalidPaymentAmountException e = assertThrows(InvalidPaymentAmountException.class,
+                () -> paymentCustomerService.createPaymentProcess(dto));
+
+        verify(ticketService, never()).updateStatus(ticketFirst);
+        verify(paymentCustomerRepo, never()).save(paymentCustomer);
+
+        assertEquals("El monto supera el valor de la deuda.", e.getMessage());
+    }
+
+    //Prueba para crear un proceso de pago de un ticket con ajuste de deuda
+    @Test
+    public void createPaymentProcess_adjustPaymentForSingleTicket() {
+        dto.setAmount(1000);
+        dto.setTotalPaymentPaid(3000);
+        dto.setTicketIDs(List.of(ticketFirst.getId()));
+
+        when(customerService.getCustomerById(dto.getCustomerID())).thenReturn(customer);
+        when(ticketService.getTicketByID(ticketFirst.getId())).thenReturn(ticketFirst);
+
+        paymentCustomerService.createPaymentProcess(dto);
+
+        ArgumentCaptor<PaymentCustomer> paymentCaptor = ArgumentCaptor.forClass(PaymentCustomer.class);
+        verify(customerService, times(1)).getCustomerById(dto.getCustomerID());
+        verify(paymentCustomerRepo, times(1)).save(paymentCaptor.capture());
+
+        List<PaymentCustomer> savedPayments = paymentCaptor.getAllValues();
+        PaymentCustomer firstSaved = savedPayments.getFirst();
+        int remainingDebt = (ticketFirst.getTotal() - dto.getTotalPaymentPaid());
+        assertEquals(1, savedPayments.size());
+        assertEquals(ticketFirst.getCustomer(), firstSaved.getCustomer());
+        assertEquals(remainingDebt - dto.getAmount(), firstSaved.getAmount());
+        assertEquals(ticketFirst, firstSaved.getDocument());
+        assertEquals("Ajuste", firstSaved.getComment());
     }
 
     //Prueba para obtener una lista de las deudas del cliente seleccionado
     @Test
-    public void getCustomerSelected_valid(){
-        paymentCustomerService.getCustomerSelected(1L);
-        verify(paymentCustomerRepo).findByCustomerId(1L);
+    public void getCustomerSelected_validSingleTicket(){
+        List<PaymentResponseDTO> responseDTOS = Collections.singletonList(mapper.toDto(paymentCustomer));
+        List<TicketHistory> tickets = Collections.singletonList(ticketFirst);
+        List<PaymentCustomer> paymentCustomers = Collections.singletonList(paymentCustomer);
+
+        when(ticketService.getByCustomerId(customer.getId())).thenReturn(tickets);
+        when(paymentCustomerRepo.findByCustomerId(customer.getId())).thenReturn(paymentCustomers);
+
+        List<PaymentResponseDTO> result = paymentCustomerService.getCustomerSelected(customer.getId());
+
+        verify(ticketService, times(1)).getByCustomerId(customer.getId());
+        verify(paymentCustomerRepo, times(1)).findByCustomerId(customer.getId());
+
+        assertEquals(result.size(), responseDTOS.size());
+        assertEquals(result.getFirst().getDocument(), responseDTOS.getFirst().getDocument());
     }
     @Test
-    public void getCustomerSelected_validValue(){
-        paymentCustomer.setDocument(ticketHistoryReal);
-        List<PaymentCustomer> mockPayment = Collections.singletonList(paymentCustomer);
-        List<TicketHistory> mockTickets = Collections.singletonList(ticketHistoryReal);
-        when(paymentCustomerRepo.findByCustomerId(1L)).thenReturn(mockPayment);
-        when(ticketService.getByCustomerId(1L)).thenReturn(mockTickets);
+    public void getCustomerSelected_validMultipleTickets(){
+        PaymentCustomer anotherPayment = PaymentCustomer.builder()
+                .id(2L).amount(2000).comment("Another Comment")
+                .date(LocalDate.now().plusDays(1)).customer(customer)
+                .document(ticketSecond).build();
+        List<PaymentResponseDTO> responseDTOS = List.of(mapper.toDto(paymentCustomer), mapper.toDto(anotherPayment));
+        List<TicketHistory> tickets = List.of(ticketFirst, ticketSecond);
+        List<PaymentCustomer> paymentCustomers = List.of(paymentCustomer, anotherPayment);
 
-        List<PaymentCustomer> result = paymentCustomerService.getCustomerSelected(1L);
-        verify(paymentCustomerRepo).findByCustomerId(1L);
-        assertEquals(1, result.size());
-        assertEquals(mockPayment, result);
+        when(ticketService.getByCustomerId(customer.getId())).thenReturn(tickets);
+        when(paymentCustomerRepo.findByCustomerId(customer.getId())).thenReturn(paymentCustomers);
+
+        List<PaymentResponseDTO> result = paymentCustomerService.getCustomerSelected(customer.getId());
+
+        verify(ticketService, times(1)).getByCustomerId(customer.getId());
+        verify(paymentCustomerRepo, times(1)).findByCustomerId(customer.getId());
+
+        assertEquals(result.size(), responseDTOS.size());
+        assertEquals(result.getFirst().getDocument(), responseDTOS.getFirst().getDocument());
+        assertEquals(result.getLast().getDocument(), responseDTOS.getLast().getDocument());
+    }
+    @Test
+    public void getCustomerSelected_validEmptyTickets(){
+        TicketHistory anotherTicket = TicketHistory.builder().id(3L).customer(customer).document("Ticket_3").build();
+        List<TicketHistory> tickets = List.of(anotherTicket);
+        List<PaymentCustomer> paymentCustomers = List.of(paymentCustomer);
+
+        when(ticketService.getByCustomerId(customer.getId())).thenReturn(tickets);
+        when(paymentCustomerRepo.findByCustomerId(customer.getId())).thenReturn(paymentCustomers);
+
+        List<PaymentResponseDTO> result = paymentCustomerService.getCustomerSelected(customer.getId());
+
+        verify(ticketService, times(1)).getByCustomerId(customer.getId());
+        verify(paymentCustomerRepo, times(1)).findByCustomerId(customer.getId());
+
+        assertTrue(result.isEmpty());
     }
 }
