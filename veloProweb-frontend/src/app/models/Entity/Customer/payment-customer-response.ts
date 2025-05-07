25344dcb46cdd9e483b9844e628f47133556e13d
Juanin92 <j.claveria19@com>
2025-05-07 17:34:06 -0400

    feat(paymentCustomer): update PaymentCustomerResponse to simplify document type and remove CustomerResponse

--- veloProweb-frontend/src/app/models/Entity/Customer/payment-customer-response.ts
+++ veloProweb-frontend/src/app/models/Entity/Customer/payment-customer-response.ts
@@ -1,11 +1,7 @@
-import { CustomerResponse } from "./customer-response";
-import { TicketHistory } from "./ticket-history.model";
-
 export interface PaymentCustomerResponse {
     id: number;
     amount: number;
     comment: string;
     date: string;
-    customer: CustomerResponse;
-    document: TicketHistory;
+    document: string;
 }
