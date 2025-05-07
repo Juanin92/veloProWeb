1d52ef3284076fe8c4a0e7ace75e23658cd24463
Juanin92 <j.claveria19@com>
2025-05-07 15:16:29 -0400

    feat(dto): enhance PaymentRequestDTO with validation constraints for ticket IDs, customer ID, amount, comment, and total payment paid

--- veloProWeb/src/main/java/com/veloProWeb/model/dto/customer/PaymentRequestDTO.java
+++ veloProWeb/src/main/java/com/veloProWeb/model/dto/customer/PaymentRequestDTO.java
@@ -1,5 +1,6 @@
 package com.veloProWeb.model.dto.customer;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +13,23 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PaymentRequestDTO {
+
+    @NotEmpty(message = "El ID del ticket no puede estar vacío")
+    @NotNull(message = "La lista de IDs de tickets no puede ser nula")
     private List<Long> ticketIDs;
+
+    @NotBlank(message = "El ID del cliente no puede estar vacío")
     private Long customerID;
+
+    @NotBlank(message = "El monto no puede estar vacío")
+    @Positive(message = "El monto debe ser positivo")
+    @Min(value = 1, message = "El monto debe ser mayor a 0")
     private int amount;
+
+    @NotBlank(message = "El comentario no puede estar vacío")
     private String comment;
+
+    @NotBlank(message = "El monto total ya pagado no puede estar vacío")
+    @Positive(message = "El monto debe ser positivo")
     private int totalPaymentPaid;
 }
