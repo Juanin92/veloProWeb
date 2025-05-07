bb55db566af054c8609cce09c618d70a79a1ef29
Juanin92 <j.claveria19@com>
2025-05-07 14:47:39 -0400

    feat(service): update IPaymentCustomerService to return PaymentResponseDTO instead of PaymentCustomer

--- veloProWeb/src/main/java/com/veloProWeb/service/customer/interfaces/IPaymentCustomerService.java
+++ veloProWeb/src/main/java/com/veloProWeb/service/customer/interfaces/IPaymentCustomerService.java
@@ -1,12 +1,12 @@
 package com.veloProWeb.service.customer.interfaces;
 
 import com.veloProWeb.model.dto.customer.PaymentRequestDTO;
-import com.veloProWeb.model.entity.customer.PaymentCustomer;
+import com.veloProWeb.model.dto.customer.PaymentResponseDTO;
 
 import java.util.List;
 
 public interface IPaymentCustomerService {
     void createPaymentProcess(PaymentRequestDTO dto);
-    List<PaymentCustomer> getAll();
-    List<PaymentCustomer> getCustomerSelected(Long id);
+    List<PaymentResponseDTO> getAll();
+    List<PaymentResponseDTO> getCustomerSelected(Long id);
 }
