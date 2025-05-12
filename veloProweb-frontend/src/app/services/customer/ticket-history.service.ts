e06e5911619442e47c9a85afaea9d0c49fb9b608
Juanin92 <j.claveria19@com>
2025-05-12 15:55:02 -0400

    feat(TicketHistoryService): remove unused methods and clean up imports

--- veloProweb-frontend/src/app/services/customer/ticket-history.service.ts
+++ veloProweb-frontend/src/app/services/customer/ticket-history.service.ts
@@ -1,9 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { TicketHistory } from '../../models/Entity/Customer/ticket-history.model';
-import { TicketRequestDTO } from '../../models/DTO/ticket-request-dto';
-import { CustomerResponse } from '../../models/Entity/Customer/customer-response';
+import { TicketHistory } from '../../models/Entity/Customer/ticket-history';
 import { AuthService } from '../User/auth.service';
 
 @Injectable({
@@ -23,23 +21,5 @@ export class TicketHistoryService {
   getListTicketByCustomer(customerId: number): Observable<TicketHistory[]>{
     let params = new HttpParams().set('customerId', customerId.toString());
     return this.httpClient.get<TicketHistory[]>(this.apiUrl, {params});
-  }
-
-  /**
-   * crea un nuevo ticket para un cliente
-   * @param ticketRequestDTO - Objeto que contiene los detalles del ticket
-   * @returns - Observable emite un mensaje de confirmación
-   */
-  createTicketToCustomer(ticketRequestDTO: TicketRequestDTO): Observable<{message: string}>{
-    return this.httpClient.post<{message: string}>(this.apiUrl, ticketRequestDTO, {headers: this.auth.getAuthHeaders()});
-  }
-
-  /**
-   * Actualiza el estado de un ticket
-   * @param ticket - ticket seleccionado
-   * @returns - Observable emite un mensaje de confirmación
-   */
-  updateStatus(ticket: TicketHistory): Observable<{message: string}>{
-    return this.httpClient.put<{message: string}>(`${this.apiUrl}/actualizar-estado`, ticket);
   }
 }
