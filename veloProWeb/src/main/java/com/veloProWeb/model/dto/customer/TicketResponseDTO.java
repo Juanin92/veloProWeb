dc506e3b0d6d176605ff52f8699fc85f8f4447ec
Juanin92 <j.claveria19@com>
2025-05-12 15:29:55 -0400

    feat(TicketResponseDTO): remove unnecessary fields and simplify the DTO structure

--- veloProWeb/src/main/java/com/veloProWeb/model/dto/customer/TicketResponseDTO.java
+++ veloProWeb/src/main/java/com/veloProWeb/model/dto/customer/TicketResponseDTO.java
@@ -1,29 +1,17 @@
 package com.veloProWeb.model.dto.customer;
 
-import com.veloProWeb.model.entity.customer.Customer;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalDate;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class TicketResponseDTO {
-    private Long id;
     private String document;
     private int total;
     private boolean status;
-    private LocalDate date;
-    private LocalDate notificationsDate;
     private String customer;
 }
