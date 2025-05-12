6808faaa5c11b5ffcd02fe578f01375de8ed34cc
Juanin92 <j.claveria19@com>
2025-05-12 19:10:29 -0400

    fix(ticket-service): standardize method naming conventions for ticket operations

---
+++ veloProWeb/src/main/java/com/veloProWeb/exceptions/product/BrandAlreadyExistsException.java
@@ -0,0 +1,5 @@
+package com.veloProWeb.exceptions.product;
+
+public class BrandAlreadyExistsException extends RuntimeException{
+    public BrandAlreadyExistsException(String message){ super(message);}
+}
