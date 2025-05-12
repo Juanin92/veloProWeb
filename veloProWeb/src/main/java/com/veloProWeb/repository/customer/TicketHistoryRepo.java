637acfe2152bdacd7e1b0559cc8eec8a5956ba3d
Juanin92 <j.claveria19@com>
2025-05-12 15:29:42 -0400

    feat(TicketHistoryRepo): add method to retrieve the most recently created TicketHistory

--- veloProWeb/src/main/java/com/veloProWeb/repository/customer/TicketHistoryRepo.java
+++ veloProWeb/src/main/java/com/veloProWeb/repository/customer/TicketHistoryRepo.java
@@ -2,6 +2,7 @@ package com.veloProWeb.repository.customer;
 
 import com.veloProWeb.model.entity.customer.TicketHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface TicketHistoryRepo extends JpaRepository<TicketHistory, Long> {
 
     List<TicketHistory> findByCustomerId(Long id);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM ticket_history ORDER BY date DESC, id DESC LIMIT 1")
+    TicketHistory findLastCreated();
 }
