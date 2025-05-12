2feace226b12281ed9a0f91dc02afba8dce49098
Juanin92 <j.claveria19@com>
2025-05-12 15:54:17 -0400

    feat(TicketHistory): Delete ticket request interface

--- veloProweb-frontend/src/app/models/DTO/ticket-request-dto.ts
+++
@@ -1,8 +0,0 @@
-import { CustomerResponse } from "../Entity/Customer/customer-response";
-
-export interface TicketRequestDTO {
-    customer: CustomerResponse;
-    number: number;
-    total: number;
-    date: Date;
-}
