2e286da9bff2e099ea80c66ae26bfc3f1150313d
Juanin92 <j.claveria19@com>
2025-05-12 19:11:14 -0400

    feat(BrandProductRepo): add custom query to retrieve all BrandProducts ordered by name

--- veloProWeb/src/main/java/com/veloProWeb/repository/Product/BrandProductRepo.java
+++ veloProWeb/src/main/java/com/veloProWeb/repository/Product/BrandProductRepo.java
@@ -2,12 +2,17 @@ package com.veloProWeb.repository.Product;
 
 import com.veloProWeb.model.entity.Product.BrandProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BrandProductRepo extends JpaRepository<BrandProduct, Long> {
 
     Optional<BrandProduct> findByName(String name);
+
+    @Query("SELECT b FROM BrandProduct b ORDER BY LOWER(b.name) ASC")
+    List<BrandProduct> findAllOrderByNameAsc();
 }
