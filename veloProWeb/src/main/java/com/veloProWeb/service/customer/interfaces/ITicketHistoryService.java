e49c22624311bee76c99795866f69cfb6fba84c8
Juanin92 <j.claveria19@com>
2025-05-12 15:29:11 -0400

    feat(ITicketHistoryService): update AddTicketToCustomer method signature and add DTO retrieval method

--- veloProWeb/src/main/java/com/veloProWeb/service/customer/interfaces/ITicketHistoryService.java
+++ veloProWeb/src/main/java/com/veloProWeb/service/customer/interfaces/ITicketHistoryService.java
@@ -1,15 +1,16 @@
 package com.veloProWeb.service.customer.interfaces;
 
+import com.veloProWeb.model.dto.customer.TicketResponseDTO;
 import com.veloProWeb.model.entity.customer.Customer;
 import com.veloProWeb.model.entity.customer.TicketHistory;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface ITicketHistoryService {
 
-    void AddTicketToCustomer(Customer customer, Long number, int total, LocalDate date);
+    void AddTicketToCustomer(Customer customer, int total);
     List<TicketHistory> getByCustomerId(Long id);
+    List<TicketResponseDTO> getByCustomerIdDTO(Long id);
     void updateStatus(TicketHistory  ticket);
     TicketHistory getTicketByID(Long Id);
 }
