e1ea59ed2fbf4c800fb8b14a9494a48edaf422b4
Juanin92 <j.claveria19@com>
2025-05-12 15:30:50 -0400

    feat(TicketHistoryService): enhance AddTicketToCustomer method and add ticket retrieval by customer ID

--- veloProWeb/src/main/java/com/veloProWeb/service/customer/TicketHistoryService.java
+++ veloProWeb/src/main/java/com/veloProWeb/service/customer/TicketHistoryService.java
@@ -1,43 +1,43 @@
 package com.veloProWeb.service.customer;
 
+import com.veloProWeb.exceptions.Customer.TicketAlreadyPaidException;
+import com.veloProWeb.exceptions.Customer.TicketNotFoundException;
+import com.veloProWeb.mapper.TicketHistoryMapper;
+import com.veloProWeb.model.dto.customer.TicketResponseDTO;
 import com.veloProWeb.model.entity.customer.Customer;
 import com.veloProWeb.model.entity.customer.TicketHistory;
 import com.veloProWeb.repository.customer.TicketHistoryRepo;
-import com.veloProWeb.service.customer.interfaces.ICustomerService;
 import com.veloProWeb.service.customer.interfaces.ITicketHistoryService;
-import com.veloProWeb.util.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
+import com.veloProWeb.util.IdentifyDocumentGenerator;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class TicketHistoryService implements ITicketHistoryService {
 
-    @Autowired private TicketHistoryRepo ticketHistoryRepo;
-    @Autowired private ICustomerService customerService;
-    @Autowired @Lazy private EmailService emailService;
-    private LocalDate lastValidationDate;
+    private final TicketHistoryRepo ticketHistoryRepo;
+    private final TicketHistoryMapper mapper;
 
     /**
      * Crea un ticket para un cliente específico
-     * @param customer cliente a quien se le crea el ticket
-     * @param number número del ticket que generará el número del documento
-     * @param total valor del monto del ticket
-     * @param date fecha de creación del ticket
+     * @param customer - nombre del cliente
+     * @param total - total del ticket
      */
+    @Transactional
     @Override
-    public void AddTicketToCustomer(Customer customer, Long number, int total, LocalDate date) {
+    public void AddTicketToCustomer(Customer customer, int total) {
+        String lastDocument = getLastTicketCreated().getDocument();
         TicketHistory ticket = new TicketHistory();
-        String name = "BO_" + number;
         ticket.setTotal(total);
-        ticket.setDocument(name);
+        ticket.setDocument(IdentifyDocumentGenerator.generateIdentifyDocumentTicket(lastDocument));
         ticket.setStatus(false);
-        ticket.setDate(date);
+        ticket.setDate(LocalDate.now());
         ticket.setCustomer(customer);
         ticketHistoryRepo.save(ticket);
     }
@@ -58,14 +58,29 @@ public class TicketHistoryService implements ITicketHistoryService {
                 .toList();
     }
 
+    @Override
+    public List<TicketResponseDTO> getByCustomerIdDTO(Long id) {
+        return ticketHistoryRepo.findByCustomerId(id)
+                .stream()
+                .filter(ticketHistory -> !ticketHistory.isStatus())
+                .sorted(Comparator.comparing(TicketHistory::getTotal))
+                .map(mapper::toDto)
+                .toList();
+    }
+
     /**
      * Actualiza el estado del ticket seleccionado a verdadero
      * @param ticket ticket para actualizar su estado
      */
+    @Transactional
     @Override
     public void updateStatus(TicketHistory ticket) {
-        ticket.setStatus(true);
-        ticketHistoryRepo.save(ticket);
+        if (!ticket.isStatus()) {
+            ticket.setStatus(true);
+            ticketHistoryRepo.save(ticket);
+        }else {
+            throw new TicketAlreadyPaidException(String.format("El ticket %s ya fue pagado", ticket.getDocument()));
+        }
     }
 
     /**
@@ -75,56 +90,10 @@ public class TicketHistoryService implements ITicketHistoryService {
      */
     @Override
     public TicketHistory getTicketByID(Long Id) {
-        return ticketHistoryRepo.findById(Id).orElseThrow(() -> new IllegalArgumentException("Ticket no encontrado"));
+        return ticketHistoryRepo.findById(Id).orElseThrow(() -> new TicketNotFoundException("Ticket no encontrado"));
     }
 
-    /**
-     * Válida los tickets de todos los clientes diariamente a las 12:00 PM.
-     * Si la fecha de validación es diferente a la fecha actual, se valida cada ticket
-     * y se envía un correo electrónico al cliente si el ticket está vencido.
-     */
-//    @Scheduled(cron = "0 0 12 * * ?")
-//    public void validateTicketsDaily() {
-//        LocalDate today = LocalDate.now();
-//        if (lastValidationDate == null || !lastValidationDate.equals(today)) {
-//            List<customer> customers = customerService.getAll();
-//            for (customer customer : customers) {
-//                List<TicketHistory> tickets = getByCustomerId(customer.getId());
-//                for (TicketHistory ticket : tickets) {
-//                    if (!ticket.isStatus() && validateDate(ticket)) {
-//                        customer.setStatus(PaymentStatus.VENCIDA);
-//                        customerService.updateTotalDebt(customer);
-//                        emailService.sendOverdueTicketNotification(customer, ticket);
-//                    }
-//                }
-//            }
-//            lastValidationDate = today;
-//        }
-//    }
-
-    /**
-     * Válida la fecha de un ticket y actualiza la fecha de notificación si es necesario.
-     * Verifica si el estado del ticket es falso y si han pasado más de 30 días desde la fecha del ticket.
-     * También verifica si la fecha de notificación es nula o si han pasado más de 15 días desde la fecha de última notificación.
-     * Si se cumplen estas condiciones, actualiza la fecha de notificación del ticket a la fecha actual y guarda el ticket en el repositorio.
-     *
-     * @param ticket ticket que se debe validar
-     * @return verdadero si el ticket fue validado y la fecha de notificación fue actualizada, falso en caso contrario
-     */
-    private boolean validateDate(TicketHistory ticket) {
-        LocalDate now = LocalDate.now();
-        LocalDate ticketDate = ticket.getDate();
-        LocalDate notificationDate = ticket.getNotificationsDate();
-
-        if (ticket.isStatus()) {
-            return false;
-        }
-        if (ChronoUnit.DAYS.between(ticketDate, now) > 30 &&
-                (notificationDate == null || ChronoUnit.DAYS.between(notificationDate, now) > 15)) {
-            ticket.setNotificationsDate(now);
-            ticketHistoryRepo.save(ticket);
-            return true;
-        }
-        return false;
+    private TicketHistory getLastTicketCreated() {
+        return ticketHistoryRepo.findLastCreated();
     }
 }
