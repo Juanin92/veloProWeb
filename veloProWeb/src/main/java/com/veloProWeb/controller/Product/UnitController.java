3b7c707a04566b1e9b4050b8898ce9bb5beccc9e
Juanin92 <j.claveria19@com>
2025-05-12 23:28:24 -0400

    feat(UnitController): refactor createUnit method to use validation and improve response handling

--- veloProWeb/src/main/java/com/veloProWeb/controller/Product/UnitController.java
+++ veloProWeb/src/main/java/com/veloProWeb/controller/Product/UnitController.java
@@ -3,7 +3,9 @@ package com.veloProWeb.controller.Product;
 import com.veloProWeb.model.entity.Product.UnitProduct;
 import com.veloProWeb.service.Product.Interfaces.IUnitService;
 import com.veloProWeb.service.Record.IRecordService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.veloProWeb.util.ResponseMessage;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,52 +13,30 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Controlador REST para gestionar operaciones relacionadas con las unidades de medidas de productos.
- * Este controlador proporciona endpoints para obtener todas las unidades y crear nuevas unidades.
- */
 @RestController
 @RequestMapping("/unidad")
-@CrossOrigin(origins = "http://localhost:4200")
+@AllArgsConstructor
 public class UnitController {
 
-    @Autowired private IUnitService unitService;
-    @Autowired private IRecordService recordService;
+    private final IUnitService unitService;
+    private final IRecordService recordService;
 
-    /**
-     * Obtiene una lista de todas las unidades de medidas
-     * @return - ResponseEntity con una lista de las unidades
-     */
     @GetMapping
     @PreAuthorize("hasAnyAuthority('ADMIN','MASTER', 'WAREHOUSE')")
     public ResponseEntity<List<UnitProduct>> getAllUnits(){
         return ResponseEntity.ok(unitService.getAll());
     }
 
-    /**
-     * Crea una nueva unidad
-     * @param unit - Unidad con los datos a crear
-     * @return - ResponseEntity con un mensaje de éxito o error según sea el caso
-     */
     @PostMapping
     @PreAuthorize("hasAnyAuthority('ADMIN','MASTER', 'WAREHOUSE')")
-    public ResponseEntity<Map<String, String>> createUnit(@RequestBody UnitProduct unit,
+    public ResponseEntity<Map<String, String>> createUnit(@RequestBody @Valid UnitProduct unit,
                                                           @AuthenticationPrincipal UserDetails userDetails){
-        Map<String, String> response = new HashMap<>();
-        try{
-            unitService.save(unit);
-            response.put("message", "Unidad de medida registrada correctamente");
-            recordService.registerAction(userDetails, "CREATE", "unidad creada: " + unit.getNameUnit());
-            return ResponseEntity.ok(response);
-        }catch (IllegalArgumentException e){
-            response.put("message",e.getMessage());
-            recordService.registerAction(userDetails, "CREATE_FAILURE",
-                    "ERROR: crear unidad(" + unit.getNameUnit() + "): " + e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-        }
+        unitService.save(unit);
+        recordService.registerAction(userDetails, "CREATE", "unidad de medida creada: " + unit.getNameUnit());
+        return new ResponseEntity<>(ResponseMessage.message("Unidad de medida registrada correctamente"),
+                HttpStatus.CREATED);
     }
 }
