a928c310d1a08225cfc6f6c638f0d06ae7eaf209
Juanin92 <j.claveria19@com>
2025-05-13 16:41:47 -0400

    feat(BrandValidator): add validation for existing brand names

---
+++ veloProWeb/src/main/java/com/veloProWeb/validation/BrandValidator.java
@@ -0,0 +1,19 @@
+package com.veloProWeb.validation;
+
+import com.veloProWeb.exceptions.product.BrandAlreadyExistsException;
+import com.veloProWeb.model.entity.Product.BrandProduct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BrandValidator {
+
+    /**
+     * Válida la existencia de una marca ya registrada
+     * @param brand - marca a validar
+     */
+    public void validateBrandDoesNotExist(BrandProduct brand){
+        if (brand != null ){
+            throw new BrandAlreadyExistsException("Nombre Existente: Hay registro de esta marca.");
+        }
+    }
+}
