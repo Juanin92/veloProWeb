fe0e2d3f1b37813891e84d600233b1fe16ff3d0f
Juanin92 <j.claveria19@com>
2025-05-13 17:37:15 -0400

    feat(Product): add Lombok @Builder annotation for improved object creation

--- veloProWeb/src/main/java/com/veloProWeb/model/entity/Product/Product.java
+++ veloProWeb/src/main/java/com/veloProWeb/model/entity/Product/Product.java
@@ -5,6 +5,7 @@ import com.veloProWeb.model.entity.Kardex;
 import com.veloProWeb.model.Enum.StatusProduct;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Entity
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
