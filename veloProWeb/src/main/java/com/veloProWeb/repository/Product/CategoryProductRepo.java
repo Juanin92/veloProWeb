81b70c4e446133e3d7c39a4a2fc565211bab3f7e
Juanin92 <j.claveria19@com>
2025-05-12 20:11:19 -0400

    feat(CategoryProductRepo): add method to retrieve all categories ordered by name

--- veloProWeb/src/main/java/com/veloProWeb/repository/Product/CategoryProductRepo.java
+++ veloProWeb/src/main/java/com/veloProWeb/repository/Product/CategoryProductRepo.java
@@ -2,11 +2,16 @@ package com.veloProWeb.repository.Product;
 
 import com.veloProWeb.model.entity.Product.CategoryProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CategoryProductRepo extends JpaRepository<CategoryProduct, Long> {
     Optional<CategoryProduct> findByName(String name);
+
+    @Query("SELECT c FROM CategoryProduct c ORDER BY LOWER(c.name) ASC")
+    List<CategoryProduct> findAllOrderByNameAsc();
 }
