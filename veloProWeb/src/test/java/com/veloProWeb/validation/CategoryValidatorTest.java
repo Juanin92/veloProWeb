5feb5022c9113e71e9ea63135ef6acc4610abc0a
Juanin92 <j.claveria19@com>
2025-05-13 16:42:14 -0400

    refactor(CategoryValidatorTest): rename test class and update validation method calls

---
+++ veloProWeb/src/test/java/com/veloProWeb/validation/CategoryValidatorTest.java
@@ -0,0 +1,26 @@
+package com.veloProWeb.validation;
+
+import com.veloProWeb.exceptions.product.CategoryAlreadyExistsException;
+import com.veloProWeb.model.entity.Product.CategoryProduct;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+public class CategoryValidatorTest {
+
+    @InjectMocks private CategoryValidator validator;
+
+    //Prueba para validar la existencia de una categoría
+    @Test
+    public void validateCategory_validException(){
+        CategoryProduct category = CategoryProduct.builder().build();
+        CategoryAlreadyExistsException e = assertThrows(CategoryAlreadyExistsException.class,
+                () -> validator.validateCategoryDoesNotExist(category));
+        assertEquals("Nombre Existente: Hay registro de esta categoría.", e.getMessage());
+    }
+}
