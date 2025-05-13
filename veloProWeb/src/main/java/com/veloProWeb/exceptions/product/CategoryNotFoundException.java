aeba204518cc8e88f346e8d2cdb8c00f6532f115
Juanin92 <j.claveria19@com>
2025-05-13 16:43:32 -0400

    feat(CategoryNotFoundException): add custom exception for category not found scenarios

---
+++ veloProWeb/src/main/java/com/veloProWeb/exceptions/product/CategoryNotFoundException.java
@@ -0,0 +1,5 @@
+package com.veloProWeb.exceptions.product;
+
+public class CategoryNotFoundException extends RuntimeException{
+    public CategoryNotFoundException(String message){super(message);}
+}
