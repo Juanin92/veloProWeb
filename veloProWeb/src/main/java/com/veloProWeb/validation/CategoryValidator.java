1e1f02daafcb7f07bc33c719e8a55573f5f4f941
Juanin92 <j.claveria19@com>
2025-05-13 16:42:05 -0400

    refactor(CategoryValidator): rename and simplify category validation methods

---
+++ veloProWeb/src/main/java/com/veloProWeb/validation/CategoryValidator.java
@@ -0,0 +1,19 @@
+package com.veloProWeb.validation;
+
+import com.veloProWeb.exceptions.product.*;
+import com.veloProWeb.model.entity.Product.CategoryProduct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryValidator {
+
+    /**
+     * Válida la existencia de una categoría ya registrada
+     * @param category - categoría a validar
+     */
+    public void validateCategoryDoesNotExist(CategoryProduct category){
+        if (category != null ){
+            throw new CategoryAlreadyExistsException("Nombre Existente: Hay registro de esta categoría.");
+        }
+    }
+}
