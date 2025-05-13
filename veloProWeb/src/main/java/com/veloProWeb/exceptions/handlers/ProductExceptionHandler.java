395147081750ad5a992a1eb58cad40fe056cfc2b
Juanin92 <j.claveria19@com>
2025-05-12 23:43:02 -0400

    feat(ProductExceptionHandler): add SubcategoryAlreadyExistsException and update exception handler

--- veloProWeb/src/main/java/com/veloProWeb/exceptions/handlers/ProductExceptionHandler.java
+++ veloProWeb/src/main/java/com/veloProWeb/exceptions/handlers/ProductExceptionHandler.java
@@ -3,6 +3,7 @@ package com.veloProWeb.exceptions.handlers;
 import com.veloProWeb.exceptions.BaseExceptionHandler;
 import com.veloProWeb.exceptions.product.BrandAlreadyExistsException;
 import com.veloProWeb.exceptions.product.CategoryAlreadyExistsException;
+import com.veloProWeb.exceptions.product.SubcategoryAlreadyExistsException;
 import com.veloProWeb.exceptions.product.UnitAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,8 @@ public class ProductExceptionHandler extends BaseExceptionHandler {
     @ExceptionHandler({
             BrandAlreadyExistsException.class,
             CategoryAlreadyExistsException.class,
-            UnitAlreadyExistsException.class
+            UnitAlreadyExistsException.class,
+            SubcategoryAlreadyExistsException.class
     })
     public ResponseEntity<Map<String, String>> handleCustomerException(Exception e){
         return buildResponse(e.getMessage(), HttpStatus.CONFLICT);
---
+++ veloProWeb/src/main/java/com/veloProWeb/exceptions/product/SubcategoryAlreadyExistsException.java
@@ -0,0 +1,5 @@
+package com.veloProWeb.exceptions.product;
+
+public class SubcategoryAlreadyExistsException extends RuntimeException{
+    public SubcategoryAlreadyExistsException(String message){super(message);}
+}
