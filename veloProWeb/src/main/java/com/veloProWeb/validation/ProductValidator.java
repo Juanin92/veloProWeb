ba329c87faba89cf078709752685ad13633a9e82
Juanin92 <j.claveria19@com>
2025-05-13 17:35:56 -0400

    refactor(ProductValidator): simplify validation logic and add exceptions for product activation and deletion

--- veloProWeb/src/main/java/com/veloProWeb/validation/ProductValidator.java
+++ veloProWeb/src/main/java/com/veloProWeb/validation/ProductValidator.java
@@ -1,22 +1,12 @@
 package com.veloProWeb.validation;
 
+import com.veloProWeb.exceptions.product.ProductAlreadyActivatedException;
+import com.veloProWeb.exceptions.product.ProductAlreadyDeletedException;
 import com.veloProWeb.model.entity.Product.*;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductValidator {
-
-    /**
-     * Válida los datos de un producto.
-     * @param product - Producto seleccionado a validar.
-     */
-    public void validateNewProduct(Product product){
-        validateBrand(product.getBrand());
-        validateCategory(product.getCategory());
-        validateSubcategory(product.getSubcategoryProduct());
-        validateUnit(product.getUnit());
-        validateDescription(product.getDescription());
-    }
 
     /**
      * Válida el monto del stock, lanzará una excepción:
@@ -51,58 +41,15 @@ public class ProductValidator {
         }
     }
 
-    /**
-     * Válida la marca de un producto, lanzará una excepción:
-     *Si esta es nula.
-     *@param brand - Objeto de una marca
-     */
-    private void validateBrand(BrandProduct brand){
-        if (brand == null){
-            throw new IllegalArgumentException("Seleccione una marca");
+    public void isActivated(Product product){
+        if (product.isStatus()) {
+            throw new ProductAlreadyActivatedException("El producto ya está activado.");
         }
     }
 
-    /**
-     * Válida la categoría de un producto, lanzará una excepción:
-     * Si esta es nula.
-     * @param category - Objeto de una categoría
-     */
-    private  void validateCategory(CategoryProduct category){
-        if (category == null){
-            throw new IllegalArgumentException("Seleccione una categoría");
-        }
-    }
-
-    /**
-     * Válida la subcategoría de un producto, lanzará una excepción:
-     * Si esta es nula.
-     * @param subcategory - Objeto de una subcategoría
-     */
-    private void validateSubcategory(SubcategoryProduct subcategory){
-        if (subcategory == null){
-            throw new IllegalArgumentException("Seleccione una subcategoría");
-        }
-    }
-
-    /**
-     * Válida la unidad de medida de un producto, lanzará una excepción:
-     * Si la unidad es nula
-     * @param unit - objeto de unidad
-     */
-    private void validateUnit(UnitProduct unit){
-        if (unit == null){
-            throw new IllegalArgumentException("Seleccione una unidad");
-        }
-    }
-
-    /**
-     * Válida la descripción de un producto, lanzará una excepción:
-     * Si la cadena es nula o está vacía.
-     * @param description - cadena que contiene el valor de la descripción
-     */
-    private void validateDescription(String description){
-        if (description == null || description.trim().isBlank()){
-            throw new IllegalArgumentException("Ingrese una descripción");
+    public void isDeleted(Product product){
+        if (!product.isStatus()) {
+            throw new ProductAlreadyDeletedException("El producto ya está desactivado.");
         }
     }
 }
