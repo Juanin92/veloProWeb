0c1249d7ff1256e75fa91e4617ead37d657759b7
Juanin92 <j.claveria19@com>
2025-05-13 16:45:17 -0400

    refactor(SubcategoryController): simplify create and get methods, add validation for category ID

--- veloProWeb/src/main/java/com/veloProWeb/controller/Product/SubcategoryController.java
+++ veloProWeb/src/main/java/com/veloProWeb/controller/Product/SubcategoryController.java
@@ -3,71 +3,45 @@ package com.veloProWeb.controller.Product;
 import com.veloProWeb.model.entity.Product.SubcategoryProduct;
 import com.veloProWeb.service.Product.Interfaces.ISubcategoryService;
 import com.veloProWeb.service.Record.IRecordService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.veloProWeb.util.ResponseMessage;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Controlador REST para gestionar operaciones relacionadas con las subcategorías de productos.
- * Este controlador proporciona endpoints para obtener todas las subcategorías y crear nuevas subcategorías.
- */
 @RestController
 @RequestMapping("/subcategoria")
-@CrossOrigin(origins = "http://localhost:4200")
+@AllArgsConstructor
+@Validated
 public class SubcategoryController {
 
-    @Autowired private ISubcategoryService subcategoryService;
-    @Autowired private IRecordService recordService;
+    private final ISubcategoryService subcategoryService;
+    private final IRecordService recordService;
 
-    /**
-     * Obtiene una lista de todas las subcategorías
-     * @return - ResponseEntity con una lista de las subcategorías
-     */
-    @GetMapping
-    @PreAuthorize("hasAnyAuthority('ADMIN','MASTER', 'WAREHOUSE')")
-    public ResponseEntity<List<SubcategoryProduct>> getAllSubcategories(){
-        return ResponseEntity.ok(subcategoryService.getAll());
-    }
-
-    /**
-     * Obtiene la lista de subcategorías asociadas a una categoría específica por su ID.
-     * @param id - Identificador de la categoría cuyas subcategorías se desean obtener.
-     * @return - ResponseEntity con una lista de las subcategorías
-     */
     @GetMapping("/{id}")
     @PreAuthorize("hasAnyAuthority('ADMIN','MASTER', 'WAREHOUSE')")
-    public ResponseEntity<List<SubcategoryProduct>> getAllSubcategoriesByCategory(@PathVariable Long id){
+    public ResponseEntity<List<SubcategoryProduct>> getAllSubcategoriesByCategory(@PathVariable @NotNull(
+            message = "El ID no puede estar vacío") Long id){
         return ResponseEntity.ok(subcategoryService.getSubcategoryByCategoryID(id));
     }
 
-    /**
-     * Crea una nueva subcategoría
-     * @param subcategory - Subcategoría con los datos a crear
-     * @return - ResponseEntity con un mensaje de éxito o error según sea el caso
-     */
     @PostMapping
     @PreAuthorize("hasAnyAuthority('ADMIN','MASTER', 'WAREHOUSE')")
-    public ResponseEntity<Map<String, String>> createSubcategories(@RequestBody SubcategoryProduct subcategory,
+    public ResponseEntity<Map<String, String>> createSubcategories(@RequestBody @Valid SubcategoryProduct subcategory,
                                                                    @AuthenticationPrincipal UserDetails userDetails){
-        Map<String, String> response = new HashMap<>();
-        try{
-            subcategoryService.save(subcategory);
-            response.put("message", "Subcategoría registrada correctamente");
-            recordService.registerAction(userDetails, "CREATE", "Subcategoría creada: " + subcategory.getName());
-            return ResponseEntity.ok(response);
-        }catch (IllegalArgumentException e){
-            response.put("message",e.getMessage());
-            recordService.registerAction(userDetails, "CREATE_FAILURE",
-                    "ERROR: crear subcategoría(" + subcategory.getName() + "): " + e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-        }
+        subcategoryService.save(subcategory);
+        recordService.registerAction(userDetails, "CREATE",
+                String.format("Subcategoría creada: %s", subcategory.getName()));
+        return new ResponseEntity<>(ResponseMessage.message("Subcategoría registrada correctamente"),
+                HttpStatus.CREATED);
     }
 }
