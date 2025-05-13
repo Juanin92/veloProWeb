2d5c30f8b05af3e91e2d265e92911986f40cdb6d
Juanin92 <j.claveria19@com>
2025-05-13 16:44:19 -0400

    feat(SubcategoryProduct): add validation annotations for name and category fields

--- veloProWeb/src/main/java/com/veloProWeb/model/entity/Product/SubcategoryProduct.java
+++ veloProWeb/src/main/java/com/veloProWeb/model/entity/Product/SubcategoryProduct.java
@@ -1,6 +1,10 @@
 package com.veloProWeb.model.entity.Product;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,8 +20,13 @@ public class SubcategoryProduct {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "El nombre de la subcategoría es obligatoria")
+    @Size(min = 3, message = "El nombre de la subcategoría debe tener al menos 2 caracteres")
+    @Pattern(regexp = "^(?!null|NULL$)[\\p{L} ]+$", message = "El nombre solo puede contener letras")
     private String name;
 
+    @NotNull(message = "Debe seleccionar una categoría")
     @ManyToOne
     @JoinColumn(name = "id_category", nullable = false)
     private CategoryProduct category;
