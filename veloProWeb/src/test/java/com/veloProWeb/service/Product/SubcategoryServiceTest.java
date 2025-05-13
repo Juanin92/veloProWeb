7e0641417d0d5a5de1b799a4cd9e96b4b510b142
Juanin92 <j.claveria19@com>
2025-05-13 16:45:27 -0400

    refactor(SubcategoryServiceTest): update tests to use builder pattern, enhance validation checks, and improve readability

--- veloProWeb/src/test/java/com/veloProWeb/service/Product/SubcategoryServiceTest.java
+++ veloProWeb/src/test/java/com/veloProWeb/service/Product/SubcategoryServiceTest.java
@@ -1,24 +1,24 @@
 package com.veloProWeb.service.Product;
 
+import com.veloProWeb.exceptions.product.CategoryNotFoundException;
+import com.veloProWeb.exceptions.product.SubcategoryAlreadyExistsException;
 import com.veloProWeb.model.entity.Product.CategoryProduct;
 import com.veloProWeb.model.entity.Product.SubcategoryProduct;
 import com.veloProWeb.repository.Product.CategoryProductRepo;
 import com.veloProWeb.repository.Product.SubcategoryProductRepo;
-import com.veloProWeb.util.TextFormatter;
-import com.veloProWeb.validation.CategoriesValidator;
+import com.veloProWeb.validation.SubcategoryValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.verify;
 
@@ -28,92 +28,105 @@ public class SubcategoryServiceTest {
     @InjectMocks private SubcategoryService subcategoryService;
     @Mock private SubcategoryProductRepo subcategoryProductRepo;
     @Mock private CategoryProductRepo categoryProductRepo;
-    @Mock private CategoriesValidator validator;
-    @Mock private TextFormatter textFormatter;
+    @Mock private SubcategoryValidator validator;
     private SubcategoryProduct subcategory;
-    private SubcategoryProduct existingSubcategory;
-    private SubcategoryProduct existingSubcategory2;
     private CategoryProduct category;
 
     @BeforeEach
     void setUp(){
-        category = new CategoryProduct();
-        category.setName("Comida");
-        category.setId(1L);
-        subcategory = new SubcategoryProduct();
-        subcategory.setName("Leche");
-        subcategory.setCategory(category);
-        existingSubcategory = new SubcategoryProduct();
-        existingSubcategory.setId(1L);
-        existingSubcategory.setName("Leche");
-        existingSubcategory.setCategory(category);
-        existingSubcategory2 = new SubcategoryProduct();
-        existingSubcategory2.setId(2L);
-        existingSubcategory2.setName("Arroz");
-        existingSubcategory2.setCategory(category);
+        category = CategoryProduct.builder().id(1L).name("Food").build();
     }
 
     //Prueba para crear una nueva subcategoría
     @Test
     public void save_valid(){
-        doNothing().when(validator).validateSubcategory("Leche");
-        when(subcategoryProductRepo.findByNameAndCategoryId("Leche",1L)).thenReturn(Optional.empty());
-        when(textFormatter.capitalize("Leche")).thenReturn("Leche");
+        subcategory = SubcategoryProduct.builder().id(null).name("Milk").category(category).build();
+        when(subcategoryProductRepo.findByNameAndCategoryId(subcategory.getName(), category.getId()))
+                .thenReturn(Optional.empty());
+        doNothing().when(validator).validateSubcategoryDoesNotExist(null);
+        doNothing().when(validator).validateSubcategoryHasCategory(subcategory);
+
         subcategoryService.save(subcategory);
 
-        verify(validator).validateSubcategory("Leche");
-        verify(subcategoryProductRepo).findByNameAndCategoryId("Leche", 1L);
-        verify(subcategoryProductRepo).save(subcategory);
-        assertEquals("Leche", subcategory.getName());
+        ArgumentCaptor<SubcategoryProduct> subcategoryArgument = ArgumentCaptor.forClass(SubcategoryProduct.class);
+        verify(subcategoryProductRepo, times(1)).findByNameAndCategoryId(subcategory.getName(),
+                category.getId());
+        verify(subcategoryProductRepo, times(1)).save(subcategoryArgument.capture());
+
+        SubcategoryProduct result = subcategoryArgument.getValue();
+        assertNull(result.getId());
+        assertEquals("Milk", result.getName());
+        assertEquals(category, result.getCategory());
     }
     @Test
     public void save_invalidExistingSubcategory(){
-        subcategory.setName("Arroz");
-        doNothing().when(validator).validateSubcategory("Arroz");
-        when(subcategoryProductRepo.findByNameAndCategoryId("Arroz", 1L)).thenReturn(Optional.of(existingSubcategory));
-        when(textFormatter.capitalize("Arroz")).thenReturn("Arroz");
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            subcategoryService.save(subcategory);
-        });
+        subcategory = SubcategoryProduct.builder().id(null).name("Milk").category(category).build();
+        when(subcategoryProductRepo.findByNameAndCategoryId(subcategory.getName(), category.getId()))
+                .thenReturn(Optional.of(subcategory));
+        doThrow(new SubcategoryAlreadyExistsException(String.format("Nombre Existente: Hay registro de esta " +
+                        "Subcategoría en la Categoría %s", subcategory.getCategory().getName())))
+                .when(validator).validateSubcategoryDoesNotExist(subcategory);
 
-        assertEquals("Nombre Existente: Hay registro de esta Subcategoría en la Categoría " + category.getName() + " .", exception.getMessage());
-        verify(validator).validateSubcategory("Arroz");
-        verify(subcategoryProductRepo).findByNameAndCategoryId("Arroz", 1L);
+        SubcategoryAlreadyExistsException exception = assertThrows(SubcategoryAlreadyExistsException.class,
+                () -> subcategoryService.save(subcategory));
+
+        verify(subcategoryProductRepo, times(1))
+                .findByNameAndCategoryId(subcategory.getName(), category.getId());
+        verify(validator, times(1)).validateSubcategoryDoesNotExist(subcategory);
         verify(subcategoryProductRepo, never()).save(subcategory);
+
+        assertEquals(String.format("Nombre Existente: Hay registro de esta Subcategoría en la Categoría %s",
+                subcategory.getCategory().getName()), exception.getMessage());
     }
     @Test
     public void save_invalidNullCategory(){
-        subcategory.setCategory(null);
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            subcategoryService.save(subcategory);
-        });
+        subcategory = SubcategoryProduct.builder().id(null).name("Milk").category(null).build();
+        doThrow(new CategoryNotFoundException("Dede seleccionar una categoría."))
+                .when(validator).validateSubcategoryHasCategory(subcategory);
+
+        CategoryNotFoundException exception = assertThrows(CategoryNotFoundException.class,
+                () -> validator.validateSubcategoryHasCategory(subcategory));
+
+        verify(subcategoryProductRepo, never()).findByNameAndCategoryId(subcategory.getName(), null);
+        verify(validator, times(1)).validateSubcategoryHasCategory(subcategory);
+        verify(subcategoryProductRepo, never()).save(subcategory);
 
         assertEquals("Dede seleccionar una categoría.", exception.getMessage());
-    }
-
-    //Prueba para obtener todas las subcategorías
-    @Test
-    public void getAll_valid(){
-        subcategoryService.getAll();
-        verify(subcategoryProductRepo).findAll();
     }
 
     //Prueba para obtener una lista de subcategoría por ID de categoría
     @Test
     public void getSubcategoryByCategoryID_valid(){
-        when(categoryProductRepo.getReferenceById(1L)).thenReturn(category);
-        List<SubcategoryProduct> expectedSubcategories = new ArrayList<>();
-        expectedSubcategories.add(existingSubcategory);
-        expectedSubcategories.add(existingSubcategory2);
+        subcategory = SubcategoryProduct.builder().id(1L).name("Milk").category(category).build();
+        SubcategoryProduct subcategory2 = SubcategoryProduct.builder().id(2L).name("Pasta").category(category).build();
+        SubcategoryProduct subcategory3 = SubcategoryProduct.builder().id(3L).name("Meat").category(category).build();
 
+        when(categoryProductRepo.findById(1L)).thenReturn(Optional.of(category));
+
+        List<SubcategoryProduct> expectedSubcategories = List.of(subcategory, subcategory2, subcategory3);
         when(subcategoryProductRepo.findByCategoryId(category.getId())).thenReturn(expectedSubcategories);
-        List<SubcategoryProduct> actualSubcategories = subcategoryService.getSubcategoryByCategoryID(1L);
 
-        assertEquals(expectedSubcategories.size(), actualSubcategories.size());
-        assertEquals(expectedSubcategories.get(0).getName(), actualSubcategories.get(0).getName());
-        assertEquals(expectedSubcategories.get(1).getName(), actualSubcategories.get(1).getName());
+        List<SubcategoryProduct> result = subcategoryService.getSubcategoryByCategoryID(1L);
 
-        verify(categoryProductRepo).getReferenceById(1L);
-        verify(subcategoryProductRepo).findByCategoryId(category.getId());
+        assertEquals(expectedSubcategories.size(), result.size());
+        assertEquals(expectedSubcategories.get(0).getName(), result.get(0).getName());
+        assertEquals(expectedSubcategories.get(1).getName(), result.get(1).getName());
+
+        verify(categoryProductRepo, times(1)).findById(1L);
+        verify(subcategoryProductRepo, times(1)).findByCategoryId(category.getId());
+    }
+    @Test
+    public void getSubcategoryByCategoryID_validEmptyList(){
+        when(categoryProductRepo.findById(1L)).thenReturn(Optional.of(category));
+
+        List<SubcategoryProduct> expectedSubcategories = List.of();
+        when(subcategoryProductRepo.findByCategoryId(category.getId())).thenReturn(expectedSubcategories);
+
+        List<SubcategoryProduct> result = subcategoryService.getSubcategoryByCategoryID(1L);
+
+        assertEquals(0, result.size());
+
+        verify(categoryProductRepo, times(1)).findById(1L);
+        verify(subcategoryProductRepo, times(1)).findByCategoryId(category.getId());
     }
 }
