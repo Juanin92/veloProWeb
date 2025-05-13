aef0425ac1e573489956d794eabd0cd41a418c9e
Juanin92 <j.claveria19@com>
2025-05-13 16:44:08 -0400

    refactor(ISubcategoryService): remove unused getAll method from interface

--- veloProWeb/src/main/java/com/veloProWeb/service/Product/Interfaces/ISubcategoryService.java
+++ veloProWeb/src/main/java/com/veloProWeb/service/Product/Interfaces/ISubcategoryService.java
@@ -6,6 +6,5 @@ import java.util.List;
 
 public interface ISubcategoryService {
     void save(SubcategoryProduct subcategory);
-    List<SubcategoryProduct> getAll();
     List<SubcategoryProduct> getSubcategoryByCategoryID(Long id);
 }
