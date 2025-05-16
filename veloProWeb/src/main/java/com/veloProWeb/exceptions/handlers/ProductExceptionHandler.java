f3274d00bb779e8afd29cba811453706ea4e28ba
Juanin92 <j.claveria19@com>
2025-05-16 16:13:57 -0400

    refactor(product-exception-handler): update exception handling methods for product-related exceptions

--- veloProWeb/src/main/java/com/veloProWeb/exceptions/handlers/ProductExceptionHandler.java
+++ veloProWeb/src/main/java/com/veloProWeb/exceptions/handlers/ProductExceptionHandler.java
@@ -1,15 +1,15 @@
 package com.veloProWeb.exceptions.handlers;
 
 import com.veloProWeb.exceptions.BaseExceptionHandler;
-import com.veloProWeb.exceptions.customer.CustomerNotFoundException;
-import com.veloProWeb.exceptions.customer.TicketNotFoundException;
 import com.veloProWeb.exceptions.product.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 
+@RestControllerAdvice
 public class ProductExceptionHandler extends BaseExceptionHandler {
 
     @ExceptionHandler({
@@ -20,12 +20,12 @@ public class ProductExceptionHandler extends BaseExceptionHandler {
             ProductAlreadyDeletedException.class,
             ProductAlreadyActivatedException.class
     })
-    public ResponseEntity<Map<String, String>> handleCustomerException(Exception e){
+    public ResponseEntity<Map<String, String>> handleProductException(Exception e){
         return buildResponse(e.getMessage(), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler({CategoryNotFoundException.class, ProductNotFoundException.class})
-    public ResponseEntity<Map<String, String>> handleCustomerNotFound(CategoryNotFoundException e) {
+    public ResponseEntity<Map<String, String>> handleProductNotFound(CategoryNotFoundException e) {
         return buildResponse(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
