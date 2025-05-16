0e621b55825b7da5b2bac0b5e01e097a5d380659
Juanin92 <j.claveria19@com>
2025-05-16 18:07:39 -0400

    refactor(stock-component): enhance product management by integrating ProductMapperService and improving product update handling

--- veloProweb-frontend/src/app/components/product/stock/stock.component.ts
+++ veloProweb-frontend/src/app/components/product/stock/stock.component.ts
@@ -1,31 +1,42 @@
 import { AfterViewInit, Component, OnInit, Renderer2 } from '@angular/core';
 import { ProductService } from '../../../services/Product/product.service';
-import { Product } from '../../../models/Entity/Product/product.model';
+import { Product } from '../../../models/Entity/Product/product';
 import { CommonModule, NgStyle } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { AddProductComponent } from "../add-product/add-product.component";
+import { AddProductComponent } from '../add-product/add-product.component';
 import { ProductHelperService } from '../../../services/Product/product-helper.service';
-import { UpdateProductComponent } from "../update-product/update-product.component";
-import { AddCategoriesComponent } from "../add-categories/add-categories.component";
+import { UpdateProductComponent } from '../update-product/update-product.component';
+import { AddCategoriesComponent } from '../add-categories/add-categories.component';
 import { RouterModule } from '@angular/router';
 import { StatusProduct } from '../../../models/enum/status-product';
 import { NotificationService } from '../../../utils/notification-service.service';
 import { ModalService } from '../../../utils/modal.service';
 import { TooltipService } from '../../../utils/tooltip.service';
 import { ProductPermissionsService } from '../../../services/Permissions/product-permissions.service';
+import { ProductUpdateForm } from '../../../models/Entity/Product/product-update-form';
+import { ErrorMessageService } from '../../../utils/error-message.service';
+import { ProductMapperService } from '../../../mapper/product-mapper.service';
 
 @Component({
   selector: 'app-stock',
   standalone: true,
-  imports: [CommonModule, NgStyle, FormsModule, RouterModule, AddProductComponent, UpdateProductComponent, AddCategoriesComponent],
+  imports: [
+    CommonModule,
+    NgStyle,
+    FormsModule,
+    RouterModule,
+    AddProductComponent,
+    UpdateProductComponent,
+    AddCategoriesComponent,
+  ],
   templateUrl: './stock.component.html',
-  styleUrl: './stock.component.css'
+  styleUrl: './stock.component.css',
 })
 export class StockComponent implements OnInit, AfterViewInit {
-
   products: Product[] = [];
   filteredProducts: Product[] = [];
-  selectedProduct: Product;
+  productUpdate: ProductUpdateForm;
+  selectedProductStock: Product;
   textFilter: string = '';
   statusProduct = StatusProduct;
   sortPosition: boolean = true;
@@ -39,12 +50,15 @@ export class StockComponent implements OnInit, AfterViewInit {
     private stockService: ProductService,
     private helper: ProductHelperService,
     private notification: NotificationService,
+    private mapper: ProductMapperService,
     public modalService: ModalService,
     private tooltipService: TooltipService,
-    private renderer: Renderer2) {
-    this.selectedProduct = helper.createEmptyProduct();
+    private renderer: Renderer2,
+    private errorMessage: ErrorMessageService
+  ) {
+    this.selectedProductStock = helper.createEmptyProduct();
+    this.productUpdate = helper.createEmptyProductUpdateForm();
   }
-
 
   ngAfterViewInit(): void {
     this.renderer.listen('document', 'mouseover', () => {
@@ -52,53 +66,63 @@ export class StockComponent implements OnInit, AfterViewInit {
     });
   }
 
-  /**
-   * Inicializa el componente cargando la lista de productos
-   */
   ngOnInit(): void {
     this.getProducts();
   }
+
 
   /**
    * Obtiene una lista de todos los productos.
    * asigna una lista con productos a la lista products y filteredProducts
    */
   getProducts(): void {
-    this.stockService.getProducts().subscribe((list) => {
-      this.products = list;
-      this.filteredProducts = list;
-    }, (error) => {
-      console.log('Error no se encontró ningún producto', error);
-    });
+    this.stockService.getProducts().subscribe(
+      (list) => {
+        this.products = list.map(productResponse => this.mapper.mapResponseToProduct(productResponse));
+        this.filteredProducts = [...this.products];
+      }
+    );
   }
 
   /**
-   * Elimina (Desactivar) un producto seleccionado 
+   * Elimina (Desactivar) un producto seleccionado
    * Valida si el producto no es valor nulo, si es correcto
    * lanza una confirmación antes de eliminar al cliente
    * @param product - Producto seleccionado
    */
   deleteProduct(product: Product): void {
-    this.selectedProduct = product;
-    if (this.selectedProduct) {
-      this.notification.showConfirmation(
-        "¿Estas seguro?",
-        "No podrás revertir la acción!",
-        "Si eliminar!",
-        "Cancelar"
-      ).then((result) => {
-        if (result.isConfirmed) {
-          this.stockService.deleteProduct(this.selectedProduct).subscribe((response) => {
-            console.log('Producto eliminado exitosamente:', response);
-            this.notification.showSuccessToast(`Se Elimino el producto ${this.selectedProduct.description} correctamente`, 'top', 3000);
-            this.getProducts();
-          }, (error) => {
-            const message = error.error.error;
-            console.log('Error al eliminar producto: ', message);
-            this.notification.showErrorToast(`Error al eliminar producto \n${message}`, 'top', 5000);
-          });
-        }
-      })
+    this.productUpdate = this.mapper.mapProductToUpdate(product);
+    if (this.productUpdate) {
+      this.notification
+        .showConfirmation(
+          '¿Estas seguro?',
+          'No podrás revertir la acción!',
+          'Si eliminar!',
+          'Cancelar'
+        )
+        .then((result) => {
+          if (result.isConfirmed) {
+            this.stockService.deleteProduct(this.productUpdate).subscribe({
+              next: (response) => {
+                this.notification.showSuccessToast(
+                  `${response.message}<br>${this.productUpdate.description}`,
+                  'top',
+                  3000
+                );
+                this.getProducts();
+              },
+              error: (error) => {
+                console.log('error: ',  error);
+                const message = this.errorMessage.errorMessageExtractor(error);
+                this.notification.showErrorToast(
+                  `Error al eliminar producto \n${message}`,
+                  'top',
+                  5000
+                );
+              },
+            });
+          }
+        });
     }
   }
 
@@ -108,16 +132,25 @@ export class StockComponent implements OnInit, AfterViewInit {
    * @param product - Producto seleccionado
    */
   activateProduct(product: Product): void {
-    this.selectedProduct = product;
-    if (this.selectedProduct) {
-      this.stockService.activeProduct(this.selectedProduct).subscribe((response) => {
-        console.log("Producto Activado");
-        this.notification.showSuccessToast(`Se activo nuevamente ${this.selectedProduct.description}.`, 'top', 3000);
-        this.getProducts();
-      }, (error) => {
-        const message = error.error.error;
-        console.log('Error al activar producto: ', message);
-        this.notification.showErrorToast(`Error al activar el producto \n${message}`, 'top', 5000);
+    this.productUpdate = this.mapper.mapProductToUpdate(product);
+    if (this.productUpdate) {
+      this.stockService.activeProduct(this.productUpdate).subscribe({
+        next: (response) => {
+          this.notification.showSuccessToast(
+          `${response.message}<br>${this.productUpdate.description}.`,
+            'top',
+            3000
+          );
+          this.getProducts();
+        },
+        error: (error) => {
+          const message = this.errorMessage.errorMessageExtractor(error);
+          this.notification.showErrorToast(
+            `Error al activar el producto \n${message}`,
+            'top',
+            5000
+          );
+        },
       });
     }
   }
@@ -128,7 +161,7 @@ export class StockComponent implements OnInit, AfterViewInit {
    */
   openModalProduct(product: Product): void {
     if (product) {
-      this.selectedProduct = { ...product };
+      this.selectedProductStock = { ...product };
       this.modalService.openModal();
     } else {
       console.error('No se pudo abrir modal, el producto no esta definido');
@@ -142,51 +175,55 @@ export class StockComponent implements OnInit, AfterViewInit {
    */
   statusColor(status: string): string {
     switch (status) {
-      case 'DISPONIBLE': return 'rgb(40, 238, 40)';
-      case 'DESCONTINUADO': return 'red';
-      case 'NODISPONIBLE': return 'rgb(9, 180, 237)';
-      default: return 'transparent';
+      case 'DISPONIBLE':
+        return 'rgb(40, 238, 40)';
+      case 'DESCONTINUADO':
+        return 'red';
+      case 'NODISPONIBLE':
+        return 'rgb(9, 180, 237)';
+      default:
+        return 'transparent';
     }
   }
 
-  toggleSortPosition(): void{
+  toggleSortPosition(): void {
     this.filteredProducts.reverse();
     this.sortPosition = !this.sortPosition;
   }
 
-  toggleSortSale(): void{
+  toggleSortSale(): void {
     this.sortSale = !this.sortSale;
     this.filteredProducts.sort((a, b) => {
-        const dateA = a.salePrice;
-        const dateB = b.salePrice;
-        return this.sortSale ? dateA - dateB : dateB - dateA;
+      const dateA = a.salePrice;
+      const dateB = b.salePrice;
+      return this.sortSale ? dateA - dateB : dateB - dateA;
     });
   }
 
-  toggleSortPurchase(): void{
+  toggleSortPurchase(): void {
     this.sortPurchase = !this.sortPurchase;
     this.filteredProducts.sort((a, b) => {
-        const dateA = a.buyPrice;
-        const dateB = b.buyPrice;
-        return this.sortPurchase ? dateA - dateB : dateB - dateA;
+      const dateA = a.buyPrice;
+      const dateB = b.buyPrice;
+      return this.sortPurchase ? dateA - dateB : dateB - dateA;
     });
   }
 
-  toggleSortStock(): void{
+  toggleSortStock(): void {
     this.sortStock = !this.sortStock;
     this.filteredProducts.sort((a, b) => {
-        const dateA = a.stock;
-        const dateB = b.stock;
-        return this.sortStock ? dateA - dateB : dateB - dateA;
+      const dateA = a.stock;
+      const dateB = b.stock;
+      return this.sortStock ? dateA - dateB : dateB - dateA;
     });
   }
 
-  toggleSortReserved(): void{
+  toggleSortReserved(): void {
     this.sortReserved = !this.sortReserved;
     this.filteredProducts.sort((a, b) => {
-        const dateA = a.reserve;
-        const dateB = b.reserve;
-        return this.sortReserved ? dateA - dateB : dateB - dateA;
+      const dateA = a.reserve;
+      const dateB = b.reserve;
+      return this.sortReserved ? dateA - dateB : dateB - dateA;
     });
   }
 
@@ -199,11 +236,20 @@ export class StockComponent implements OnInit, AfterViewInit {
     if (this.textFilter.trim() === '') {
       this.filteredProducts = this.products;
     } else {
-      this.filteredProducts = this.products.filter(product =>
-        product.brand.name.toLowerCase().includes(this.textFilter.toLowerCase()) ||
-        product.category.name.toLowerCase().includes(this.textFilter.toLowerCase()) ||
-        product.subcategoryProduct.name.toLowerCase().includes(this.textFilter.toLowerCase()) ||
-        product.description.toLowerCase().includes(this.textFilter.toLowerCase())
+      this.filteredProducts = this.products.filter(
+        (product) =>
+          product.brand
+            .toLowerCase()
+            .includes(this.textFilter.toLowerCase()) ||
+          product.category
+            .toLowerCase()
+            .includes(this.textFilter.toLowerCase()) ||
+          product.subcategoryProduct
+            .toLowerCase()
+            .includes(this.textFilter.toLowerCase()) ||
+          product.description
+            .toLowerCase()
+            .includes(this.textFilter.toLowerCase())
       );
     }
   }
