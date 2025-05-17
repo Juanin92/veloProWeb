d579f750e045f1c133d3e9bf4a82927b35739af3
Juanin92 <j.claveria19@com>
2025-05-17 19:17:51 -0400

    feat(ProductEventService): implement product event handling for stock changes and alerts

---
+++ veloProWeb/src/main/java/com/veloProWeb/service/product/ProductEventService.java
@@ -0,0 +1,31 @@
+package com.veloProWeb.service.product;
+
+import com.veloProWeb.model.Enum.MovementsType;
+import com.veloProWeb.model.dto.product.ProductUpdatedRequestDTO;
+import com.veloProWeb.model.entity.product.Product;
+import com.veloProWeb.service.Report.IkardexService;
+import com.veloProWeb.service.User.Interface.IAlertService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ProductEventService {
+
+    private final IAlertService alertService;
+    private final IkardexService kardexService;
+
+    public void handleCreateRegister(Product product, String comment){
+        kardexService.addKardex(product, 0, comment, MovementsType.AJUSTE);
+    }
+
+    public void isChangeStockOriginalValue(Product product, int originalStock, ProductUpdatedRequestDTO dto) {
+        if (originalStock != dto.getStock()) {
+            String comment = String.format("%s - stock original: %s, stock nuevo: %s", dto.getComment(),
+                    originalStock, dto.getStock());
+            int quantity = Math.abs(originalStock - dto.getStock());
+            kardexService.addKardex(product, quantity, comment, MovementsType.AJUSTE);
+            alertService.createAlert(product, comment);
+        }
+    }
+}
