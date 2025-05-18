f3076fca5272136193cbfb018ec7f036ebbf12a4
Juanin92 <j.claveria19@com>
2025-05-17 20:30:21 -0400

    feat(ProductRepo): add queries to find out-of-stock and critical stock products

--- veloProWeb/src/main/java/com/veloProWeb/repository/product/ProductRepo.java
+++ veloProWeb/src/main/java/com/veloProWeb/repository/product/ProductRepo.java
@@ -4,8 +4,10 @@ import com.veloProWeb.model.entity.product.Product;
 import io.micrometer.common.lang.NonNullApi;
 import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @NonNullApi
@@ -13,4 +15,10 @@ import java.util.Optional;
 public interface ProductRepo extends JpaRepository<Product,Long> {
 
     Optional<Product> findById(@Nonnull Long id);
+
+    @Query("SELECT p FROM Product p WHERE p.stock = 0")
+    List<Product> findOutOfStock();
+
+    @Query("SELECT p FROM Product p WHERE p.stock < p.threshold")
+    List<Product> findCriticalStock();
 }
