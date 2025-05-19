4253211c3e4c70328c11f82b626e9fe5b2845a40
Juanin92 <j.claveria19@com>
2025-05-19 19:27:02 -0400

    feat(SupplierRequestDTO): add SupplierRequestDTO with validation annotations

---
+++ veloProWeb/src/main/java/com/veloProWeb/model/dto/purchase/SupplierRequestDTO.java
@@ -0,0 +1,31 @@
+package com.veloProWeb.model.dto.purchase;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SupplierRequestDTO {
+
+    @NotBlank(message = "El nombre de la empresa es obligatorio")
+    private String name;
+
+    @NotBlank(message = "El rut es obligatorio")
+    @Pattern(regexp = "^\\d{7,8}-[\\dKk]$", message = "El rut no tiene un formato válido")
+    private String rut;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Debe ingresar un email válido")
+    private String email;
+
+    @NotBlank(message = "El Teléfono es obligatorio")
+    @Pattern(regexp = "^\\+569 \\d{8}$", message = "El número debe tener el formato: +569 12345678")
+    private String phone;
+}
