4e62db0e8e7d53eac4ce0d7a6f0ac90c21ffbd25
Juanin92 <j.claveria19@com>
2025-05-19 19:22:04 -0400

    refactor(Supplier): update purchaseList fetch type to LAZY and add Lombok annotations

--- veloProWeb/src/main/java/com/veloProWeb/model/entity/Purchase/Supplier.java
+++ veloProWeb/src/main/java/com/veloProWeb/model/entity/Purchase/Supplier.java
@@ -1,13 +1,19 @@
 package com.veloProWeb.model.entity.Purchase;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Supplier {
 
     @Id
@@ -18,7 +24,7 @@ public class Supplier {
     private String email;
     private String phone;
 
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true,fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "supplier", cascade = CascadeType.ALL, orphanRemoval = true,fetch = FetchType.LAZY)
     List<Purchase> purchaseList = new ArrayList<>();
 
     @Override
