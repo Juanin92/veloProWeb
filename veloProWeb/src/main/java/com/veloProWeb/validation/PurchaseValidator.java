804151b7e1a653b0765db562cc41c046a9340961
Juanin92 <j.claveria19@com>
2025-05-20 19:54:03 -0400

    fix(Purchase): refactor PurchaseValidator to improve supplier validation and expose validation methods

--- veloProWeb/src/main/java/com/veloProWeb/validation/PurchaseValidator.java
+++ veloProWeb/src/main/java/com/veloProWeb/validation/PurchaseValidator.java
@@ -1,5 +1,6 @@
 package com.veloProWeb.validation;
 
+import com.veloProWeb.exceptions.supplier.SupplierNotFoundException;
 import com.veloProWeb.model.entity.Purchase.Purchase;
 import com.veloProWeb.model.entity.Purchase.Supplier;
 import org.springframework.stereotype.Component;
@@ -10,46 +11,13 @@ import java.time.LocalDate;
 public class PurchaseValidator {
 
     /**
-     * Válida los datos de una compra
-     * @param purchase - Objeto con los valores de una compra
-     */
-    public void validate(Purchase purchase){
-        validateDate(purchase.getDate());
-        validateSupplier(purchase.getSupplier());
-        validateDocument(purchase.getDocument());
-        validateTotal(purchase.getPurchaseTotal());
-    }
-
-    /**
-     * Válida que la compra tenga una fecha, lanzará excepción
-     * Si la fecha es nula
-     * @param date - Formato de fecha a validar
-     */
-    private void validateDate(LocalDate date){
-        if (date == null){
-            throw new IllegalArgumentException("Ingrese una fecha");
-        }
-    }
-
-    /**
      * Válida que la compra tenga un proveedor seleccionado, lanzará una excepción
      * Si el valor de proveedor es nulo
      * @param supplier - Objeto con los datos de un proveedor
      */
-    private void validateSupplier(Supplier supplier){
+    public void hasSupplier(Supplier supplier){
         if (supplier == null) {
-            throw new IllegalArgumentException("Seleccione un proveedor");
-        }
-    }
-
-    /**
-     * Válida que la compra posea un documento, lanzará excepción
-     * Si el documento es nulo o si la cadena esta vaciá
-     * @param document - cadena que contiene el valor del documento
-     */
-    private void validateDocument(String document){
-        if (document == null || document.trim().isBlank()) {
-            throw new IllegalArgumentException("Ingrese número de documento");
+            throw new SupplierNotFoundException("Proveedor no encontrado");
         }
     }
 
@@ -58,7 +26,7 @@ public class PurchaseValidator {
      * si el total es menor o igual a 0
      * @param total - cantidad a validar
      */
-    private void validateTotal(int total){
+    public void validateTotalPurchase(int total){
         if (total <= 0) {
             throw new IllegalArgumentException("Ingrese sólo números");
         }
