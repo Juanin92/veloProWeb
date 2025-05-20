a320d884d149fa81317b0b067588d141ee3ca60c
Juanin92 <j.claveria19@com>
2025-05-19 20:57:50 -0400

    feat(SupplierService): refactor methods to use SupplierRequestDTO and SupplierResponseDTO for supplier operations

--- veloProWeb/src/main/java/com/veloProWeb/service/Purchase/SupplierService.java
+++ veloProWeb/src/main/java/com/veloProWeb/service/Purchase/SupplierService.java
@@ -1,79 +1,76 @@
 package com.veloProWeb.service.Purchase;
 
+import com.veloProWeb.exceptions.supplier.SupplierNotFoundException;
+import com.veloProWeb.mapper.SupplierMapper;
+import com.veloProWeb.model.dto.purchase.SupplierRequestDTO;
+import com.veloProWeb.model.dto.purchase.SupplierResponseDTO;
 import com.veloProWeb.model.entity.Purchase.Supplier;
 import com.veloProWeb.repository.Purchase.SupplierRepo;
 import com.veloProWeb.service.Purchase.Interfaces.ISupplierService;
 import com.veloProWeb.validation.SupplierValidator;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class SupplierService implements ISupplierService {
 
-    @Autowired private SupplierRepo supplierRepo;
-    @Autowired private SupplierValidator validator;
+    private final SupplierRepo supplierRepo;
+    private final SupplierValidator validator;
+    private final SupplierMapper mapper;
 
     /**
      * Crea un nuevo proveedor
      * Se válida que el proveedor no exista un registro de él, si no lanzara excepción
-     * Se válida que el email siempre un valor predeterminado si es nulo o está vacío
-     * Se válida todos los datos del proveedor sean correctos
-     * @param supplier - Objeto que contiene los valores necesarios
+     * @param dto - Objeto que contiene los valores necesarios
      */
+    @Transactional
     @Override
-    public void createSupplier(Supplier supplier) {
-        Supplier supplierDB = getSupplierCreated(supplier.getRut());
-        if (supplierDB != null) {
-            throw new IllegalArgumentException("Proveedor Existente: Hay un registro de este proveedor.");
-        }else {
-            if (supplier.getEmail() == null || supplier.getEmail().isEmpty()) {
-                supplier.setEmail("x@x.xxx");
-            }
-            validator.validate(supplier);
-            supplier.setId(null);
-            supplier.setEmail(supplier.getEmail());
-            supplier.setName(supplier.getName());
-            supplier.setRut(supplier.getRut());
-            supplier.setPhone(supplier.getPhone());
-            supplierRepo.save(supplier);
-        }
+    public void createSupplier(SupplierRequestDTO dto) {
+        Supplier supplierDB = getSupplierCreated(dto.getRut());
+        validator.validateSupplierDoesNotExist(supplierDB);
+        Supplier supplier = mapper.toEntity(dto);
+        supplierRepo.save(supplier);
     }
 
     /**
      * Actualiza los datos de un proveedor
-     * Se válida que el proveedor no sea nulo, si no lanzara una excepción
-     * Se válida que el email siempre un valor predeterminado si es nulo o está vacío
-     * Se válida todos los datos del proveedor sean correctos
-     * @param supplier - Objeto que contiene los valores necesarios
+     *
+     * @param dto - Objeto que contiene los valores necesarios
      */
+    @Transactional
     @Override
-    public void updateSupplier(Supplier supplier) {
-        if (supplier == null) {
-            throw new IllegalArgumentException("Debe seleccionar un proveedor para actualizar sus datos");
-        } else{
-            if (supplier.getEmail() == null || supplier.getEmail().isEmpty()) {
-                supplier.setEmail("x@x.xxx");
-            }
-            validator.validate(supplier);
-            supplier.setId(supplier.getId());
-            supplier.setEmail(supplier.getEmail());
-            supplier.setName(supplier.getName());
-            supplier.setRut(supplier.getRut());
-            supplier.setPhone(supplier.getPhone());
-            supplierRepo.save(supplier);
-        }
+    public void updateSupplier(SupplierRequestDTO dto) {
+        Supplier supplierExists = getSupplierCreated(dto.getRut());
+        validator.validateSupplierExists(supplierExists);
+        mapper.updateSupplierFromDto(supplierExists, dto);
+        supplierRepo.save(supplierExists);
     }
 
     /**
      * Obtiene el registro de todos los proveedores
+     *
      * @return - Lista con los proveedores encontrados
      */
     @Override
-    public List<Supplier> getAll() {
-        return supplierRepo.findAll();
+    public List<SupplierResponseDTO> getAll() {
+        return supplierRepo.findAll().stream()
+                .map(mapper::responseDTO)
+                .toList();
+    }
+
+    /**
+     * Busca un proveedor ya creado por su RUT
+     * @param rut - rut del proveedor
+     * @return - proveedor encontrado o null si no encuentra similitud
+     */
+    @Override
+    public SupplierResponseDTO getSupplierByRut(String rut) {
+        return supplierRepo.findByRut(rut).map(mapper::responseDTO)
+                .orElseThrow(() -> new SupplierNotFoundException("No existe registro del proveedor"));
     }
 
     /**
@@ -82,17 +79,6 @@ public class SupplierService implements ISupplierService {
      * @return - proveedor encontrado o null si no encuentra similitud
      */
     private Supplier getSupplierCreated(String rut){
-        Optional<Supplier> supplier = supplierRepo.findByRut(rut);
-        return supplier.orElse(null);
-    }
-
-    /**
-     * Obtener un proveedor por un ID
-     * @param id - Identificador del proveedor a obtener
-     * @return - Un proveedor si está presente o un objeto vació
-     */
-    @Override
-    public Optional<Supplier> getSupplierById(Long id) {
-        return supplierRepo.findById(id);
+        return supplierRepo.findByRut(rut).orElse(null);
     }
 }
