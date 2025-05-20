f3ef35530a2e2d472565cfa18c7ce1b1b8f46bc1
Juanin92 <j.claveria19@com>
2025-05-19 20:51:20 -0400

    feat(SupplierValidator): add validation methods for supplier existence checks

--- veloProWeb/src/main/java/com/veloProWeb/validation/SupplierValidator.java
+++ veloProWeb/src/main/java/com/veloProWeb/validation/SupplierValidator.java
@@ -1,5 +1,7 @@
 package com.veloProWeb.validation;
 
+import com.veloProWeb.exceptions.supplier.SupplierAlreadyExistsException;
+import com.veloProWeb.exceptions.supplier.SupplierNotFoundException;
 import com.veloProWeb.model.entity.Purchase.Supplier;
 import org.springframework.stereotype.Component;
 
@@ -7,57 +9,22 @@ import org.springframework.stereotype.Component;
 public class SupplierValidator {
 
     /**
-     * Válida los valores de un proveedor
-     * @param supplier - Objeto que contiene los valores
+     * Válida que el proveedor no exista en el sistema
+     * @param supplier - proveedor a consultar
      */
-    public void validate(Supplier supplier){
-        validateName(supplier.getName());
-        validatePhone(supplier.getPhone());
-        validateEmail(supplier.getEmail());
-        validateRut(supplier.getRut());
-    }
-
-    /**
-     * Válida el nombre del proveedor, lanzará una excepción
-     * Si el nombre es nulo, la cadena está vacía o el largo es menor a 3 caracteres
-     * @param name - cadena que contiene el nombre
-     */
-    private void validateName(String name){
-        if (name == null || name.trim().isBlank() || name.trim().length() < 3) {
-            throw new IllegalArgumentException("Ingrese un nombre válido.");
+    public void validateSupplierDoesNotExist(Supplier supplier){
+        if (supplier != null) {
+            throw new SupplierAlreadyExistsException("Ya hay un registro de este proveedor en el sistema.");
         }
     }
 
     /**
-     * Válida el número de teléfono, lanzará una excepción
-     * si la cadena es nula, vacía o el largo es diferente a 13 caracteres
-     * @param phone - cadena que contiene el número de teléfono
+     * Válida que el proveedor exista en el sistema
+     * @param supplier - Proveedor a consultar
      */
-    private void validatePhone(String phone){
-        if (phone == null || phone.trim().isBlank() || phone.trim().length() != 13) {
-            throw new IllegalArgumentException("Ingrese número válido, Ej: +569 12345678");
-        }
-    }
-
-    /**
-     * Válida el email del proveedor, lanzará una excepción
-     * si cadena esta vacía o el formato no corresponde a un email
-     * @param email - cadena que contiene el valor de email
-     */
-    private void validateEmail(String email){
-        if (email.trim().isBlank() || !email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")) {
-            throw new IllegalArgumentException("Ingrese Email válido.");
-        }
-    }
-
-    /**
-     * Válida el rut de un proveedor, lanzará una excepción
-     * si la cadena está vacía o el formato de la cadena no corresponde a un rut
-     * @param rut - cadena que contiene el rut del proveedor
-     */
-    private void validateRut(String rut){
-        if (rut.trim().isBlank() || !rut.matches("^\\d{7,8}-\\d|[kK]$")) {
-            throw new IllegalArgumentException("Rut no es correcto, Ingrese un formato válido");
+    public void validateSupplierExists(Supplier supplier){
+        if (supplier == null) {
+            throw new SupplierNotFoundException("No existe registro del proveedor");
         }
     }
 }
