dbbe70dd401a30e115bae1f8d0f0606cc2c87b86
Juanin92 <j.claveria19@com>
2025-05-20 21:19:27 -0400

    feat(Purchase): enhance PurchaseMapper with new methods for mapping purchase details and responses

--- veloProWeb/src/main/java/com/veloProWeb/mapper/PurchaseMapper.java
+++ veloProWeb/src/main/java/com/veloProWeb/mapper/PurchaseMapper.java
@@ -1,16 +1,22 @@
 package com.veloProWeb.mapper;
 
+import com.veloProWeb.model.dto.purchase.DetailPurchaseRequestDTO;
+import com.veloProWeb.model.dto.purchase.DetailPurchaseResponseDTO;
 import com.veloProWeb.model.dto.purchase.PurchaseRequestDTO;
+import com.veloProWeb.model.dto.purchase.PurchaseResponseDTO;
 import com.veloProWeb.model.entity.Purchase.Purchase;
+import com.veloProWeb.model.entity.Purchase.PurchaseDetail;
 import com.veloProWeb.model.entity.Purchase.Supplier;
+import com.veloProWeb.model.entity.product.Product;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.stream.Collectors;
 
 @Component
 public class PurchaseMapper {
 
-    public Purchase toEntity(PurchaseRequestDTO dto, Supplier supplier){
+    public Purchase toPurchaseEntity(PurchaseRequestDTO dto, Supplier supplier){
         return Purchase.builder()
                 .document(dto.getDocument())
                 .documentType(dto.getDocumentType())
@@ -18,6 +24,43 @@ public class PurchaseMapper {
                 .purchaseTotal(dto.getTotal())
                 .date(LocalDate.now())
                 .supplier(supplier)
+                .build();
+    }
+
+    public PurchaseDetail toPurchaseDetailEntity(DetailPurchaseRequestDTO dto, Product product, Purchase purchase){
+        return PurchaseDetail.builder()
+                .price(dto.getPrice())
+                .quantity(dto.getQuantity())
+                .tax(dto.getTax())
+                .total(dto.getTotal())
+                .product(product)
+                .purchase(purchase)
+                .build();
+    }
+
+    public PurchaseResponseDTO toPurchaseResponseDTO(Purchase purchase){
+        return PurchaseResponseDTO.builder()
+                .document(purchase.getDocument())
+                .documentType(purchase.getDocumentType())
+                .date(purchase.getDate())
+                .iva(purchase.getIva())
+                .purchaseTotal(purchase.getPurchaseTotal())
+                .supplier(purchase.getSupplier().getName())
+                .detailsList(
+                        purchase.getPurchaseDetails().stream()
+                        .map(this::toDetailPurchaseResponseDTO)
+                                .collect(Collectors.toList())
+                )
+                .build();
+    }
+
+    public DetailPurchaseResponseDTO toDetailPurchaseResponseDTO(PurchaseDetail purchaseDetail){
+        return DetailPurchaseResponseDTO.builder()
+                .descriptionProduct(purchaseDetail.getProduct().getDescription())
+                .price(purchaseDetail.getPrice())
+                .tax(purchaseDetail.getTax())
+                .total(purchaseDetail.getTotal())
+                .quantity(purchaseDetail.getQuantity())
                 .build();
     }
 }
