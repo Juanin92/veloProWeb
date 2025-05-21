08339d94648e3187a308719c80e8c928e292e16a
Juanin92 <j.claveria19@com>
2025-05-20 22:11:00 -0400

    feat(Purchase): refactor IPurchaseService and PurchaseService to use PurchaseResponseDTO

--- veloProWeb/src/main/java/com/veloProWeb/service/Purchase/Interfaces/IPurchaseService.java
+++ veloProWeb/src/main/java/com/veloProWeb/service/Purchase/Interfaces/IPurchaseService.java
@@ -1,14 +1,13 @@
 package com.veloProWeb.service.Purchase.Interfaces;
 
 import com.veloProWeb.model.dto.purchase.PurchaseRequestDTO;
+import com.veloProWeb.model.dto.purchase.PurchaseResponseDTO;
 import com.veloProWeb.model.entity.Purchase.Purchase;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IPurchaseService {
     Purchase createPurchase(PurchaseRequestDTO dto);
     Long totalPurchase();
-    List<PurchaseRequestDTO> getAllPurchases();
-    Optional<Purchase> getPurchaseById(Long id);
+    List<PurchaseResponseDTO> getAllPurchases();
 }
--- veloProWeb/src/main/java/com/veloProWeb/service/Purchase/PurchaseService.java
+++ veloProWeb/src/main/java/com/veloProWeb/service/Purchase/PurchaseService.java
@@ -1,6 +1,8 @@
 package com.veloProWeb.service.Purchase;
 
+import com.veloProWeb.mapper.PurchaseMapper;
 import com.veloProWeb.model.dto.purchase.PurchaseRequestDTO;
+import com.veloProWeb.model.dto.purchase.PurchaseResponseDTO;
 import com.veloProWeb.model.entity.Purchase.Purchase;
 import com.veloProWeb.model.entity.Purchase.Supplier;
 import com.veloProWeb.repository.Purchase.PurchaseRepo;
@@ -9,10 +11,9 @@ import com.veloProWeb.service.Purchase.Interfaces.IPurchaseService;
 import com.veloProWeb.validation.PurchaseValidator;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -21,25 +22,19 @@ public class PurchaseService implements IPurchaseService {
     private final PurchaseRepo purchaseRepo;
     private final SupplierRepo supplierRepo;
     private final PurchaseValidator validator;
+    private final PurchaseMapper mapper;
 
     /**
      * Crea una nueva compra.
      * @param dto - contiene los datos necesarios para crear la compra.
      * @return - objeto Compra que representa la compra creada y persistida en la BD.
-     * @throws IllegalArgumentException si el proveedor especificado no existe en la BD
      */
+    @Transactional
     @Override
     public Purchase createPurchase(PurchaseRequestDTO dto) {
-        Purchase purchase =  new Purchase();
-        purchase.setId(null);
-        purchase.setDocument(dto.getDocument());
-        purchase.setDocumentType(dto.getDocumentType());
-        purchase.setIva(dto.getTax());
-        purchase.setPurchaseTotal(dto.getTotal());
-        purchase.setDate(dto.getDate());
-        Optional<Supplier> supplier = supplierRepo.findById(dto.getIdSupplier());
-        purchase.setSupplier(supplier.orElseThrow(() -> new IllegalArgumentException("No ha seleccionado un proveedor")));
-        validator.validate(purchase);
+        Supplier supplier = supplierRepo.findByRut(dto.getSupplier()).orElse(null);
+        validator.hasSupplier(supplier);
+        Purchase purchase =  mapper.toPurchaseEntity(dto, supplier);
         purchaseRepo.save(purchase);
         return purchase;
     }
@@ -55,44 +50,12 @@ public class PurchaseService implements IPurchaseService {
 
     /**
      * Obtiene todas las compras registradas
-     * @return - Lista dto con las compras registradas
+     * @return - Lista DTO con las compras registradas
      */
     @Override
-    public List<PurchaseRequestDTO> getAllPurchases() {
-        List<Purchase> purchases = purchaseRepo.findAll();
-        List<PurchaseRequestDTO> dtoList = new ArrayList<>();
-        for (Purchase purchase : purchases){
-            PurchaseRequestDTO dto = convertToPurchaseRequestDTO(purchase);
-            dtoList.add(dto);
-        }
-        return dtoList;
-    }
-
-    /**
-     * Obtiene una compra especifíca
-     * @param id - Identificador de la compra a buscar
-     * @return - Una compra si está presente o un objeto vació
-     */
-    @Override
-    public Optional<Purchase> getPurchaseById(Long id) {
-        return purchaseRepo.findById(id);
-    }
-
-    /**
-     *  Convierte un objeto Purchase a un dto
-     * @param purchase - la compra que se va convertir
-     * @return - objeto dto que contiene los detalle de la compra
-     */
-    private PurchaseRequestDTO convertToPurchaseRequestDTO(Purchase purchase){
-        PurchaseRequestDTO dto = new PurchaseRequestDTO();
-        dto.setId(purchase.getId());
-        dto.setDate(purchase.getDate());
-        dto.setIdSupplier(purchase.getSupplier().getId());
-        dto.setDocumentType(purchase.getDocumentType());
-        dto.setDocument(purchase.getDocument());
-        dto.setTax(purchase.getIva());
-        dto.setTotal(purchase.getPurchaseTotal());
-        dto.setDetailList(null);
-        return dto;
+    public List<PurchaseResponseDTO> getAllPurchases() {
+        return purchaseRepo.findAll().stream()
+                .map(mapper::toPurchaseResponseDTO)
+                .toList();
     }
 }
