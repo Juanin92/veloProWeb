14ce0402b879711b49dd2ad869404b85a6b2436a
Juanin92 <j.claveria19@com>
2025-05-21 22:04:47 -0400

    refactor(Purchase): remove id and purchase fields from PurchaseDetails interface

--- veloProweb-frontend/src/app/models/Entity/Purchase/purchase-details.ts
+++ veloProweb-frontend/src/app/models/Entity/Purchase/purchase-details.ts
@@ -2,11 +2,9 @@ import { Product } from "../Product/product";
 import { Purchase } from "./purchase";
 
 export interface PurchaseDetails {
-    id: number;
     quantity: number;
     price: number;
     tax: number;
     total: number;
-    purchase: Purchase;
     product: Product;
 }
