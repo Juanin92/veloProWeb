4c35546a60b72ac8db682afd31b2c449eaba31f4
Juanin92 <j.claveria19@com>
2025-05-22 12:37:42 -0400

    feat(Purchase): add total validation method in PurchaseValidator

--- veloProWeb/src/test/java/com/veloProWeb/validation/PurchaseValidatorTest.java
+++ veloProWeb/src/test/java/com/veloProWeb/validation/PurchaseValidatorTest.java
@@ -1,10 +1,14 @@
 package com.veloProWeb.validation;
 
 import com.veloProWeb.exceptions.purchase.PurchaseNotFoundException;
+import com.veloProWeb.exceptions.purchase.PurchaseTotalMismatchException;
+import com.veloProWeb.model.dto.purchase.PurchaseDetailRequestDTO;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -19,5 +23,20 @@ public class PurchaseValidatorTest {
         PurchaseNotFoundException exception = assertThrows(PurchaseNotFoundException.class,
                 () -> validator.hasPurchase(null));
         assertEquals("Compra no encontrada", exception.getMessage());
+    }
+
+    //Prueba para validar el total de la compra
+    @Test
+    public void validateTotal_valid(){
+        PurchaseDetailRequestDTO dto = PurchaseDetailRequestDTO.builder().total(1000).build();
+        PurchaseDetailRequestDTO dto2 = PurchaseDetailRequestDTO.builder().total(1000).build();
+
+        PurchaseTotalMismatchException e = assertThrows(PurchaseTotalMismatchException.class,
+                ()-> validator.validateTotal(3000, List.of(dto, dto2)));
+        assertEquals("""
+                    Valor de total de compra no coinciden \
+                    
+                    total compra: 3000\s
+                    total de detalle: 2000""", e.getMessage());
     }
 }
