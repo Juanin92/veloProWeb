bb78baaf49665c4bf1ead0a8185cc7d93e884d60
Juanin92 <j.claveria19@com>
2025-05-22 19:26:01 -0400

    feat(Kardex): add KardexMapper to convert Kardex entities to KardexResponseDTO

---
+++ veloProWeb/src/main/java/com/veloProWeb/mapper/KardexMapper.java
@@ -0,0 +1,22 @@
+package com.veloProWeb.mapper;
+
+import com.veloProWeb.model.dto.KardexResponseDTO;
+import com.veloProWeb.model.entity.Kardex;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KardexMapper {
+
+    public KardexResponseDTO toResponseDTO(Kardex kardex){
+        return KardexResponseDTO.builder()
+                .date(kardex.getDate())
+                .product(kardex.getProduct().getDescription())
+                .stock(kardex.getStock())
+                .price(kardex.getPrice())
+                .movementsType(kardex.getMovementsType())
+                .quantity(kardex.getQuantity())
+                .user(String.format("%s %S", kardex.getUser().getName(), kardex.getUser().getSurname()))
+                .comment(kardex.getComment())
+                .build();
+    }
+}
