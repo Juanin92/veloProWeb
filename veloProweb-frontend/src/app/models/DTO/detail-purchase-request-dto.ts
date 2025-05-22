979b1510b6d9dfe069bf54b025d6d59868ae462c
Juanin92 <j.claveria19@com>
2025-05-22 18:54:23 -0400

    feat(Purchase): Delete dtos

--- veloProweb-frontend/src/app/models/DTO/detail-purchase-request-dto.ts
+++
@@ -1,7 +0,0 @@
-export interface DetailPurchaseRequestDTO {
-    descriptionProduct: string;
-    price: number;
-    tax: number;
-    quantity: number;
-    total: number;
-}
--- veloProweb-frontend/src/app/models/DTO/purchase-detail-dto.ts
+++
@@ -1,9 +0,0 @@
-export interface PurchaseDetailDTO {
-    id: number;
-    quantity: number;
-    price: number;
-    tax: number;
-    total: number;
-    idPurchase: number;
-    idProduct: number;
-}
