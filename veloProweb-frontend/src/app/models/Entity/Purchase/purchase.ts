b350670899fcb75bd1bc7b09e7a28002ca6669ee
Juanin92 <j.claveria19@com>
2025-05-21 20:51:59 -0400

    refactor(Purchase): update Purchase interface to rename purchaseTotal to total

--- veloProweb-frontend/src/app/models/Entity/Purchase/purchase.ts
+++ veloProweb-frontend/src/app/models/Entity/Purchase/purchase.ts
@@ -1,11 +1,10 @@
 import { Supplier } from "./supplier";
 
 export interface Purchase {
-    id: number;
     document: string;
     documentType: string;
     tax: number;
-    purchaseTotal: number;
+    total: number;
     date: string;
     supplier: Supplier | null;
 }
