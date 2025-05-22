686eb09157fa3b88fc3a82ba31878486b77dd486
Juanin92 <j.claveria19@com>
2025-05-21 22:05:23 -0400

    refactor(Purchase): update mapToPurchaseRequest to accept details and add mapping for PurchaseDetailRequest

--- veloProweb-frontend/src/app/mapper/purchase-mapper.service.ts
+++ veloProweb-frontend/src/app/mapper/purchase-mapper.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Purchase } from '../models/Entity/Purchase/purchase';
 import { PurchaseRequest } from '../models/Entity/Purchase/purchase-request';
+import { PurchaseDetailRequest } from '../models/Entity/Purchase/purchase-detail-request';
+import { PurchaseDetails } from '../models/Entity/Purchase/purchase-details';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,7 @@ export class PurchaseMapperService {
 
   constructor() { }
 
-  mapToPurchaseRequest(purchase: Purchase): PurchaseRequest{
+  mapToPurchaseRequest(purchase: Purchase, details: PurchaseDetailRequest[]): PurchaseRequest{
     return {
       supplier: purchase.supplier!.rut,
       documentType: purchase.documentType,
@@ -17,7 +19,22 @@ export class PurchaseMapperService {
       date: purchase.date,
       tax: purchase.tax,
       total: purchase.total,
-      detailList: []
+      detailList: details,
+    }
+  }
+
+  mapToPurchaseDetailRequest(details: PurchaseDetails[]): PurchaseDetailRequest[]{
+    return details.map(detail => this.mapDetailToRequest(detail));
+  }
+
+  mapDetailToRequest(detail: PurchaseDetails): PurchaseDetailRequest{
+    return  {
+      idProduct: detail.product.id,
+      idPurchase: 0,
+      price: detail.price,
+      tax: detail.tax,
+      quantity: detail.quantity,
+      total: detail.total
     }
   }
 }
