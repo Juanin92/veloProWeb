e71755ccb4926106d5989b78368cddea1fc41e60
Juanin92 <j.claveria19@com>
2025-05-22 23:32:49 -0400

    feat(Kardex): update IKardexService to include method for retrieving product movements since a specific date

--- veloProWeb/src/main/java/com/veloProWeb/service/Report/IKardexService.java
+++ veloProWeb/src/main/java/com/veloProWeb/service/Report/IKardexService.java
@@ -1,14 +1,16 @@
 package com.veloProWeb.service.Report;
 
 import com.veloProWeb.model.dto.KardexResponseDTO;
+import com.veloProWeb.model.entity.Kardex;
 import com.veloProWeb.model.entity.product.Product;
 import com.veloProWeb.model.Enum.MovementsType;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface IKardexService {
     void addKardex(UserDetails userDetails, Product product, int quantity, String comment, MovementsType moves);
     List<KardexResponseDTO> getAll();
-    void checkLowSales(Product product);
+    List<Kardex> getProductMovementsSinceDate(Product product, LocalDate starDate);
 }
