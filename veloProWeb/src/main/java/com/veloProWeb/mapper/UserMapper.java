d13db5aed7eb2087912149cfa17bf1f4ededc7ee
Juanin92 <j.claveria19@com>
2025-05-24 20:49:14 -0400

    feat(User): add UserMapper to convert UserRequestDTO to User entity

---
+++ veloProWeb/src/main/java/com/veloProWeb/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package com.veloProWeb.mapper;
+
+import com.veloProWeb.model.dto.user.UserRequestDTO;
+import com.veloProWeb.model.entity.User.User;
+import com.veloProWeb.util.TextFormatter;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class UserMapper {
+
+    public User toEntity(UserRequestDTO dto){
+        return User.builder()
+                .name(TextFormatter.capitalize(dto.getName()))
+                .surname(TextFormatter.capitalize(dto.getSurname()))
+                .username(dto.getUsername())
+                .password(dto.getUsername() + dto.getRut().substring(0,5))
+                .rut(dto.getRut())
+                .email(dto.getEmail())
+                .role(dto.getRole())
+                .date(LocalDate.now())
+                .status(true)
+                .token(null)
+                .build();
+    }
+}
