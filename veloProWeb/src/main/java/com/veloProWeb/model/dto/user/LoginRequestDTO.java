0c5780ffe9a5c8ba086709e21b137af6f9c615e1
Juanin92 <j.claveria19@com>
2025-05-27 17:10:19 -0400

    refactor(LoginRequest): rename to LoginRequestDTO and add validation annotations

--- veloProWeb/src/main/java/com/veloProWeb/model/dto/user/LoginRequestDTO.java
+++ veloProWeb/src/main/java/com/veloProWeb/model/dto/user/LoginRequestDTO.java
@@ -1,13 +1,20 @@
 package com.veloProWeb.model.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class LoginRequest {
+public class LoginRequestDTO {
+
+    @NotBlank(message = "El nombre de usuario es obligatorio")
     private String username;
+
+    @NotBlank(message = "La contraseña es obligatoria")
     private String password;
 }
