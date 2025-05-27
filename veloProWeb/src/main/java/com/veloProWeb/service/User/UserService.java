d454b6448d4352cfd6ae7457c6d00a087a2dabbd
Juanin92 <j.claveria19@com>
2025-05-27 11:58:49 -0400

    refactor(UserService): update user management methods to use new DTOs and improve validation logic

--- veloProWeb/src/main/java/com/veloProWeb/service/User/UserService.java
+++ veloProWeb/src/main/java/com/veloProWeb/service/User/UserService.java
@@ -1,138 +1,134 @@
 package com.veloProWeb.service.User;
 
-import com.veloProWeb.model.dto.UpdateUserDTO;
-import com.veloProWeb.model.dto.UserDTO;
+import com.veloProWeb.exceptions.user.UserNotFoundException;
+import com.veloProWeb.mapper.UserMapper;
+import com.veloProWeb.model.dto.user.UpdateUserDTO;
+import com.veloProWeb.model.dto.user.UserRequestDTO;
+import com.veloProWeb.model.dto.user.UserResponseDTO;
 import com.veloProWeb.model.entity.User.User;
 import com.veloProWeb.repository.UserRepo;
-import com.veloProWeb.security.Service.CodeGenerator;
 import com.veloProWeb.service.User.Interface.IUserService;
-import com.veloProWeb.util.EmailService;
 import com.veloProWeb.validation.UserValidator;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class UserService implements IUserService {
 
-    @Autowired private UserRepo userRepository;
-    @Autowired private UserValidator validator;
-    @Autowired private BCryptPasswordEncoder passwordEncoder;
-    @Autowired private CodeGenerator codeGenerator;
-    @Autowired private EmailService emailService;
+    private final UserRepo userRepository;
+    private final UserValidator validator;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final UserMapper mapper;
 
     /**
      * Crea un usuario nuevo.
      * Válida que no exista un registro del usuario y que el nombre de username no este tampoco registrado
      * @param dto - Objeto con los datos necesario de usuario
      */
+    @Transactional
     @Override
-    public void addUser(UserDTO dto) {
-        User userDB = getUserCreated(dto.getRut());
-        if (userDB != null){
-            throw new IllegalArgumentException("Usuario Existente: Ya hay existe el usuario");
-        } else {
-            User user = new User();
-            user.setId(null);
-            user.setDate(LocalDate.now());
-            user.setName(dto.getName().substring(0, 1).toUpperCase() + dto.getName().substring(1));
-            user.setSurname(dto.getSurname().substring(0, 1).toUpperCase() + dto.getSurname().substring(1));
-            user.setRut(dto.getRut());
-            user.setEmail(dto.getEmail());
-            user.setRole(dto.getRole());
-            user.setToken(null);
-            user.setStatus(true);
-            Optional<User> userNameExist = userRepository.findByUsername(dto.getUsername());
-            if (userNameExist.isPresent() && userNameExist.get().getUsername().equalsIgnoreCase(dto.getUsername())) {
-                throw new IllegalArgumentException("Este nombre de usuario ya existe");
-            }else {
-                user.setUsername(dto.getUsername());
-            }
-            user.setPassword(user.getUsername() + user.getRut().substring(0,5));
-            validator.validate(user);
-            user.setPassword(passwordEncoder.encode(user.getUsername() + user.getRut().substring(0,5)));
-            userRepository.save(user);
-        }
+    public void createUser(UserRequestDTO dto) {
+        validator.validateRoleIsNotMaster(dto.getRole());
+        validateUsernameIsAvailable(dto.getUsername());
+        User existingUser = findUserByRut(dto.getRut());
+        validator.validateUserDoesNotExist(existingUser);
+
+        User user = mapper.toEntity(dto);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
     }
 
     /**
-     * Actualizar los datos de un usuario seleccionado.
+     * Actualizar los datos de un usuario seleccionado. Actualizado por un administrador.
      * Válida que el username no esté registrado anteriormente y el usuario exista
      * @param dto - Objeto con los datos actualizados del usuario
      */
+    @Transactional
     @Override
-    public void updateUser(UserDTO dto) {
-        User existingUser = getUserCreated(dto.getRut());
-        if (existingUser == null){
-            throw new IllegalArgumentException("El usuario no existe en la base de datos.");
-        }
-        existingUser.setName(dto.getName().substring(0, 1).toUpperCase() + dto.getName().substring(1));
-        existingUser.setSurname(dto.getSurname().substring(0, 1).toUpperCase() + dto.getSurname().substring(1));
-        existingUser.setRut(dto.getRut());
-        Optional<User> userNameExist = userRepository.findByUsername(dto.getUsername());
-        if (userNameExist.isPresent() && !userNameExist.get().getId().equals(existingUser.getId())) {
-            throw new IllegalArgumentException("Este nombre de usuario ya existe");
-        }else {
-            existingUser.setUsername(dto.getUsername());
-        }
-        existingUser.setEmail(dto.getEmail());
-        existingUser.setToken(null);
-        validator.validate(existingUser);
-        existingUser.setPassword(passwordEncoder.encode(existingUser.getPassword()));
-        userRepository.save(existingUser);
+    public void updateUser(UserRequestDTO dto) {
+        User user = findUserByRut(dto.getRut());
+        validator.validateUserExists(user);
+        validateUsernameIsAvailableForUpdate(dto.getUsername(), dto.getRut());
+        user.setUsername(dto.getUsername());
+        user.setEmail(dto.getEmail());
+        userRepository.save(user);
+    }
+
+    /**
+     * Esta función es cuando un usuario actualiza sus propios datos.
+     * Actualiza los datos de un usuario existente, incluyendo la posibilidad de cambiar su contraseña.
+     * @param dto - Objeto con los datos actualizados del usuario
+     * @param username - Nombre de usuario del usuario que está actualizando sus datos
+     */
+    @Transactional
+    @Override
+    public void updateOwnData(UpdateUserDTO dto, String username) {
+        User user = getUserByUsername(username);
+        validator.validateIsNotDeleted(user.isStatus());
+        validator.validateUpdateUser(user, dto, existByUsername(dto.getUsername()), existByEmail(dto.getEmail()));
+
+        changePasswordWithCurrentCredentials(dto, user);
+
+        user.setUsername(dto.getUsername());
+        user.setEmail(dto.getEmail());
+        userRepository.save(user);
     }
 
     /**
      * Eliminar/Desactivar usuario.
      * Válida que el usuario exista y no esté ya eliminado
-     * @param username - Cadena con el nombre de usuario
+     * @param dto - Objeto con los datos del usuario
      */
+    @Transactional
     @Override
-    public void deleteUser(String username) {
-        User user = getUserWithUsername(username);
-        if (user.isStatus()) {
-            user.setStatus(false);
-            userRepository.save(user);
-        } else {
-            throw new IllegalArgumentException("El usuario ya está inactivo y no puede ser eliminado nuevamente.");
-        }
+    public void deleteUser(UserRequestDTO dto) {
+        User user = getUserByUsername(dto.getUsername());
+        validator.validateIsNotDeleted(user.isStatus());
+        user.setStatus(false);
+        userRepository.save(user);
     }
 
     /**
      * Activar un usuario.
      * Válida que usuario exista y esté eliminado
-     * @param username - Cadena con el nombre de usuario
+     * @param dto - Objeto con los datos del usuario
      */
+    @Transactional
     @Override
-    public void activateUser(String username) {
-        User user = getUserWithUsername(username);
-        if (!user.isStatus()) {
-            user.setStatus(true);
-            user.setDate(LocalDate.now());
-            userRepository.save(user);
-        } else {
-            throw new IllegalArgumentException("El usuario ya está activo y no puede ser activado nuevamente.");
-        }
+    public void activateUser(UserRequestDTO dto) {
+        User user = getUserByUsername(dto.getUsername());
+        validator.validateIsActivated(user.isStatus());
+        user.setStatus(true);
+        user.setDate(LocalDate.now());
+        userRepository.save(user);
     }
 
     /**
      * Obtiene una lista de todos los usuarios registrados
-     * @return - Lista con los usuarios
+     * @return - Lista de usuarios registrados
      */
     @Override
-    public List<UserDTO> getAllUser() {
-        return userRepository.findAll().stream().map(user -> {
-            return new UserDTO(user.getName(), user.getSurname(), user.getUsername(), user.getRut(),
-                    user.getEmail(), user.getRole(), user.isStatus(), user.getDate());
-        }).collect(Collectors.toList());
+    public List<UserResponseDTO> getAllUsers() {
+        return userRepository.findAll().stream()
+                .map(mapper::toResponseDTO)
+                .toList();
+    }
+
+    /**
+     * Obtiene los datos de un usuario existente
+     * @param username - Nombre de usuario a buscar
+     * @return - Clase DTO con los datos necesarios.
+     */
+    @Override
+    public UserResponseDTO getUserProfile(String username) {
+        User user = getUserByUsername(username);
+        return mapper.toResponseDTO(user);
     }
 
     /**
@@ -141,120 +137,57 @@ public class UserService implements IUserService {
      * @return - usuario con el mismo rut o null si no se encuentra
      */
     @Override
-    public User getUserWithUsername(String username) {
-        Optional<User> optionalUser = userRepository.findByUsername(username);
-        return optionalUser.orElseThrow(() -> new IllegalArgumentException("Usuario no encontrado"));
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("Usuario no encontrado"));
     }
 
     /**
-     * Verifica si la contraseña seleccionada coincide con la contraseña del usuario autenticado
-     * @param password - Contraseña entregada por el usuario
-     * @param userDetails - Usuario autenticado
-     * @return - True si hay coincidencia, False cuando no hay coincidencia
+     * Cambia la contraseña del usuario con las credenciales actuales.
+     * Valida que la contraseña actual sea correcta y que la nueva contraseña coincida.
+     * @param dto - Objeto con los datos de actualización del usuario
+     * @param user - Usuario al que se le cambiará la contraseña
      */
-    @Override
-    public boolean getAuthUser(String password, UserDetails userDetails) {
-        return passwordEncoder.matches(password, userDetails.getPassword());
-    }
-
-    /**
-     * Verifica que el código de seguridad coincide con el usuario autenticado.
-     * @param username - nombre de usuario autenticado
-     * @param token - código de seguridad
-     */
-    @Override
-    public void getAuthUserToken(String username, String token) {
-        User user = getUserWithUsername(username);
-        if (user != null && user.getToken() != null) {
-            if (!passwordEncoder.matches(token, user.getToken())){
-                throw new IllegalArgumentException("Los códigos de seguridad no tiene similitud");
-            }
-        }
-    }
-
-    /**
-     *  Envia un correo con el código de seguridad generado aleatoriamente.
-     *  Encripta el código al guardarlo
-     * @param username - Usuario al que se le envia el correo
-     */
-    @Override
-    public void sendEmailCode(String username) {
-        User user = getUserWithUsername(username);
-        String code = codeGenerator.generate();
-        emailService.sendPasswordResetCode(user, code);
-        user.setToken(passwordEncoder.encode(code));
-        userRepository.save(user);
-    }
-
-    @Override
-    public void sendEmailUpdatePassword(User user) {
-
-    }
-
-    /**
-     * Actualiza los datos de un usuario existente.
-     * Permite modificar el nombre de usuario, el correo electrónico y la contraseña de un usuario.
-     * Verifica que nombre de usuario y email nuevo no estén en uso.
-     * Verifica que la contraseña actual o el código de recuperación sean correctos.
-     * @param dto - Clase con los datos necesarios a modificar
-     * @param username - Nombre del usuario a modificar
-     */
-    @Override
-    public void updateUserData(UpdateUserDTO dto, String username) {
-        User user = getUserWithUsername(username);
-        if (!user.getUsername().equals(dto.getUsername()) && existByUsername(dto.getUsername())) {
-            throw new IllegalArgumentException("El nombre de usuario ya está en uso");
-        }
-        if (!user.getEmail().equals(dto.getEmail()) && existByEmail(dto.getEmail())) {
-            throw new IllegalArgumentException("El email ya está registrado");
-        }
+    private void  changePasswordWithCurrentCredentials(UpdateUserDTO dto, User user){
         if (dto.getCurrentPassword() != null && !dto.getCurrentPassword().isEmpty()) {
+            validator.validatePassword(dto);
             boolean isCurrentPasswordValid = passwordEncoder.matches(dto.getCurrentPassword(), user.getPassword());
-            boolean isTokenValid = user.getToken() != null && passwordEncoder.matches(dto.getCurrentPassword(), user.getToken());
-            if (!isCurrentPasswordValid && !isTokenValid) {
-                throw new IllegalArgumentException("La contraseña actual o el código de recuperación son incorrectos");
-            }
-            if (!dto.getNewPassword().equals(dto.getConfirmPassword())) {
-                throw new IllegalArgumentException("Las contraseñas nuevas no coinciden");
-            }
+            boolean isTokenValid = user.getToken() != null && passwordEncoder.matches(dto.getCurrentPassword(),
+                    user.getToken());
+            validator.validateCurrentCredentials(isCurrentPasswordValid, isTokenValid);
+            validator.validateNewPasswordMatch(dto);
             user.setPassword(passwordEncoder.encode(dto.getNewPassword()));
             user.setToken(null);
         }
-
-        user.setUsername(dto.getUsername());
-        user.setEmail(dto.getEmail());
-        validator.validateStatus(user.isStatus());
-        validator.validate(user);
-        userRepository.save(user);
     }
 
     /**
-     * Obtiene los datos de un usuario existente
-     * @param username - Nombre de usuario a buscar
-     * @return - Clase dto con los datos necesarios.
+     * Valida que el nombre de usuario no esté registrado.
+     * @param username - nombre de usuario a validar
      */
-    @Override
-    public UserDTO getData(String username) {
-        User user = getUserWithUsername(username);
-        return new UserDTO(user.getName(), user.getSurname(), user.getUsername(), user.getRut(),
-                user.getEmail(), user.getRole(), user.isStatus(), user.getDate());
+    private void validateUsernameIsAvailable(String username){
+        User user = userRepository.findByUsername(username).orElse(null);
+        validator.validateUsernameIsAvailable(user, username);
     }
 
-    @Override
-    public boolean hasRequiredRole(UserDetails userDetails, String... roles) {
-        return userDetails.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .anyMatch(authority -> java.util.Arrays.asList(roles).contains(authority));
+    /**
+     * Valida que el nombre de usuario no esté registrado al actualizar.
+     * @param newUsername - nuevo nombre de usuario a validar
+     * @param rutOriginal - RUT original del usuario
+     */
+    private void validateUsernameIsAvailableForUpdate(String newUsername, String rutOriginal) {
+        User user = userRepository.findByUsername(newUsername).orElse(null);
+        validator.validateUsernameIsAvailableForUpdate(user, rutOriginal);
     }
+
 
     /**
      * Obtiene un usuario ya registrado por su RUT.
      * @param rut - El RUT del usuario a buscar.
      * @return - usuario con el mismo RUT o null si no se encuentra
      */
-    private User getUserCreated(String rut){
-        Optional<User> user = userRepository.findByRut(rut);
-        return user.orElse(null);
+    private User findUserByRut(String rut){
+        return userRepository.findByRut(rut).orElse(null);
     }
 
     /**
@@ -273,19 +206,5 @@ public class UserService implements IUserService {
      */
     private boolean existByEmail(String email){
         return userRepository.existsByEmail(email);
-    }
-
-    /**
-     * Limpia los campos de token del usuario cada 2 hr automáticamente
-     */
-    @Scheduled(fixedRate = 7200000)
-    private void cleanTokenToUsers(){
-        List<User> usersWithTokens = userRepository.findByTokenIsNotNull();
-        if (!usersWithTokens.isEmpty()) {
-            for (User user : usersWithTokens) {
-                user.setToken(null);
-                userRepository.save(user);
-            }
-        }
     }
 }
