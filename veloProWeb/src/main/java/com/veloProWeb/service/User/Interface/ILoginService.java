d8489ad41bbdf1ec587adb556ef58ca5871abe92
Juanin92 <j.claveria19@com>
2025-05-27 16:20:28 -0400

    refactor(ILoginService): add methods for password validation and security token management

--- veloProWeb/src/main/java/com/veloProWeb/service/User/Interface/ILoginService.java
+++ veloProWeb/src/main/java/com/veloProWeb/service/User/Interface/ILoginService.java
@@ -1,4 +1,9 @@
 package com.veloProWeb.service.User.Interface;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface ILoginService {
+    boolean isPasswordValid(String password, UserDetails userDetails);
+    void validateSecurityToken(String username, String token);
+    void sendSecurityTokenByEmail(String username);
 }
