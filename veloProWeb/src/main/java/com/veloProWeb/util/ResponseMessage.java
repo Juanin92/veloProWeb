0ea54195cafe0f0f7de9c33d7cf7eab800d127bf
Juanin92 <j.claveria19@com>
2025-05-27 17:10:05 -0400

    feat(ResponseMessage): add methods for creating JSON response messages with various key-value pairs

--- veloProWeb/src/main/java/com/veloProWeb/util/ResponseMessage.java
+++ veloProWeb/src/main/java/com/veloProWeb/util/ResponseMessage.java
@@ -27,4 +27,33 @@ public class ResponseMessage {
         response.put(key, text);
         return response;
     }
+
+    /**
+     * Crear un mensaje de respuesta en formato JSON con dos claves y valores
+     * @param key - primera clave
+     * @param value - valor de la primera clave
+     * @param booleanKey - segunda clave
+     * @param booleanValue - valor de la segunda clave
+     * @return - un mapa con los mensajes
+     */
+    public static Map<String, Object> messageWithBooleanKey(String key, String value, String booleanKey,
+                                                            boolean booleanValue) {
+        Map<String, Object> response = new HashMap<>();
+        response.put(key, value);
+        response.put(booleanKey, booleanValue);
+        return response;
+    }
+
+    /**
+     * Crear un mensaje de respuesta en formato JSON con múltiples claves y valores
+     * @param keyValues - pares clave-valor
+     * @return - un mapa con los mensajes
+     */
+    public static Map<String, String> multiMessage(String... keyValues) {
+        Map<String, String> response = new HashMap<>();
+        for (int i = 0; i < keyValues.length; i += 2) {
+            response.put(keyValues[i], keyValues[i + 1]);
+        }
+        return response;
+    }
 }
