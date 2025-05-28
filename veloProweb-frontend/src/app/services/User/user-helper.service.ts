5a37d1e9b45ec60211c99281c98b1844c11c8e12
Juanin92 <j.claveria19@com>
2025-05-28 19:50:02 -0400

    feat(User): add userHelper service

---
+++ veloProweb-frontend/src/app/services/User/user-helper.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { UserResponse } from '../../models/Entity/user/user-response';
+import { UpdateUserForm } from '../../models/Entity/user/update-user-form';
+import { Role } from '../../models/enum/role';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserHelperService {
+
+  constructor() { }
+
+  public initializeUpdateUser(user: UserResponse, currentPassword: string, 
+    newPassword: string, newPasswordConfirmed: string): UpdateUserForm {
+      return {
+        username: user.username,
+        email: user.email,
+        currentPassword: currentPassword,
+        newPassword: newPassword,
+        confirmPassword: newPasswordConfirmed
+      }
+    }
+  
+    public initializeUser(): UserResponse{
+      return {
+        name: '',
+        surname: '',
+        username: '',
+        rut: '',
+        email: '',
+        status: true,
+        role: Role.VOID,
+        date: ''
+      }
+    }
+}
