56083bf95309f59bbca61c22a20b0abdc8d7b58d
Juanin92 <j.claveria19@com>
2025-05-29 16:00:54 -0400

    refactor(test): update TaskServiceTest to use TaskRequestDTO and TaskResponseDTO

--- veloProWeb/src/test/java/com/veloProWeb/service/communication/TaskServiceTest.java
+++ veloProWeb/src/test/java/com/veloProWeb/service/communication/TaskServiceTest.java
@@ -1,11 +1,13 @@
 package com.veloProWeb.service.communication;
 
-import com.veloProWeb.model.dto.TaskDTO;
+import com.veloProWeb.exceptions.communication.TaskNotFoundException;
+import com.veloProWeb.mapper.TaskMapper;
+import com.veloProWeb.model.dto.communication.TaskRequestDTO;
+import com.veloProWeb.model.dto.communication.TaskResponseDTO;
 import com.veloProWeb.model.entity.communication.Task;
 import com.veloProWeb.model.entity.User.User;
 import com.veloProWeb.repository.communication.TaskRepo;
 import com.veloProWeb.service.user.UserService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -14,7 +16,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,78 +28,90 @@ public class TaskServiceTest {
     @InjectMocks private TaskService taskService;
     @Mock private TaskRepo taskRepo;
     @Mock private UserService userService;
-    private Task task, task2,task3, task4;
-    private User user;
-    private TaskDTO dto;
-
-    @BeforeEach
-    void setUp(){
-        user = new User();
-        user.setUsername("Juan");
-        user.setId(1L);
-        task = new Task(1L, "Test 1", true, LocalDate.now(), user);
-        task2 = new Task(2L, "Test 1", false, LocalDate.now(), user);
-        task3 = new Task(3L, "Test 1", true, LocalDate.now(), user);
-        task4 = new Task(3L, "Test 1", true, LocalDate.now(), new User());
-        dto = new TaskDTO(1L,"TestDTO", true, LocalDate.now(), "Juan");
-    }
+    @Mock private TaskMapper mapper;
 
     //Prueba para obtener una lista de tareas de un usuario
     @Test
-    public void getTaskByUser_valid(){
-        List<Task> tasks = Arrays.asList(task,task2,task3,task4);
-        List<Task> taskListFiltered = Arrays.asList(task, task3);
-        when(userService.getUserByUsername(dto.getUser())).thenReturn(user);
-        when(taskRepo.findAll()).thenReturn(tasks);
-        List<TaskDTO> result = taskService.getTaskByUser(dto.getUser());
+    public void getTaskByUser(){
+        User user = User.builder().id(1L).username("johnny").name("John").surname("Doe").build();
+        Task task = Task.builder().id(1L).user(user).status(true).description("Task1").created(LocalDate.now()).build();
+        Task task2 = Task.builder().id(2L).user(user).status(true).description("Task").created(LocalDate.now()).build();
+        when(taskRepo.findByStatusTrueAndUser(user)).thenReturn(List.of(task, task2));
+        when(userService.getUserByUsername("johnny")).thenReturn(user);
+        TaskResponseDTO dto = TaskResponseDTO.builder().user("John Doe").status(true).description("Task1").id(1L).build();
+        TaskResponseDTO dto2 = TaskResponseDTO.builder().user("John Doe").status(true).description("Task").id(2L).build();
+        List<TaskResponseDTO> mappedDtos = List.of(dto, dto2);
+        when(mapper.toTaskResponse(task)).thenReturn(dto);
+        when(mapper.toTaskResponse(task2)).thenReturn(dto2);
 
-        verify(userService, times(1)).getUserByUsername(dto.getUser());
-        verify(taskRepo, times(1)).findAll();
-        assertEquals(taskListFiltered.size(), result.size());
-    }
+        List<TaskResponseDTO> result = taskService.getTaskByUser("johnny");
 
-    @Test
-    public void createTask_valid(){
-        when(userService.getUserByUsername(dto.getUser())).thenReturn(user);
-        taskService.createTask(dto);
-        ArgumentCaptor<Task> taskArgumentCaptor = ArgumentCaptor.forClass(Task.class);
-        verify(userService, times(1)).getUserByUsername(dto.getUser());
-        verify(taskRepo, times(1)).save(taskArgumentCaptor.capture());
-        Task result = taskArgumentCaptor.getValue();
-        assertEquals(result.getCreated(), LocalDate.now());
-        assertTrue(result.isStatus());
-        assertEquals(user, result.getUser());
-    }
-    @Test
-    public void createTask_validNotFoundTaskAndUser(){
-        TaskDTO nullDTO = new TaskDTO();
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,() -> taskService.createTask(dto));
-        verify(taskRepo, never()).save(task);
-        assertEquals("Tarea debe contener un usuario seleccionado!", e.getMessage());
+        verify(userService, times(1)).getUserByUsername("johnny");
+        verify(taskRepo, times(1)).findByStatusTrueAndUser(user);
+        verify(mapper, times(2)).toTaskResponse(any(Task.class));
+
+        assertEquals(mappedDtos.size(), result.size());
+        assertEquals(mappedDtos.getFirst().getDescription(), result.getFirst().getDescription());
     }
 
     //Prueba para crear una tarea a un usuario
     @Test
-    public void completeTask_valid(){
+    public void createTask(){
+        TaskRequestDTO dto = TaskRequestDTO.builder().description("test task").user("johnny").build();
+        User user = User.builder().id(1L).username("johnny").name("John").surname("Doe").build();
+        when(userService.getUserByUsername(dto.getUser())).thenReturn(user);
+        Task mappedTask = Task.builder().created(LocalDate.now()).description("test task").status(true)
+                .user(user).build();
+        when(mapper.toEntity(dto, user)).thenReturn(mappedTask);
+
+        ArgumentCaptor<Task> taskCaptor = ArgumentCaptor.forClass(Task.class);
+        taskService.createTask(dto);
+
+        verify(userService, times(1)).getUserByUsername(dto.getUser());
+        verify(mapper, times(1)).toEntity(dto, user);
+        verify(taskRepo, times(1)).save(taskCaptor.capture());
+
+        Task result = taskCaptor.getValue();
+        assertTrue(result.isStatus());
+        assertEquals(user, result.getUser());
+        assertEquals(mappedTask.getDescription(), result.getDescription());
+    }
+
+    //Prueba para dejar como completa una tarea
+    @Test
+    public void completeTask(){
+        Task task = Task.builder().id(1L).status(true).build();
         when(taskRepo.findById(1L)).thenReturn(Optional.of(task));
+
+        ArgumentCaptor<Task> taskCaptor = ArgumentCaptor.forClass(Task.class);
         taskService.completeTask(1L);
 
-        verify(taskRepo).save(task);
-        assertFalse(task.isStatus());
+        verify(taskRepo, times(1)).findById(1L);
+        verify(taskRepo, times(1)).save(taskCaptor.capture());
+
+        Task result = taskCaptor.getValue();
+        assertFalse(result.isStatus());
     }
     @Test
-    public void completeTask_validNotFoundTask(){
+    public void completeTask_taskNotFound(){
         when(taskRepo.findById(6L)).thenReturn(Optional.empty());
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,() -> taskService.completeTask(6L));
+        TaskNotFoundException e = assertThrows(TaskNotFoundException.class,() -> taskService.completeTask(6L));
 
         verify(taskRepo, never()).save(any(Task.class));
         assertEquals("Tarea no encontrada", e.getMessage());
     }
-    @Test
-    public void completeTask_validStatusFalse(){
-        when(taskRepo.findById(2L)).thenReturn(Optional.of(task2));
-        taskService.completeTask(2L);
 
-        verify(taskRepo, never()).save(task);
+    //Prueba para obtener todas las tareas
+    @Test
+    void getAllTasks() {
+        Task task = Task.builder().id(1L).status(true).description("Task1").created(LocalDate.now()).build();
+        Task task2 = Task.builder().id(2L).status(true).description("Task").created(LocalDate.now()).build();
+        when(taskRepo.findAll()).thenReturn(List.of(task, task2));
+
+        List<TaskResponseDTO> result = taskService.getAllTasks();
+
+        verify(taskRepo, times(1)).findAll();
+
+        assertEquals(2, result.size());
     }
 }
