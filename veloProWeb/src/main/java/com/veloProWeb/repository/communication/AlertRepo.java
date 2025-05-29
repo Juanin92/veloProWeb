8d2c88d2ab1eeccd416c041de53e134cbff5885e
Juanin92 <j.claveria19@com>
2025-05-29 18:08:25 -0400

    refactor(repo): update AlertRepo to use AlertStatus enum in query methods

--- veloProWeb/src/main/java/com/veloProWeb/repository/communication/AlertRepo.java
+++ veloProWeb/src/main/java/com/veloProWeb/repository/communication/AlertRepo.java
@@ -1,5 +1,6 @@
 package com.veloProWeb.repository.communication;
 
+import com.veloProWeb.model.Enum.AlertStatus;
 import com.veloProWeb.model.entity.product.Product;
 import com.veloProWeb.model.entity.communication.Alert;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,6 @@ import java.util.List;
 
 @Repository
 public interface AlertRepo extends JpaRepository<Alert, Long> {
-    List<Alert> findByProductAndDescriptionAndStatusIn(Product product, String description, List<String> status);
+    List<Alert> findByProductAndDescriptionAndStatusIn(Product product, String description, List<AlertStatus> status);
+    List<Alert> findByStatusNot(AlertStatus alertStatus);
 }
