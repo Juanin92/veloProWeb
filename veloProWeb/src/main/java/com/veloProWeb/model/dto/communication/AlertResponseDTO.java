56f61dccb2b1d2810ad8f002957c5b3b7bb2cdee
Juanin92 <j.claveria19@com>
2025-05-29 18:08:16 -0400

    feat(dto): add AlertResponseDTO for alert response handling

---
+++ veloProWeb/src/main/java/com/veloProWeb/model/dto/communication/AlertResponseDTO.java
@@ -0,0 +1,22 @@
+package com.veloProWeb.model.dto.communication;
+
+import com.veloProWeb.model.Enum.AlertStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlertResponseDTO {
+
+    private Long id;
+    private String description;
+    private AlertStatus status;
+    private LocalDate created;
+    private String product;
+}
