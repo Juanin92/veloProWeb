a29df1e282dbb49cc0847b9418658fa45c9dc0e3
Juanin92 <j.claveria19@com>
2025-05-29 18:08:44 -0400

    refactor(controller): update AlertController to use AlertResponseDTO and AlertStatus enum

--- veloProWeb/src/main/java/com/veloProWeb/controller/communication/AlertController.java
+++ veloProWeb/src/main/java/com/veloProWeb/controller/communication/AlertController.java
@@ -1,55 +1,44 @@
 package com.veloProWeb.controller.communication;
 
-import com.veloProWeb.model.entity.communication.Alert;
-import com.veloProWeb.service.reporting.interfaces.IRecordService;
+import com.veloProWeb.model.Enum.AlertStatus;
+import com.veloProWeb.model.dto.communication.AlertResponseDTO;
 import com.veloProWeb.service.communication.interfaces.IAlertService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import com.veloProWeb.util.ResponseMessage;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/alertas")
-@CrossOrigin(origins = "http://localhost:4200")
+@AllArgsConstructor
+@Validated
 public class AlertController {
 
-    @Autowired private IAlertService alertService;
-    @Autowired private IRecordService recordService;
+    private final IAlertService alertService;
 
     @GetMapping()
     @PreAuthorize("hasAnyAuthority('ADMIN', 'MASTER', 'WAREHOUSE')")
-    public ResponseEntity<List<Alert>> getAlerts(){
-        try {
-            return ResponseEntity.ok(alertService.getAlerts());
-        }catch (IllegalArgumentException e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+    public ResponseEntity<List<AlertResponseDTO>> getAlerts() {
+        return ResponseEntity.ok(alertService.getAlerts());
     }
 
     @PutMapping()
     @PreAuthorize("hasAnyAuthority('MASTER', 'WAREHOUSE')")
-    public ResponseEntity<Map<String, String>> handleStatus(@RequestBody Alert alert, @RequestParam int action,
-                                                            @AuthenticationPrincipal UserDetails userDetails){
-        Map<String, String> response = new HashMap<>();
-        try{
-            response.put("message", "Cambio de status correcto de la alerta");
-            alertService.handleAlertStatus(alert, action);
-            recordService.registerAction(userDetails, "UPDATE",
-                    String.format("Actualizar estado de la alerta %s (%s) ", alert.getDescription(), alert.getStatus()));
-            return ResponseEntity.ok(response);
-        }catch (IllegalArgumentException e){
-            response.put("error", e.getMessage());
-            recordService.registerAction(userDetails, "UPDATE_FAILURE",
-                    String.format("Error: actualizar estado de la alerta %s (%s) - %s ", alert.getDescription(),
-                            alert.getStatus(), e.getMessage()));
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-        }
+    public ResponseEntity<Map<String, String>> handleStatus(@RequestParam
+                                                            @NotNull(message = "ID de la alerta es obligatorio")
+                                                            Long alertId, @RequestParam
+                                                            @NotNull(message = "Debe seleccionar una opción")
+                                                                AlertStatus action,
+                                                            @AuthenticationPrincipal UserDetails userDetails) {
+        alertService.updateAlertStatus(alertId, action, userDetails);
+        return ResponseEntity.ok(ResponseMessage.message("Cambio de status correcto de la alerta"));
     }
 }
