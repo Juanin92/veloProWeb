146431c2af0199536add05d713ec30f0af4b39a4
Juanin92 <j.claveria19@com>
2025-05-29 15:59:37 -0400

    refactor(dto): rename TaskDTO to TaskResponseDTO and update package structure

--- veloProWeb/src/main/java/com/veloProWeb/model/dto/communication/TaskResponseDTO.java
+++ veloProWeb/src/main/java/com/veloProWeb/model/dto/communication/TaskResponseDTO.java
@@ -1,15 +1,17 @@
-package com.veloProWeb.model.dto;
+package com.veloProWeb.model.dto.communication;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class TaskDTO {
+public class TaskResponseDTO {
     private Long id;
     private String description;
     private boolean status;
