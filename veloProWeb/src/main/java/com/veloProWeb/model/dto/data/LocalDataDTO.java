57593b68cdd04dcddbc92273f35c0f37d46b347f
Juanin92 <j.claveria19@com>
2025-05-29 22:53:27 -0400

    feat(dto): add LocalDataDTO with validation annotations

---
+++ veloProWeb/src/main/java/com/veloProWeb/model/dto/data/LocalDataDTO.java
@@ -0,0 +1,32 @@
+package com.veloProWeb.model.dto.data;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LocalDataDTO {
+
+    @NotBlank(message = "El nombre de la empresa es obligatorio")
+    private String name;
+
+    @NotBlank(message = "El Teléfono es obligatorio")
+    @Pattern(regexp = "^\\+569 \\d{8}$", message = "El número debe tener el formato: +569 12345678")
+    private String phone;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Debe ingresar un email válido")
+    private String email;
+
+    private String emailSecurityApp;
+
+    @NotBlank(message = "La dirección de la empresa es obligatoria")
+    private String address;
+}
