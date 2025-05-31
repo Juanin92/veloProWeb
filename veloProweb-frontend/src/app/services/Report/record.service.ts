8e3cca3f4be6f5b9b835b238c2eeb6102a22925b
Juanin92 <j.claveria19@com>
2025-05-31 17:29:53 -0400

    fix(imports): update RecordService import path in register.component.ts

--- veloProweb-frontend/src/app/services/Report/record.service.ts
+++ veloProweb-frontend/src/app/services/Report/record.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Record } from '../models/Entity/record';
+import { Record } from '../../models/Entity/record';
 
 @Injectable({
   providedIn: 'root'
