b1b4d5b70ec0269a73b47312009f9ab759620b8f
Juanin92 <j.claveria19@com>
2025-05-31 20:15:12 -0400

    feat(controller): update CashRegisterController to use request/response DTOs and improve response handling

--- veloProWeb/src/main/java/com/veloProWeb/controller/Sale/CashRegisterController.java
+++ veloProWeb/src/main/java/com/veloProWeb/controller/Sale/CashRegisterController.java
@@ -1,9 +1,12 @@
 package com.veloProWeb.controller.Sale;
 
-import com.veloProWeb.model.dto.CashRegisterDTO;
+import com.veloProWeb.model.dto.sale.CashRegisterRequestDTO;
+import com.veloProWeb.model.dto.sale.CashRegisterResponseDTO;
 import com.veloProWeb.service.reporting.interfaces.IRecordService;
 import com.veloProWeb.service.sale.Interface.ICashRegisterService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.veloProWeb.util.ResponseMessage;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,17 +14,16 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/caja")
-@CrossOrigin(origins = "http://localhost:4200")
+@AllArgsConstructor
 public class CashRegisterController {
 
-    @Autowired private ICashRegisterService cashRegisterService;
-    @Autowired private IRecordService recordService;
+    private final ICashRegisterService cashRegisterService;
+    private final IRecordService recordService;
 
     /**
      * Obtiene una lista con los registros de caja
@@ -29,9 +31,9 @@ public class CashRegisterController {
      */
     @GetMapping
     @PreAuthorize("hasAnyAuthority('ADMIN', 'MASTER')")
-    public List<CashRegisterDTO> getCashRegisters(@AuthenticationPrincipal UserDetails userDetails){
+    public  ResponseEntity<List<CashRegisterResponseDTO>> getCashRegisters(@AuthenticationPrincipal UserDetails userDetails){
         recordService.registerAction(userDetails, "VIEW", "Lista de registro de la caja");
-        return cashRegisterService.getAll();
+        return ResponseEntity.ok(cashRegisterService.getAll());
     }
 
     @GetMapping("/verificar-apertura")
@@ -46,32 +48,18 @@ public class CashRegisterController {
     @PreAuthorize("hasAnyAuthority('ADMIN', 'MASTER', 'SELLER', 'GUEST')")
     public ResponseEntity<Map<String, String>> addRegisterOpening(@AuthenticationPrincipal UserDetails userDetails,
                                                                   @RequestBody int amount){
-        Map<String, String> response = new HashMap<>();
-        try{
-            cashRegisterService.addRegisterOpening(userDetails.getUsername(), amount);
-            recordService.registerAction(userDetails, "OPEN", String.format("Apertura de caja con $%s pesos", amount));
-            response.put("message", "Apertura de caja exitosa");
-            return ResponseEntity.ok(response);
-        }catch (Exception e){
-            response.put("message", e.getMessage());
-            return ResponseEntity.badRequest().body(response);
-        }
+        cashRegisterService.openRegister(userDetails.getUsername(), amount);
+        recordService.registerAction(userDetails, "OPEN", String.format("Apertura de caja con $%s pesos", amount));
+        return new ResponseEntity<>(ResponseMessage.message("Apertura de caja exitosa"), HttpStatus.CREATED);
     }
 
     @PostMapping("/cierre")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'MASTER', 'SELLER', 'GUEST')")
     public ResponseEntity<Map<String, String>> addRegisterClosing(@AuthenticationPrincipal UserDetails userDetails,
-                                                                  @RequestBody CashRegisterDTO dto){
-        Map<String, String> response = new HashMap<>();
-        try{
-            cashRegisterService.addRegisterClosing(userDetails.getUsername(), dto);
-            recordService.registerAction(userDetails, "CLOSE", String.format("Cierre de caja: $%s pesos - " +
-                    "POS: $%s pesos", dto.getAmountClosingCash(), dto.getAmountClosingPos()));
-            response.put("message", "Cierre de caja exitoso");
-            return ResponseEntity.ok(response);
-        }catch (Exception e){
-            response.put("message", e.getMessage());
-            return ResponseEntity.badRequest().body(response);
-        }
+                                                                  @RequestBody CashRegisterRequestDTO dto){
+        cashRegisterService.closeRegister(userDetails.getUsername(), dto);
+        recordService.registerAction(userDetails, "CLOSE", String.format("Cierre de caja: $%s pesos - " +
+                "POS: $%s pesos", dto.getAmountClosingCash(), dto.getAmountClosingPos()));
+        return new ResponseEntity<>(ResponseMessage.message("Cierre de caja exitoso"), HttpStatus.CREATED);
     }
 }
