a221acc676b1a30058182f8558887e29e6560df8
Juanin92 <j.claveria19@com>
2025-06-02 20:18:15 -0400

    refactor(service): update IDispatchService to use DispatchRequestDTO and DispatchResponseDTO; change method signatures to reflect new DTOs and DispatchStatus

--- veloProWeb/src/main/java/com/veloProWeb/service/sale/Interface/IDispatchService.java
+++ veloProWeb/src/main/java/com/veloProWeb/service/sale/Interface/IDispatchService.java
@@ -1,15 +1,17 @@
 package com.veloProWeb.service.sale.Interface;
 
-import com.veloProWeb.model.dto.DispatchDTO;
+import com.veloProWeb.model.Enum.DispatchStatus;
+import com.veloProWeb.model.dto.sale.DispatchRequestDTO;
+import com.veloProWeb.model.dto.sale.DispatchResponseDTO;
 import com.veloProWeb.model.entity.Sale.Dispatch;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface IDispatchService {
-    List<DispatchDTO> getDispatches();
-    Dispatch createDispatch(DispatchDTO dto);
-    void handleStatus(Long dispatchID, int action);
+    List<DispatchResponseDTO> getDispatches();
+    Dispatch createDispatch(DispatchRequestDTO dto);
+    void handleStatus(Long dispatchID, DispatchStatus statusAction);
     void handleDispatchReceiveToSale(Long dispatchID);
     Optional<Dispatch> getDispatchById(Long id);
 }
