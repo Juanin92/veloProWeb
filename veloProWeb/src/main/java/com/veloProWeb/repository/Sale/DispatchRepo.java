bc2e2bee303c7f4fb1cf108f2ed25359ed053a0a
Juanin92 <j.claveria19@com>
2025-06-02 23:02:12 -0400

    feat(repository): add method to find Dispatches by status

--- veloProWeb/src/main/java/com/veloProWeb/repository/Sale/DispatchRepo.java
+++ veloProWeb/src/main/java/com/veloProWeb/repository/Sale/DispatchRepo.java
@@ -1,9 +1,13 @@
 package com.veloProWeb.repository.Sale;
 
+import com.veloProWeb.model.Enum.DispatchStatus;
 import com.veloProWeb.model.entity.Sale.Dispatch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DispatchRepo extends JpaRepository<Dispatch, Long> {
+    List<Dispatch> findByStatusNot(DispatchStatus status);
 }
