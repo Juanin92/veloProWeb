9b799424d8b9c176250714252517721a628f6b69
Juanin92 <j.claveria19@com>
2025-06-04 17:35:37 -0400

    refactor(kardex): update Kardex interface to use string types for product and user fields

--- veloProweb-frontend/src/app/models/Entity/inventory/kardex.ts
+++ veloProweb-frontend/src/app/models/Entity/inventory/kardex.ts
@@ -1,16 +1,12 @@
 import { MovementsType } from "../../enum/movements-type";
-import { Product } from "../product/product";
-import { User } from "../user";
-
 
 export interface Kardex {
-    id: number;
-    date: Date;
-    quantity: number;
-    stock: number;
-    comment: string;
-    price: number;
-    movementsType: MovementsType;
-    product: Product;
-    user: User;
+    date: string,
+    product: string,
+    stock: number,
+    price: number,
+    movementsType: MovementsType,
+    quantity: number,
+    user: string,
+    comment: string,
 }
