1c9dabdb8a2a498d23b91793819d998fb374718e
Juanin92 <j.claveria19@com>
2025-06-04 17:34:25 -0400

    refactor(kardex): clean up KardexController by removing commented code and simplifying getAllKardex method

--- veloProWeb/src/main/java/com/veloProWeb/controller/inventory/KardexController.java
+++ veloProWeb/src/main/java/com/veloProWeb/controller/inventory/KardexController.java
@@ -4,7 +4,6 @@ import com.veloProWeb.model.dto.inventory.KardexResponseDTO;
 import com.veloProWeb.service.reporting.interfaces.IRecordService;
 import com.veloProWeb.service.inventory.IKardexService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,10 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/**
- * Controlador REST para gestionar operaciones relacionadas con lso registros/movimientos del inventario.
- * Este controlador proporciona endpoints para obtener listas de registros.
- */
 @RestController
 @RequestMapping("/kardex")
 @AllArgsConstructor
@@ -24,18 +19,10 @@ public class KardexController {
     private final IKardexService kardexService;
     private final IRecordService recordService;
 
-    /**
-     * Obtener todos los registros
-     * @return - ResponseEntity emite una lista de los registros
-     */
     @GetMapping
     @PreAuthorize("hasAnyAuthority('ADMIN', 'MASTER', 'WAREHOUSE')")
     public ResponseEntity<List<KardexResponseDTO>> getAllKardex(@AuthenticationPrincipal UserDetails userDetails){
-        try{
-            recordService.registerAction(userDetails, "VIEW_KARDEX", "Observo Reporte producto");
-            return ResponseEntity.ok(kardexService.getAll());
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+        recordService.registerAction(userDetails, "VIEW_KARDEX", "Observo Reporte producto");
+        return ResponseEntity.ok(kardexService.getAll());
     }
 }
