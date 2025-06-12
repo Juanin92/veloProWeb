5edff57f776f3365e72e4d6bae010c12c35cf9d6
Juanin92 <j.claveria19@com>
2025-06-12 10:33:16 -0400

    feat(sale): update ISaleService methods to return SaleResponseDTO and improve naming

--- veloProWeb/src/main/java/com/veloProWeb/service/sale/Interface/ISaleService.java
+++ veloProWeb/src/main/java/com/veloProWeb/service/sale/Interface/ISaleService.java
@@ -1,14 +1,14 @@
 package com.veloProWeb.service.sale.Interface;
 
 import com.veloProWeb.model.dto.sale.SaleRequestDTO;
+import com.veloProWeb.model.dto.sale.SaleResponseDTO;
 import com.veloProWeb.model.entity.Sale.Sale;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ISaleService {
     Sale createSale(SaleRequestDTO dto);
     Long totalSales();
-    List<SaleRequestDTO> getAllSale();
-    Optional<Sale> getSaleById(Long id);
+    List<SaleResponseDTO> getAllSales();
+    Sale getSaleById(Long id);
 }
