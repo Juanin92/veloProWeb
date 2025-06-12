6d4b47823b657cf5d04a6a4dc623eefd493feea1
Juanin92 <j.claveria19@com>
2025-06-12 10:31:04 -0400

    feat(sale): add SaleResponseDTO for sale transaction details

---
+++ veloProWeb/src/main/java/com/veloProWeb/model/dto/sale/SaleResponseDTO.java
@@ -0,0 +1,29 @@
+package com.veloProWeb.model.dto.sale;
+
+import com.veloProWeb.model.Enum.PaymentMethod;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaleResponseDTO {
+    private LocalDate date;
+    private PaymentMethod paymentMethod;
+    private String document;
+    private String comment;
+    private int discount;
+    private int tax;
+    private int totalSale;
+    private boolean status;
+    private String customer;
+    private LocalDate notification;
+    private boolean ticketStatus;
+    private List<SaleDetailResponseDTO> saleDetails;
+}
