309ed21e4023f15b765683db2800be34851af48b
Juanin92 <j.claveria19@com>
2025-06-12 10:27:57 -0400

    feat(sale): refactor SaleHelperService to initialize SaleRequest and remove unused methods

--- veloProweb-frontend/src/app/services/Sale/sale-helper.service.ts
+++ veloProweb-frontend/src/app/services/Sale/sale-helper.service.ts
@@ -3,63 +3,23 @@ import { Sale } from '../../models/entity/sale/sale';
 import { SaleDetail } from '../../models/entity/sale/sale-detail';
 import { SaleRequestDTO } from '../../models/DTO/sale-request-dto';
 import { SaleDetailDTO } from '../../models/DTO/sale-detail-dto';
+import { PaymentMethod } from '../../models/enum/payment-method';
+import { SaleRequest } from '../../models/entity/sale/sale-request';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SaleHelperService {
 
-  /**
-     * Crea una venta con valores predeterminados
-     * @returns - Venta con valores predeterminados
-     */
-  createEmptySale(): Sale {
+  initializeSaleRequest(): SaleRequest{
     return {
-      id: 0,
-      date: '',
+      idCustomer: 0,
       paymentMethod: null,
-      document: '',
-      comment: '',
-      discount: 0,
       tax: 0,
-      totalSale: 0,
-      status: false,
-      customer: null
+      total: 0,
+      discount: 0,
+      comment: '',
+      detailList: [],
     }
-  }
-
-  /**
-     * Crea un DTO de ventas para unir la información de una venta y sus detalles, 
-     * @param sale - Objeto venta con los valores necesarios
-     * @param details - lista de detalle de venta con valores
-     * @returns - DTO con los valores necesarios para realizar una solicitud.
-     */
-  createDto(sale: Sale, details: SaleDetail[], numberDocument: number, totalSale: number,
-    discount: number): SaleRequestDTO {
-    return {
-      id: 0,
-      date: sale.date,
-      idCustomer: sale.customer?.id ?? null,
-      paymentMethod: sale.paymentMethod!,
-      tax: sale.tax,
-      total: totalSale,
-      discount: discount,
-      numberDocument: numberDocument,
-      comment: sale.comment,
-      detailList: details.map(detail => this.createDetailDto(detail)),
-    };
-  }
-
-  /**
-   * Crea un DTO de detalle de ventas
-   * @param detail - Objeto detalle de ventas a utilizar sus valores
-   * @returns - DTO con valores necesarios
-   */
-  private createDetailDto(detail: SaleDetail): SaleDetailDTO {
-    return {
-      id: detail.id,
-      idProduct: detail.product.id,
-      quantity: detail.quantity,
-    };
   }
 }
