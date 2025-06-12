6442ec9e52c6961fa006f2c306a96045f97629d7
Juanin92 <j.claveria19@com>
2025-06-12 10:31:29 -0400

    feat(sale): add method to retrieve the most recently created sale

--- veloProWeb/src/main/java/com/veloProWeb/repository/Sale/SaleRepo.java
+++ veloProWeb/src/main/java/com/veloProWeb/repository/Sale/SaleRepo.java
@@ -2,9 +2,14 @@ package com.veloProWeb.repository.Sale;
 
 import com.veloProWeb.model.entity.Sale.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface SaleRepo extends JpaRepository<Sale, Long> {
 
+    @Query(nativeQuery = true, value = "SELECT * FROM sale ORDER BY date DESC, id DESC LIMIT 1")
+    Optional<Sale> findLastCreated();
 }
