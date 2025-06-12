e55c679aa48f8bb546a4759f36bdedbd81e85c2b
Juanin92 <j.claveria19@com>
2025-06-12 10:34:14 -0400

    feat(sale): refactor SaleService to improve sale creation and retrieval methods

--- veloProWeb/src/main/java/com/veloProWeb/service/sale/SaleService.java
+++ veloProWeb/src/main/java/com/veloProWeb/service/sale/SaleService.java
@@ -1,54 +1,46 @@
 package com.veloProWeb.service.sale;
 
-import com.veloProWeb.model.dto.sale.DetailSaleDTO;
-import com.veloProWeb.model.dto.sale.SaleRequestDTO;
-import com.veloProWeb.model.entity.customer.Customer;
-import com.veloProWeb.model.entity.Sale.Sale;
-import com.veloProWeb.model.entity.Sale.SaleDetail;
+import com.veloProWeb.exceptions.sale.SaleNotFoundException;
+import com.veloProWeb.mapper.SaleMapper;
 import com.veloProWeb.model.Enum.PaymentMethod;
+import com.veloProWeb.model.dto.sale.SaleRequestDTO;
+import com.veloProWeb.model.dto.sale.SaleResponseDTO;
+import com.veloProWeb.model.entity.Sale.Sale;
+import com.veloProWeb.model.entity.customer.Customer;
 import com.veloProWeb.repository.Sale.SaleRepo;
-import com.veloProWeb.service.customer.CustomerService;
-import com.veloProWeb.service.customer.TicketHistoryService;
+import com.veloProWeb.service.sale.Interface.ISaleEventService;
 import com.veloProWeb.service.sale.Interface.ISaleService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class SaleService implements ISaleService {
 
     private final SaleRepo saleRepo;
-    private final TicketHistoryService ticketHistoryService;
-    private final CustomerService customerService;
+    private final ISaleEventService eventService;
+    private final SaleMapper mapper;
 
     /**
      * Crea una nueva venta.
+     *
      * @param dto - contiene los datos necesarios para crear una venta.
      * @return - objeto venta que representa la venta creada y persistida en la BD.
      */
+    @Transactional
     @Override
     public Sale createSale(SaleRequestDTO dto) {
-        Sale sale = new Sale();
-        sale.setId(null);
-        sale.setDate(LocalDate.now());
-        sale.setDocument("BO_" + dto.getNumberDocument());
-        sale.setDiscount(dto.getDiscount());
-        sale.setStatus(true);
-        sale.setTax(dto.getTax());
-        sale.setTotalSale(dto.getTotal());
-        configureSaleByPaymentMethod(sale, dto);
+        Sale sale = buildSaleForPaymentMethod(dto);
         saleRepo.save(sale);
         return sale;
     }
 
     /**
      * Obtiene el número total de ventas registradas
+     *
      * @return - cantidad total de ventas
      */
     @Override
@@ -58,114 +50,64 @@ public class SaleService implements ISaleService {
 
     /**
      * Obtener todas las ventas registradas
-     * Crea una lista que contendrá los datos en la lista del objeto dto
+     * Crea una lista que contendrá los datos en la lista del objeto DTO
+     *
      * @return - La lista de objetos con los detalle de la venta
      */
     @Override
-    public List<SaleRequestDTO> getAllSale() {
-        List<Sale> saleList = saleRepo.findAll();
-        List<SaleRequestDTO> dtoList = new ArrayList<>();
-        for (Sale sale : saleList){
-            SaleRequestDTO dto = convertToSaleRequestDTO(sale);
-            dtoList.add(dto);
-        }
-        return dtoList;
+    public List<SaleResponseDTO> getAllSales() {
+        return saleRepo.findAll().stream()
+                .map(mapper::toResponseDTO)
+                .toList();
     }
 
     /**
      * Obtener una venta determinada por un ID
+     *
      * @param id - Identificador de la venta requerida
      * @return - Optional de venta encontrada
      */
     @Override
-    public Optional<Sale> getSaleById(Long id) {
-        return saleRepo.findById(id);
+    public Sale getSaleById(Long id) {
+        return saleRepo.findById(id).orElseThrow(() -> new SaleNotFoundException("Venta no encontrada"));
     }
 
     /**
      * Configura una sale dependiendo el método de pago realizo en la venta.
      * Si el pago es mixto o préstamo se actualiza la deuda del cliente
      * y se agrega un ticket asociado al cliente con la venta
-     * @param sale - Venta con los datos necesarios
-     * @param dto - dto con los datos que proporcionan información para la venta
-     * @throws IllegalArgumentException si el cliente especificado no existe en la BD
+     *
+     * @param dto - DTO con los datos que proporcionan información para la venta
      */
-    private void configureSaleByPaymentMethod(Sale sale,SaleRequestDTO dto) {
-        switch (dto.getPaymentMethod()){
-            case PRESTAMO -> {
-                sale.setPaymentMethod(PaymentMethod.PRESTAMO);
-                sale.setComment(null);
-                Customer customer = customerService.getCustomerById(dto.getIdCustomer());
-                sale.setCustomer(customer);
-                customer.setTotalDebt(customer.getDebt() + dto.getTotal());
-                customerService.updateTotalDebt(customer);
-                customerService.addSaleToCustomer(customer);
-                ticketHistoryService.addTicketToCustomer(customer, dto.getTotal());
-                break;
-            }
-            case MIXTO -> {
-                sale.setPaymentMethod(PaymentMethod.MIXTO);
-                sale.setComment("Abono inicial: $" + dto.getComment());
-                Customer customer = customerService.getCustomerById(dto.getIdCustomer());
-                sale.setCustomer(customer);
-                customer.setTotalDebt(customer.getDebt() + dto.getTotal());
-                customerService.updateTotalDebt(customer);
-                ticketHistoryService.addTicketToCustomer(customer, dto.getTotal());
-                break;
-            }
-            case DEBITO -> {
-                sale.setPaymentMethod(PaymentMethod.DEBITO);
-                sale.setCustomer(null);
-                sale.setComment("Comprobante: n°" + dto.getComment());
-                break;
-            }
-            case CREDITO -> {
-                sale.setPaymentMethod(PaymentMethod.CREDITO);
-                sale.setCustomer(null);
-                sale.setComment("Comprobante: n°" + dto.getComment());
-                break;
-            }
-            case TRANSFERENCIA -> {
-                sale.setPaymentMethod(PaymentMethod.TRANSFERENCIA);
-                sale.setCustomer(null);
-                    sale.setComment("Transferencia: n°" + dto.getComment());
-                break;
-            }
-            case EFECTIVO -> {
-                sale.setPaymentMethod(PaymentMethod.EFECTIVO);
-                sale.setCustomer(null);
-                sale.setComment("Efectivo: $" + dto.getComment());
-                break;
-            }
+    private Sale buildSaleForPaymentMethod(SaleRequestDTO dto) {
+        String lastDocument = getLastSaleDocument();
+        PaymentMethod paymentMethod = dto.getPaymentMethod();
+        Customer customer = eventService.needsCustomer(paymentMethod, dto.getIdCustomer());
+        String comment = switch (paymentMethod) {
+            case PRESTAMO -> null;
+            case MIXTO -> String.format("Abono inicial: $%s", dto.getComment());
+            case DEBITO, CREDITO -> String.format("Comprobante: n°%s", dto.getComment());
+            case TRANSFERENCIA -> String.format("Transferencia: n°%s", dto.getComment());
+            case EFECTIVO -> String.format("Efectivo: $%s", dto.getComment());
+        };
+        Sale sale = mapper.toSaleEntity(dto, lastDocument, paymentMethod, comment, customer);
+
+        switch (paymentMethod) {
+            case PRESTAMO -> eventService.createSaleLoanPaymentEvent(customer, dto.getTotal());
+            case MIXTO    -> eventService.createSaleMixPaymentEvent(customer, dto.getTotal());
         }
+
+        return sale;
     }
 
     /**
-     *  Convierte un objeto sale a un dto
-     * @param sale - la venta que se va convertir
-     * @return - objeto dto que contiene los detalle de la venta
+     * Obtiene la última venta creada
+     *
+     * @return - venta creado
      */
-    private SaleRequestDTO convertToSaleRequestDTO(Sale sale){
-        SaleRequestDTO dto = new SaleRequestDTO();
-        dto.setId(sale.getId());
-        dto.setDate(sale.getDate());
-        dto.setIdCustomer(sale.getCustomer() != null && sale.getCustomer().getId() != null ? sale.getCustomer().getId() : 0L);
-        dto.setPaymentMethod(sale.getPaymentMethod());
-        dto.setTax(sale.getTax());
-        dto.setTotal(sale.getTotalSale());
-        dto.setDiscount(sale.getDiscount());
-        dto.setComment(sale.getComment() + " # " + sale.getDocument());
-        dto.setNumberDocument(0);
-
-        List<DetailSaleDTO> detailSaleDTOS = new ArrayList<>();
-        for (SaleDetail detail : sale.getSaleDetails()){
-            DetailSaleDTO detailSaleDTO =  new DetailSaleDTO();
-            detailSaleDTO.setId(detail.getId());
-            detailSaleDTO.setIdProduct(detail.getProduct().getId());
-            detailSaleDTO.setQuantity(detail.getQuantity());
-            detailSaleDTOS.add(detailSaleDTO);
-        }
-        dto.setDetailList(detailSaleDTOS);
-        return dto;
+    private String getLastSaleDocument() {
+        return saleRepo.findLastCreated()
+                .map(Sale::getDocument)
+                .orElse("0000");
     }
 }
