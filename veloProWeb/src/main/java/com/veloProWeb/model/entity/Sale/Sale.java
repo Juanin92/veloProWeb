5a84ba1e0fcfc067a974a44a79c792dc8ea76ff7
Juanin92 <j.claveria19@com>
2025-06-12 10:31:58 -0400

    feat(sale): add Lombok annotations for Sale entity and update fetch strategy for saleDetails

--- veloProWeb/src/main/java/com/veloProWeb/model/entity/Sale/Sale.java
+++ veloProWeb/src/main/java/com/veloProWeb/model/entity/Sale/Sale.java
@@ -3,7 +3,10 @@ package com.veloProWeb.model.entity.Sale;
 import com.veloProWeb.model.entity.customer.Customer;
 import com.veloProWeb.model.Enum.PaymentMethod;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -13,6 +16,9 @@ import java.util.List;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Sale {
 
     @Id
@@ -34,9 +40,9 @@ public class Sale {
     private boolean status;
 
     @ManyToOne
-    @JoinColumn(name = "id_customer", nullable = true)
+    @JoinColumn(name = "id_customer")
     private Customer customer;
 
-    @OneToMany(mappedBy = "sale", cascade = CascadeType.ALL,orphanRemoval = true,fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "sale", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<SaleDetail> saleDetails = new ArrayList<>();
 }
