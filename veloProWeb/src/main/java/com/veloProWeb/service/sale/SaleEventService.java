03479ebca8641eaca8e1149783f800df825942ae
Juanin92 <j.claveria19@com>
2025-06-12 10:34:03 -0400

    feat(sale): add SaleEventService for handling sale payment events

---
+++ veloProWeb/src/main/java/com/veloProWeb/service/sale/SaleEventService.java
@@ -0,0 +1,59 @@
+package com.veloProWeb.service.sale;
+
+import com.veloProWeb.model.Enum.PaymentMethod;
+import com.veloProWeb.model.entity.customer.Customer;
+import com.veloProWeb.service.customer.interfaces.ICustomerService;
+import com.veloProWeb.service.customer.interfaces.ITicketHistoryService;
+import com.veloProWeb.service.sale.Interface.ISaleEventService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class SaleEventService implements ISaleEventService {
+
+    private final ICustomerService customerService;
+    private final ITicketHistoryService ticketHistoryService;
+
+
+    /**
+     * Crea los eventos de venta con método de pago préstamo.
+     *
+     * @param customer - Cliente que hace el pago.
+     * @param amount   - Monto del pago realizado.
+     */
+    @Override
+    public void createSaleLoanPaymentEvent(Customer customer, int amount) {
+        customer.setTotalDebt(customer.getDebt() + amount);
+        customerService.updateTotalDebt(customer);
+        customerService.addSaleToCustomer(customer);
+        ticketHistoryService.addTicketToCustomer(customer, amount);
+    }
+
+    /**
+     * Crea los eventos de venta con método de pago mixto.
+     *
+     * @param customer - Cliente que hace el pago.
+     * @param amount   - Monto del pago realizado.
+     */
+    @Override
+    public void createSaleMixPaymentEvent(Customer customer, int amount) {
+        customer.setTotalDebt(customer.getDebt() + amount);
+        customerService.updateTotalDebt(customer);
+        ticketHistoryService.addTicketToCustomer(customer, amount);
+    }
+
+    /**
+     * Verifica si se necesita un cliente para el método de pago.
+     * @param paymentMethod - Método de pago utilizado en la venta.
+     * @param idCustomer - Identificador del cliente.
+     * @return - Devuelve el cliente o de lo contrario devuelve null.
+     */
+    @Override
+    public Customer needsCustomer(PaymentMethod paymentMethod, Long idCustomer) {
+        if (paymentMethod == PaymentMethod.PRESTAMO || paymentMethod == PaymentMethod.MIXTO) {
+            return customerService.getCustomerById(idCustomer);
+        }
+        return  null;
+    }
+}
