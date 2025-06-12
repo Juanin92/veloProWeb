844adb678f2a088c7bb05c8a86bc20b5bca64271
Juanin92 <j.claveria19@com>
2025-06-12 10:33:50 -0400

    feat(sale): refactor SaleDetailService methods for improved clarity and consistency

--- veloProWeb/src/main/java/com/veloProWeb/service/sale/SaleDetailService.java
+++ veloProWeb/src/main/java/com/veloProWeb/service/sale/SaleDetailService.java
@@ -1,7 +1,8 @@
 package com.veloProWeb.service.sale;
 
-import com.veloProWeb.model.dto.sale.DetailSaleDTO;
+import com.veloProWeb.mapper.SaleMapper;
 import com.veloProWeb.model.dto.sale.DetailSaleRequestDTO;
+import com.veloProWeb.model.dto.sale.SaleDetailRequestDTO;
 import com.veloProWeb.model.entity.customer.Customer;
 import com.veloProWeb.model.entity.customer.TicketHistory;
 import com.veloProWeb.model.entity.product.Product;
@@ -19,6 +20,7 @@ import com.veloProWeb.service.inventory.IKardexService;
 import com.veloProWeb.service.sale.Interface.IDispatchService;
 import com.veloProWeb.service.sale.Interface.ISaleDetailService;
 import com.veloProWeb.service.sale.Interface.ISaleService;
+import com.veloProWeb.validation.SaleValidator;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -26,7 +28,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -41,6 +42,8 @@ public class SaleDetailService implements ISaleDetailService {
     private final ISaleService saleService;
     private final IKardexService kardexService;
     private final IDispatchService dispatchService;
+    private final SaleValidator validator;
+    private final SaleMapper mapper;
 
     /**
      * Crear detalle de ventas proporcionadas para una venta
@@ -49,35 +52,19 @@ public class SaleDetailService implements ISaleDetailService {
      * Crea un registro del movimiento del producto
      * @param dtoList - Lista de objetos dto que contienen los detalles de la venta.
      * @param sale - Objeto que representa la venta asociada a los detalles.
-     * @throws IllegalArgumentException Si no se encuentra un producto con el ID proporcionado en alguno de los detalles.
+     * @param userDetails - detalles del usuario autenticado.
      */
     @Override
-    public void createSaleDetailsToSale(List<DetailSaleDTO> dtoList, Sale sale, UserDetails userDetails) {
-        for (DetailSaleDTO dto : dtoList) {
+    public void addDetailsToSale(List<SaleDetailRequestDTO> dtoList, Sale sale, UserDetails userDetails) {
+        validator.hasSale(sale);
+        for (SaleDetailRequestDTO dto : dtoList) {
             Product product = productService.getProductById(dto.getIdProduct());
-            SaleDetail saleDetail = new SaleDetail();
-            saleDetail.setId(null);
-            saleDetail.setQuantity(dto.getQuantity());
-            saleDetail.setPrice((int) (product.getSalePrice() * 1.19));
-            saleDetail.setTax((int) (product.getSalePrice() * 0.19));
-            saleDetail.setTotal((int) ((product.getSalePrice() * 1.19) * dto.getQuantity()));
-            saleDetail.setSale(sale);
-            saleDetail.setDispatch(null);
-            saleDetail.setProduct(product);
+            SaleDetail saleDetail = mapper.toSaleDetailEntityFromSale(dto, product, sale);
             saleDetailRepo.save(saleDetail);
             productService.updateStockSale(product, saleDetail.getQuantity());
-            kardexService.addKardex(userDetails, product, dto.getQuantity(),
-                    "Venta / " + sale.getDocument(), MovementsType.SALIDA);
+            kardexService.addKardex(userDetails, product, dto.getQuantity(), "Venta " + sale.getDocument(),
+                    MovementsType.SALIDA);
         }
-    }
-
-    /**
-     * Obtiene el registro de todos los detalles de ventas
-     * @return - Lista con los detalles de ventas
-     */
-    @Override
-    public List<SaleDetail> getAll() {
-        return saleDetailRepo.findAll();
     }
 
     /**
@@ -88,21 +75,21 @@ public class SaleDetailService implements ISaleDetailService {
      * @return - Lista de dto con detalles de la venta
      */
     @Override
-    public List<DetailSaleRequestDTO> getSaleDetailsToSale(Long idSale) {
+    public List<DetailSaleRequestDTO> getDetailsBySaleId(Long idSale) {
         List<DetailSaleRequestDTO> saleRequestDTOS = new ArrayList<>();
         List<SaleDetail> saleDetails = saleDetailRepo.findBySaleId(idSale);
-        Optional<Sale> sale = saleService.getSaleById(idSale);
+        Sale sale = saleService.getSaleById(idSale);
         String customerNames = "";
         boolean status = true;
         LocalDate notification = null;
-        if (sale.isPresent()){
-            if (sale.get().getCustomer() != null){
-                Long idCustomer = sale.get().getCustomer().getId();
+        if (sale != null){
+            if (sale.getCustomer() != null){
+                Long idCustomer = sale.getCustomer().getId();
                 Customer customer = customerService.getCustomerById(idCustomer);
                 customerNames = customer.getName() + " " + customer.getSurname();
                 List<TicketHistory> ticketHistoryList = ticketHistoryRepo.findByCustomerId(customer.getId());
                 for (TicketHistory ticket : ticketHistoryList){
-                    if (ticket.getDocument().equals(sale.get().getDocument())){
+                    if (ticket.getDocument().equals(sale.getDocument())){
                         status = ticket.isStatus();
                         notification = ticket.getNotificationsDate();
                     }
@@ -131,47 +118,13 @@ public class SaleDetailService implements ISaleDetailService {
      * @param dispatch - Objeto que representa el despacho asociado a los detalles.
      */
     @Override
-    public void createSaleDetailsToDispatch(List<DetailSaleDTO> dtoList, Dispatch dispatch) {
-        for (DetailSaleDTO dto : dtoList) {
+    public void createSaleDetailsToDispatch(List<SaleDetailRequestDTO> dtoList, Dispatch dispatch) {
+        for (SaleDetailRequestDTO dto : dtoList) {
             Product product = productService.getProductById(dto.getIdProduct());
-            SaleDetail saleDetail = new SaleDetail();
-            saleDetail.setId(null);
-            saleDetail.setQuantity(dto.getQuantity());
-            saleDetail.setPrice((int) (product.getSalePrice() * 1.19));
-            saleDetail.setTax((int) (product.getSalePrice() * 0.19));
-            saleDetail.setTotal((int) ((product.getSalePrice() * 1.19) * dto.getQuantity()));
-            saleDetail.setSale(null);
-            saleDetail.setDispatch(dispatch);
-            saleDetail.setProduct(product);
+            SaleDetail saleDetail = mapper.toSaleDetailEntityFromDispatch(dto, product, dispatch);
             saleDetailRepo.save(saleDetail);
             productService.updateStockAndReserveDispatch(product, saleDetail.getQuantity(), true);
         }
-    }
-
-    /**
-     * Obtener detalle de venta de un despacho específico.
-     * @param idDispatch - Identificador del despacho.
-     * @return - Lista de dto con detalles de la venta
-     */
-    @Override
-    public List<DetailSaleRequestDTO> getSaleDetailsToDispatch(Long idDispatch) {
-        List<DetailSaleRequestDTO> saleRequestDTOS = new ArrayList<>();
-        List<SaleDetail> saleDetails = saleDetailRepo.findByDispatchId(idDispatch);
-        Optional<Dispatch> dispatch = dispatchService.getDispatchById(idDispatch);
-        if (dispatch.isPresent()){
-            for (SaleDetail saleDetail : saleDetails){
-                DetailSaleRequestDTO dto = new DetailSaleRequestDTO();
-                dto.setQuantity(saleDetail.getQuantity());
-                dto.setPrice(saleDetail.getPrice());
-                dto.setTax(saleDetail.getTax());
-                dto.setDescriptionProduct(saleDetail.getProduct().getDescription());
-                dto.setCustomer(null);
-                dto.setTicketStatus(false);
-                dto.setNotification(null);
-                saleRequestDTOS.add(dto);
-            }
-        }
-        return saleRequestDTOS;
     }
 
     /**
@@ -184,23 +137,19 @@ public class SaleDetailService implements ISaleDetailService {
      */
     @Override
     public void addSaleToSaleDetailsDispatch(Long idDispatch, Sale sale) {
-        Optional<Dispatch> dispatch = dispatchService.getDispatchById(idDispatch);
-        if (dispatch.isPresent()) {
-            List<SaleDetail> saleDetails = saleDetailRepo.findByDispatchId(dispatch.get().getId());
-            int totalTax = 0;
-            for (SaleDetail saleDetail : saleDetails){
-                saleDetail.setSale(sale);
-                saleDetailRepo.save(saleDetail);
-                totalTax += saleDetail.getTax() * saleDetail.getQuantity();
-                productService.updateStockAndReserveDispatch(saleDetail.getProduct(), saleDetail.getQuantity(), false);
-                productService.updateStockSale(saleDetail.getProduct(), saleDetail.getQuantity());
-            }
-            dispatch.get().setHasSale(true);
-            dispatchRepo.save(dispatch.get());
-            sale.setTax(totalTax);
-            saleRepo.save(sale);
-        }else {
-            throw new IllegalArgumentException("No se encontró el despacho");
+        Dispatch dispatch = dispatchService.getDispatchById(idDispatch);
+        List<SaleDetail> saleDetails = saleDetailRepo.findByDispatchId(dispatch.getId());
+        int totalTax = 0;
+        for (SaleDetail saleDetail : saleDetails){
+            saleDetail.setSale(sale);
+            saleDetailRepo.save(saleDetail);
+            totalTax += saleDetail.getTax() * saleDetail.getQuantity();
+            productService.updateStockAndReserveDispatch(saleDetail.getProduct(), saleDetail.getQuantity(), false);
+            productService.updateStockSale(saleDetail.getProduct(), saleDetail.getQuantity());
         }
+        dispatch.setHasSale(true);
+        dispatchRepo.save(dispatch);
+        sale.setTax(totalTax);
+        saleRepo.save(sale);
     }
 }
