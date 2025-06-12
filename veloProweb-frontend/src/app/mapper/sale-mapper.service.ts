56054cc30d1df3fe77948386908a476db795fff1
Juanin92 <j.claveria19@com>
2025-06-12 17:03:50 -0400

    feat(sale): add mapping method for SaleDetailResponse to SaleDetailRequest

--- veloProweb-frontend/src/app/mapper/sale-mapper.service.ts
+++ veloProweb-frontend/src/app/mapper/sale-mapper.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { SaleDetail } from '../models/entity/sale/sale-detail';
 import { SaleDetailRequest } from '../models/entity/sale/sale-detail-request';
+import { SaleDetailResponse } from '../models/entity/sale/sale-detail-response';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,20 @@ export class SaleMapperService {
       return details.map(detail => this.mapDetailToRequest(detail));
   }
 
-  mapDetailToRequest(detail: SaleDetail): SaleDetailRequest{
+  mapSaleDetailResponseToRequest(details: SaleDetailResponse[]): SaleDetailRequest[]{
+      return details.map(detail => this.mapDetailResponseToRequest(detail));
+  }
+
+  private mapDetailToRequest(detail: SaleDetail): SaleDetailRequest{
     return {
       idProduct: detail.product.id,
+      quantity: detail.quantity,
+    }
+  }
+
+  private mapDetailResponseToRequest(detail: SaleDetailResponse): SaleDetailRequest{
+    return {
+      idProduct: detail.idProduct,
       quantity: detail.quantity,
     }
   }
