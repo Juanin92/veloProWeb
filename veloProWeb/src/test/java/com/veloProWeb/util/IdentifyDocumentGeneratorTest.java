23738fe7afd793f92fdd10e5971689332686ba1d
Juanin92 <j.claveria19@com>
2025-06-13 17:41:05 -0400

    refactor(test): update IdentifyDocumentGeneratorTest to use dynamic date in document identifiers

--- veloProWeb/src/test/java/com/veloProWeb/util/IdentifyDocumentGeneratorTest.java
+++ veloProWeb/src/test/java/com/veloProWeb/util/IdentifyDocumentGeneratorTest.java
@@ -1,48 +1,56 @@
 package com.veloProWeb.util;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(MockitoExtension.class)
 public class IdentifyDocumentGeneratorTest {
+
+    String date = LocalDate.now().format(DateTimeFormatter.ofPattern("MMyy"));
 
     // Prueba para generar identificador para un ticket
     @Test
     public void generateIdentifyDocumentTicket_valid(){
-        String document = "T-0525-0001";
+        String document = String.format("T-%s-0001",date);
         String result = IdentifyDocumentGenerator.generateIdentifyDocumentTicket(document);
-        assertEquals("T-0525-0002", result);
+        assertEquals(String.format("T-%s-0002",date), result);
     }
     @Test
     public void generateIdentifyDocumentTicket_FirstDocument(){
         String result = IdentifyDocumentGenerator.generateIdentifyDocumentTicket(null);
-        assertEquals("T-0525-0001", result);
+        assertEquals(String.format("T-%s-0001",date), result);
     }
     @ParameterizedTest
     @ValueSource(strings = {"T_21", "TicketTest01"})
     public void generateIdentifyDocumentTicket_DifferentFormatDocument(String document){
         String result = IdentifyDocumentGenerator.generateIdentifyDocumentTicket(document);
-        assertEquals("T-0525-0001", result);
+        assertEquals(String.format("T-%s-0001",date), result);
     }
 
     // Prueba para generar identificador para una boleta de venta
     @Test
     public void generateIdentifyDocumentSale_valid(){
-        String document = "BO-0525-0001";
+        String document = String.format("BO-%s-0001",date);
         String result = IdentifyDocumentGenerator.generateIdentifyDocumentSale(document);
-        assertEquals("BO-0525-0002", result);
+        assertEquals(String.format("BO-%s-0002",date), result);
     }
     @Test
     public void generateIdentifyDocumentSale_FirstDocument(){
         String result = IdentifyDocumentGenerator.generateIdentifyDocumentSale(null);
-        assertEquals("BO-0525-0001", result);
+        assertEquals(String.format("BO-%s-0001",date), result);
     }
     @ParameterizedTest
     @ValueSource(strings = {"BO_21", "TicketTest01"})
     public void generateIdentifyDocumentSale_DifferentFormatDocument(String document){
         String result = IdentifyDocumentGenerator.generateIdentifyDocumentSale(document);
-        assertEquals("BO-0525-0001", result);
+        assertEquals(String.format("BO-%s-0001",date), result);
     }
 }
