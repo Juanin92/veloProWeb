3061c9c3c5eb268e2f5deed4a5fe35c1566bd5b3
Juanin92 <j.claveria19@com>
2025-06-17 12:08:46 -0400

    refactor(sale): remove unused imports from sale-helper.service.ts

--- veloProweb-frontend/src/app/services/Sale/sale-helper.service.ts
+++ veloProweb-frontend/src/app/services/Sale/sale-helper.service.ts
@@ -1,9 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Sale } from '../../models/entity/sale/sale';
-import { SaleDetail } from '../../models/entity/sale/sale-detail';
-import { SaleRequestDTO } from '../../models/DTO/sale-request-dto';
-import { SaleDetailDTO } from '../../models/DTO/sale-detail-dto';
-import { PaymentMethod } from '../../models/enum/payment-method';
 import { SaleRequest } from '../../models/entity/sale/sale-request';
 
 @Injectable({
