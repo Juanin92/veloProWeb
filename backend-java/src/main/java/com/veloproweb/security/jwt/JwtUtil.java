7e1c97f9a661abd61c873b164ba069c8bdbf5daa
Juanin92 <j.claveria19@com>
2025-08-05 19:51:21 -0400

    refactor: change return type of validateToken method from Boolean to boolean for consistency

--- backend-java/src/main/java/com/veloproweb/security/jwt/JwtUtil.java
+++ backend-java/src/main/java/com/veloproweb/security/jwt/JwtUtil.java
@@ -109,7 +109,7 @@ public class JwtUtil {
      * @param userDetails - Los detalles del usuario.
      * @return - true si el token es válido, false de lo contrario.
      */
-    public Boolean validateToken(String token, UserDetails userDetails) {
+    public boolean validateToken(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
