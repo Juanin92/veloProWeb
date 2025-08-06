482fdba594b21e60e0bda240ff56a9aebba32508
Juanin92 <j.claveria19@com>
2025-08-05 21:02:06 -0400

    refactor: remove unused import for Authentication in AuthController

--- backend-java/src/main/java/com/veloproweb/controller/user/AuthController.java
+++ backend-java/src/main/java/com/veloproweb/controller/user/AuthController.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
